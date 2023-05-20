75037c1dc084d5ad8592da10b416e261d1c42bd4
appiniakhil <appiniakhil01@gmail.com>
2023-05-20 23:12:51 +0530

    .

--- solutions/0001-Two Sum.cpp
+++ solutions/0001-Two Sum.cpp
@@ -1,19 +1,40 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        //Naive Approach
+        //Time Complexity : O(n^2)
+        // vector<int> ans;
+        // int n = nums.size();
+        // for(int i =0;i<n;i++)
+        // {
+        //     for(int j =i+1;j<n;j++)
+        //     {
+        //         if(nums[i] + nums[j] == target)
+        //         {
+        //             ans.push_back(i);
+        //             ans.push_back(j);
+        //         }
+        //     }
+        // }
+        // return ans;
+
+        //Optimised Approach
+        //Time Complexity : O(n) 
+        unordered_map<int, int> hash;
         vector<int> ans;
-        int n = nums.size();
-        for(int i =0;i<n;i++)
+
+        for (int i = 0; i < nums.size(); i++) 
         {
-            for(int j =i+1;j<n;j++)
+            int remaining = target - nums[i];
+            if (hash.find(remaining) != hash.end()) 
             {
-                if(nums[i] + nums[j] == target)
-                {
-                    ans.push_back(i);
-                    ans.push_back(j);
-                }
+                ans.push_back(hash[remaining]);
+                ans.push_back(i);
+                break;
             }
+            hash[nums[i]] = i;
         }
         return ans;
+
     }
 };
