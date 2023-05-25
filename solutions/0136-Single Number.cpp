c662d9a5915c3202fe11975203f3cc866f3bcf77
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-05-25 22:38:36 +0530

    Update 0136-Single Number.cpp

--- solutions/0136-Single Number.cpp
+++ solutions/0136-Single Number.cpp
@@ -1,1 +1,35 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        //naive approach
+        //Time Complexity: O(n^2)
+        //exceeds Time limit
+        // for (int i =0;i <nums.size();i++) 
+        // {
+        //     bool duplicate = false;
+        //     for (int j =0;j <nums.size();j++) 
+        //     {
+        //         if (i !=j && nums[i] == nums[j]) 
+        //         {
+        //             duplicate = true;
+        //             break;
+        //         }
+        //     }
+        //     if (!duplicate) 
+        //     {
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
 
+        //Optimised Approach
+        //Time Complexity: O(n)
+        //Using XOR operation
+        int ans =0;
+        for(int x : nums)
+        {
+           ans ^= x;
+        }
+        return ans;
+    } 
+};
