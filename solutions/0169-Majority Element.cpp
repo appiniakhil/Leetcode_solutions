706f0bd5cbdafd193f549fbdd2b5f43e7d3f02ec
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-05-25 23:13:20 +0530

    Update 0169-Majority Element.cpp

--- solutions/0169-Majority Element.cpp
+++ solutions/0169-Majority Element.cpp
@@ -4,21 +4,45 @@ public:
         //naive approach
         //Time Complexity: O(n) && Space Complexity: O(n)
         //Using Hashing
-        unordered_map<int, int> hash;
-        for (int x : nums) 
-        {
-            hash[x]++;
-        }
+        // unordered_map<int, int> hash;
+        // for (int x : nums) 
+        // {
+        //     hash[x]++;
+        // }
 
-        // Find the element with the maximum count
-        int majority = 0;
-        int maxCount = 0;
-        for (auto it = hash.begin(); it != hash.end(); it++) 
+        // // Find the element with the maximum count
+        // int majority = 0;
+        // int maxCount = 0;
+        // for (auto it = hash.begin(); it != hash.end(); it++) 
+        // {
+        //     if (it->second > maxCount) 
+        //     {
+        //         majority = it->first;
+        //         maxCount = it->second;
+        //     }
+        // }
+
+        // return majority;
+
+        //Optimised Approach
+        //Time Complexity: O(n) && Space Complexity: O(1)
+        int majority = nums[0];
+        int ans = 1;
+
+        for (int i =1;i<nums.size();i++) 
         {
-            if (it->second > maxCount) 
+            if(nums[i] == majority)
             {
-                majority = it->first;
-                maxCount = it->second;
+                ans++;
+            } 
+            else 
+            {
+                ans--;
+                if (ans == 0) 
+                {
+                    majority =nums[i];
+                    ans =1;
+                }
             }
         }
 
