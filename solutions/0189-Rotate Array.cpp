8688360528f16fece214b17c6e0fb992cc66969b
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-05-30 18:53:43 +0530

    Update 0189-Rotate Array.cpp

--- solutions/0189-Rotate Array.cpp
+++ solutions/0189-Rotate Array.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        //Naive Approach
+        //Time Complexity :O(n^2)
+        //Time Limit Exceeded
+        // for(int i =1;i<=k;i++)
+        // {
+        //     int temp = nums[0];
+        //     for(int j=1;j<nums.size();j++)
+        //     {
+        //         int temp1 = nums[j];
+        //         nums[j] = temp;
+        //         temp = temp1;
+        //     }
+        //     nums[0] = temp;
+        // }
+    }
+};
