24e1022dd8f6303fc64d446c60ad2f74536aaae1
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-05-31 22:36:53 +0530

    Update 0074- Search a 2D Matrix.cpp

--- solutions/0074- Search a 2D Matrix.cpp
+++ solutions/0074- Search a 2D Matrix.cpp
@@ -17,12 +17,52 @@ public:
         // }
         // return false;
 
+        //Better Approach
+        //Time Complexity :O(m*log(n))
+        // int n = matrix.size();
+        // int m = matrix[0].size();
+        // int i = 0,j = m-1;
+        // while(i<n && j >= 0)
+        // {
+        //     if(matrix[i][j] == target)
+        //     {
+        //         return true;
+        //     }
+        //     if(matrix[i][j] > target)
+        //     {
+        //         j--;
+        //     }
+        //     else
+        //     {
+        //         i++;
+        //     }
+        // }
+        // return false;
+
         //Optimised Approach
-        //Time Complexity :O(log(m*n))
+        //Time Complexity :O(log(n*m))
         //Binary Search
         int n = matrix.size();
         int m = matrix[0].size();
-        
 
+        int low = 0;
+        int high = (n*m) - 1;
+        while(low <= high)
+        {
+            int mid = (low + high)/2;
+            if(matrix[mid/m][mid%m] == target)
+            {
+                return true;
+            }
+            if(matrix[mid/m][mid%m] < target)
+            {
+                low  = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return false;
     }
 };
