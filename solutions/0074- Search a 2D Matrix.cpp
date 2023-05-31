11a3c02f74332784c0ea2089858495de48e48a3f
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-05-31 21:52:34 +0530

    Update 0074- Search a 2D Matrix.cpp

--- solutions/0074- Search a 2D Matrix.cpp
+++ solutions/0074- Search a 2D Matrix.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        //Naive Approach
+        //Time Complexity :O(m*n)
+        // int n = matrix.size();
+        // int m = matrix[0].size();
+        // for(int i = 0;i<n;i++)
+        // {
+        //     for(int j = 0;j<m;j++)
+        //     {
+        //         if(matrix[i][j] == target)
+        //         {
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
 
+        //Optimised Approach
+        //Time Complexity :O(log(m*n))
+        //Binary Search
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+
+    }
+};
