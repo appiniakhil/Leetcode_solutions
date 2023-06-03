072eb136760a5f193e571401bfe2f0ff53a7ca28
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-06-04 03:39:56 +0530

    Update 0090-Subsets II.cpp

--- solutions/0090-Subsets II.cpp
+++ solutions/0090-Subsets II.cpp
@@ -1,25 +1,62 @@
 class Solution {
 public:
     //Using Recursion
-    //Time Complexity: O((2^n)*n) && Space Complexity :O(2^n)*O(k)
-    //Where k is the no of subsets
-    void findSubsets(int ind,vector<int> &arr,vector<vector<int>> &ans,vector<int> &ds)
+    //Time Complexity :O(n!*n) && Space Complexity :O(n)+O(n)
+    // void recurPermute(vector<int> &ds, vector<int> &arr, vector<vector<int>> &ans, int freq[])
+    // {
+    //     if(ds.size() == arr.size())
+    //     {
+    //         ans.push_back(ds);
+    //         return;
+    //     }
+
+    //     for(int i =0;i<arr.size();i++)
+    //     {
+    //         if(!freq[i])
+    //         {
+    //             ds.push_back(arr[i]);
+    //             freq[i] = 1;
+    //             recurPermute(ds,arr,ans,freq);
+    //             freq[i] = 0;
+    //             ds.pop_back();
+    //         }
+    //     }
+    // }
+    // vector<vector<int>> permute(vector<int>& nums) {
+    //     vector<vector<int>> ans;
+    //     vector<int> ds;
+    //     int freq[nums.size()];
+    //     for(int i =0;i<nums.size();i++) freq[i] = 0;
+    //     recurPermute(ds,nums,ans,freq);
+    //     return ans;
+    // }
+
+
+    // Second Approach without any extra array
+    //Using Recursion
+    //Time Complexity :O(n!*n) && Space Complexity :O(n)
+
+    void recurPermute(int ind, vector<int> &arr, vector<vector<int>> &ans)
     {
-        ans.push_back(ds);
-        for(int i=ind;i<arr.size();i++)
+        if(ind == arr.size())
         {
-            if(i != ind && arr[i] == arr[i-1]) continue;
-            ds.push_back(arr[i]);
-            findSubsets(i+1,arr,ans,ds);
-            ds.pop_back();
+            ans.push_back(arr);
+            return;
+        }
+
+        for(int i =ind;i<arr.size();i++)
+        {
+            swap(arr[ind],arr[i]);
+            recurPermute(ind+1,arr,ans);
+            swap(arr[ind],arr[i]);
         }
     }
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+
+    vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
-        sort(nums.begin(),nums.end());
-        findSubsets(0,nums,ans,ds);
+        recurPermute(0,nums,ans);
         return ans;
     }
-};
 
+
+};
