0daff64f7e62f47589819cc29987d62a750ec028
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-06-04 15:44:05 +0530

    Update 0046-Permutations.cpp

--- solutions/0046-Permutations.cpp
+++ solutions/0046-Permutations.cpp
@@ -2,32 +2,61 @@ class Solution {
 public:
     //Using Recursion
     //Time Complexity :O(n!*n) && Space Complexity :O(n)+O(n)
-    void recurPermute(vector<int> &ds, vector<int> &arr, vector<vector<int>> &ans, int freq[])
+    // void recurPermute(vector<int> &ds, vector<int> &arr, vector<vector<int>> &ans, int freq[])
+    // {
+    //     if(ds.size() == arr.size())
+    //     {
+    //         ans.push_back(ds);
+    //         return;
+    //     }
+
+    //     for(int i =0;i<arr.size();i++)
+    //     {
+    //         if(!freq[i])
+    //         {
+    //             ds.push_back(arr[i]);
+    //             freq[i] = 1;
+    //             recurPermute(ds,arr,ans,freq);
+    //             freq[i] = 0;
+    //             ds.pop_back();
+    //         }
+    //     }
+    // }
+    // vector<vector<int>> permute(vector<int>& nums) {
+    //     vector<vector<int>> ans;
+    //     vector<int> ds;
+    //     int freq[nums.size()];
+    //     for(int i =0;i<nums.size();i++) freq[i] = 0;
+    //     recurPermute(ds,nums,ans,freq);
+    //     return ans;
+    // }
+
+
+    // Second Approach without any extra array
+    //Using Recursion
+    //Time Complexity :O(n!*n) && Space Complexity :O(n)
+
+    void recurPermute(int ind, vector<int> &arr, vector<vector<int>> &ans)
     {
-        if(ds.size() == arr.size())
+        if(ind == arr.size())
         {
-            ans.push_back(ds);
+            ans.push_back(arr);
             return;
         }
 
-        for(int i =0;i<arr.size();i++)
+        for(int i =ind;i<arr.size();i++)
         {
-            if(!freq[i])
-            {
-                ds.push_back(arr[i]);
-                freq[i] = 1;
-                recurPermute(ds,arr,ans,freq);
-                freq[i] = 0;
-                ds.pop_back();
-            }
+            swap(arr[ind],arr[i]);
+            recurPermute(ind+1,arr,ans);
+            swap(arr[ind],arr[i]);
         }
     }
+
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
-        int freq[nums.size()];
-        for(int i =0;i<nums.size();i++) freq[i] = 0;
-        recurPermute(ds,nums,ans,freq);
+        recurPermute(0,nums,ans);
         return ans;
     }
+
+
 };
