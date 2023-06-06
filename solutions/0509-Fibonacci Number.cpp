78e6a5efc875db4c4d24bbd8810c8f921fffccd5
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-06-06 22:34:19 +0530

    Update 0509-Fibonacci Number.cpp

--- solutions/0509-Fibonacci Number.cpp
+++ solutions/0509-Fibonacci Number.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
 
-    int helper(int n, vector<int> &dp)
-    {
-        if(n <= 1)
-        {
-            return n;
-        }
-        if(dp[n] != -1) return dp[n];
-        return dp[n] = helper(n-1,dp) + helper(n-2,dp);
-    }
+    // int helper(int n, vector<int> &dp)
+    // {
+    //     if(n <= 1)
+    //     {
+    //         return n;
+    //     }
+    //     if(dp[n] != -1) return dp[n];
+    //     return dp[n] = helper(n-1,dp) + helper(n-2,dp);
+    // }
     int fib(int n) {
         //Naive Approach
+        //Using Recursion
         // if(n == 1)
         // {
         //     return 1;
@@ -22,9 +23,40 @@ public:
         // }
         // return fib(n-1) + fib(n-2);
         
-        //Optimised Approach
+        //Using Memorisation
+        //Time Complexity :O(n) && Space Complexity :O(n)+O(n)
+        // vector<int> dp(n+1, -1);
+        // return helper(n,dp);
+
+        //Using Tabulation
         //Time Complexity :O(n) && Space Complexity :O(n)
-        vector<int> dp(n+1, -1);
-        return helper(n,dp);
+        // vector<int> dp(n+1,0);
+        // for(int i=0;i<=n;i++)
+        // {
+        //     if(i==0 || i==1)
+        //     {
+        //         dp[i] = i;
+        //     }
+        //     else
+        //     {
+        //         dp[i] = dp[i-1] + dp[i-2];
+        //     }
+        // }
+        // return dp[n];
+
+        //Space Optimisation
+        //Time Complexity :O(n) && Space Complexity :O(1)
+        int prev2 = 0;
+        int prev = 1;
+        if(n==0){
+            return 0;
+        }
+        for(int i=2;i<=n;i++)
+        {
+            int curi = prev2 + prev;
+            prev2 = prev;
+            prev = curi;
+        }
+        return prev;
     }
 };
