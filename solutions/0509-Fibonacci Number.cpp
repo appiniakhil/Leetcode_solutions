c9636f10e3abc9748537d04eede900055675ac1b
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-06-06 22:07:36 +0530

    Update 0509-Fibonacci Number.cpp

--- solutions/0509-Fibonacci Number.cpp
+++ solutions/0509-Fibonacci Number.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
 
+    int helper(int n, vector<int> &dp)
+    {
+        if(n <= 1)
+        {
+            return n;
+        }
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = helper(n-1,dp) + helper(n-2,dp);
+    }
+    int fib(int n) {
+        //Naive Approach
+        // if(n == 1)
+        // {
+        //     return 1;
+        // }
+        // if(n == 0)
+        // {
+        //     return 0;
+        // }
+        // return fib(n-1) + fib(n-2);
+        
+        //Optimised Approach
+        vector<int> dp(n+1, -1);
+        return helper(n,dp);
+    }
+};
