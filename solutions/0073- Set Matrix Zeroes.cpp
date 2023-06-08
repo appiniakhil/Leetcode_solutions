91d421c460039a3ac135a09c6f43cfac4cb386bf
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-06-09 00:14:41 +0530

    Update 0073- Set Matrix Zeroes.cpp

--- solutions/0073- Set Matrix Zeroes.cpp
+++ solutions/0073- Set Matrix Zeroes.cpp
@@ -1,1 +1,33 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        //Naive Approach
+        //Time Complexity :O(2*n*m) and Space Complexity :O(n) + O(m)
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int> col(m,0);
+        vector<int> row(n,0);
 
+        for(int i =0;i<n;i++)
+        {
+            for(int j =0;j<m;j++)
+            {
+                if(matrix[i][j] == 0)
+                {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+        for(int i =0;i<n;i++)
+        {
+            for(int j =0;j<m;j++)
+            {
+                if(row[i] || col[j])
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
