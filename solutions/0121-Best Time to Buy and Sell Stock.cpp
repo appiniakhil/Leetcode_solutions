ba613e40945e49d0e94f3c8a7b9c5a8a0afaf38a
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-07-13 19:19:30 +0530

    Update 0121-Best Time to Buy and Sell Stock.cpp

--- solutions/0121-Best Time to Buy and Sell Stock.cpp
+++ solutions/0121-Best Time to Buy and Sell Stock.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
 
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int maxProfit(vector<int>& prices) {
+        int mini = prices[0];
+        int maxProfit = 0;
+        int n = prices.size();
+
+        for(int i=0;i<n;i++)
+        {
+            int cost = prices[i] - mini;
+            maxProfit = max(maxProfit, cost);
+            mini = min(mini,prices[i]);
+        }
+
+        return maxProfit;
+    }
+};
