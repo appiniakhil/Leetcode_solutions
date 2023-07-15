1cfb5c03699e50099d8d4f15940ffcbc93215810
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-07-16 05:14:30 +0530

    Update 0119-Pascal's Triangle II.cpp

--- solutions/0119-Pascal's Triangle II.cpp
+++ solutions/0119-Pascal's Triangle II.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
 
+    //Time Complexity ;O(n*n) && Space Complexity :O(n*n)
+    vector<int> getRow(int rowIndex) {
+        vector<vector<int>> dp(rowIndex+1);
+
+        dp[0].push_back(1);
+        for(int i=1;i<rowIndex+1;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                if(j == 0 || j == i)
+                {
+                    dp[i].push_back(1);
+                }
+                else
+                {
+                    dp[i].push_back(dp[i-1][j-1] + dp[i-1][j]);
+                }
+            }
+        }
+
+        return dp[rowIndex];
+    }
+};
