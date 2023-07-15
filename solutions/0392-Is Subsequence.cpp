fa35fff74816898aaa86f308777b02708c5d3c04
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-07-15 23:34:37 +0530

    Update 0392-Is Subsequence.cpp

--- solutions/0392-Is Subsequence.cpp
+++ solutions/0392-Is Subsequence.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
 
+    //Using Two pointers
+    //Time Complexity : O(n + m) && Space Complexity :O(1)
+    bool isSubsequence(string s, string t) {
+        
+        int firstptr = 0;
+        int secondptr = 0;
+
+        while(secondptr < t.size() && firstptr < s.size())
+        {
+            if(s[firstptr] == t[secondptr])
+            {
+                firstptr++;
+                secondptr++;
+            }
+            else
+            {
+                secondptr++;
+            }
+        }
+
+        return firstptr == s.length();
+    }
+};
