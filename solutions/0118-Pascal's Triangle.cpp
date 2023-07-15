438695f1baa0f61797ed7eb8c54435b0f64ad323
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-07-16 04:55:54 +0530

    Update 0118-Pascal's Triangle.cpp

--- solutions/0118-Pascal's Triangle.cpp
+++ solutions/0118-Pascal's Triangle.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    //Naive Approach
+    //Time Complexity :O(n*n) && Space Complexity :O(n*n)
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> dp(numRows);
 
+        dp[0].push_back(1);
+        for(int i=1;i<numRows;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                if(j == 0 || j == i)
+                {
+                    dp[i].push_back(1);
+                }
+                else
+                {
+                    dp[i].push_back(dp[i-1][j-1] + dp[i-1][j]);
+                }
+            }
+        }
+
+        return dp;
+    }
+};
