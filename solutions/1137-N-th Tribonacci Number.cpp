13b665b60e09339ad7f4cd313ddb4aa5c688c168
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-07-18 02:25:11 +0530

    Update 1137-N-th Tribonacci Number.cpp

--- solutions/1137-N-th Tribonacci Number.cpp
+++ solutions/1137-N-th Tribonacci Number.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
 
+    //Using Tabulation
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    int tribonacci(int n) {
+        vector<int> dp(n+3,0);
+        dp[0] = 0;
+        dp[1] = dp[2] = 1;
+
+        for(int i =3;i<=n;i++)
+        {
+            dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
+        }
+
+        return dp[n];
+    }
+};
