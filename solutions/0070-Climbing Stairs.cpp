0c9d2b0e61c16115acb803e358efd9abbe094ddc
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-07-18 14:54:13 +0530

    Update 0070-Climbing Stairs.cpp

--- solutions/0070-Climbing Stairs.cpp
+++ solutions/0070-Climbing Stairs.cpp
@@ -1,37 +1,86 @@
 class Solution {
 public:
-    // int helper(int n , vector<int> &dp)
+
+    //Using Recursion
+    //Time Complexity :O(2^n) && Space Complexity :O(n)
+    //Time Limit Exceeded
+    // int helper(int index)
     // {
-    //     if(n <= 1) return 1;
-    //     if(dp[n] != -1) return dp[n];
-    //     return dp[n] = helper(n-1,dp) + helper(n-2,dp);
+    //     if(index <= 1)
+    //     {
+    //         return 1;
+    //     }
+
+    //     int oneStep = helper(index-1);
+    //     int secondStep = helper(index-2);
+
+    //     return oneStep + secondStep;
     // }
+
+    // int climbStairs(int n) {
+    //     return helper(n);
+    // }
+
+    //Using Memorisation
+    //Time Complexity :O(n) && Space Complexity :O(n) + O(n)
+    // int helper(int index,vector<int>& dp)
+    // {
+    //     if(index <= 1)
+    //     {
+    //         return 1;
+    //     }
+
+    //     if(dp[index] != -1)
+    //     {
+    //         return dp[index];
+    //     }
+
+    //     int oneStep = helper(index-1,dp);
+    //     int secondStep = helper(index-2,dp);
+
+    //     return dp[index] = oneStep + secondStep;
+    // }
+
+    // int climbStairs(int n) {
+    //     vector<int> dp(n+1,-1);
+
+    //     return helper(n,dp);
+    // }
+
+    //Using Tabulation
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    // int climbStairs(int n) {
+    //     vector<int> dp(n+1, 0);
+
+    //     dp[0] = dp[1] = 1;
+
+    //     for(int index=2;index<=n;index++)
+    //     {
+    //         int oneStep = dp[index-1];
+    //         int secondStep = dp[index-2];
+
+    //         dp[index] = oneStep + secondStep;
+    //     }
+
+    //     return dp[n];
+    // }
+
+    //Using Space Optimisation
+    //Time Complexity :O(n) && Space Complexity :O(1)
     int climbStairs(int n) {
-        //Using Memorisation
-        //Time Complexity :O(n) && Space Complexity :O(n)+O(n)
-        // vector<int> dp(n+1,-1);
-        // return helper(n,dp);
+        int prev2 = 1, prev = 1;
 
-        //Using Tabulation
-        //Time Complexity :O(n) && Space Complexity :O(n)
-        // vector<int> dp(n+1,0);
-        // dp[0] = dp[1] = 1;
-        // for(int i =2;i<=n;i++)
-        // {
-        //     dp[i] = dp[i-1] + dp[i-2];
-        // }
-        // return dp[n];
-
-        //Space Optimisation
-        //Time Complexity :O(n) && Space Complexity :O(1)
-        int prev2 = 1;
-        int prev = 1;
-        for(int i =2;i<=n;i++)
+        for(int index=2;index<=n;index++)
         {
-            int curi = prev2 + prev;
+            int oneStep = prev;
+            int secondStep = prev2;
+
+            int curi = oneStep + secondStep;
+
             prev2 = prev;
             prev = curi;
         }
+
         return prev;
     }
 };
