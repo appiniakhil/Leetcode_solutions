3a6e83ff472d63448679c575c51d524ad1129d6f
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-07-19 00:44:28 +0530

    Update 1492-The kth Factor of n.cpp

--- solutions/1492-The kth Factor of n.cpp
+++ solutions/1492-The kth Factor of n.cpp
@@ -1,1 +1,44 @@
+class Solution {
+public:
 
+    //Time and Complexity :O(n) and Space Complexity :O(n)
+    // int kthFactor(int n, int k) {
+    //     vector<int> temp;
+
+    //     for(int i=1;i<=n;i++)
+    //     {
+    //         if(n%i == 0)
+    //         {
+    //             temp.push_back(i);
+    //         }
+    //     }
+
+    //     int tempSize = temp.size();
+    //     if(tempSize >= k)
+    //     {
+    //         return temp[k-1];
+    //     }
+
+    //     return -1;
+    // }
+
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int kthFactor(int n, int k) {
+        int fact=0;
+
+        for(int i=1;i<=n;i++)
+        {
+            if(n%i == 0)
+            {
+                fact++;
+            }
+
+            if(fact == k)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
