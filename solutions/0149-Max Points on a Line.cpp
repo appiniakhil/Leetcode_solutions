855523142d1f62c64d860682632284aec04a419a
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-07-19 23:38:29 +0530

    Update 0149-Max Points on a Line.cpp

--- solutions/0149-Max Points on a Line.cpp
+++ solutions/0149-Max Points on a Line.cpp
@@ -1,1 +1,34 @@
+class Solution {
+public:
 
+    //Time Complexity :O(n * n) && Space Complexity :O(n)
+    double helper(vector<int>& p1, vector<int>& p2) {
+        return ((double)(p2[1] - p1[1]))/((double)(p2[0] - p1[0]));
+    }
+
+    int maxPoints(vector<vector<int>>& points) {
+        int n = points.size(), maxCount = 0;
+        double slope;
+
+        for(int i=0; i<n-1; i++) 
+        {
+            unordered_map<double,int> mp;
+
+            for(int j=0; j<n; j++) 
+            {   
+                if(i == j)
+                {
+                    continue;
+                }
+
+                slope = helper(points[i], points[j]);
+
+                mp[slope]++;
+
+                maxCount = max(maxCount, mp[slope]);
+            }
+        }
+
+        return maxCount + 1;
+    }
+};
