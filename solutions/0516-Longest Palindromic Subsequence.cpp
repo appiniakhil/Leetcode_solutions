92b159a4dd6101d8a9d8028ecad51fb5b5211162
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-07-20 09:53:04 +0530

    Update 0516-Longest Palindromic Subsequence.cpp

--- solutions/0516-Longest Palindromic Subsequence.cpp
+++ solutions/0516-Longest Palindromic Subsequence.cpp
@@ -1,1 +1,39 @@
+class Solution {
+public: 
+    int helper(string& s1, string& s2)
+    {
+        int n = s1.size();
+        int m = s2.size();
 
+        int dp[n+1][m+1];
+
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=m;j++)
+            {
+                if(i == 0 || j == 0)
+                {
+                    dp[i][j] = 0;
+                } 
+                else if(s1[i-1] == s2[j-1])
+                {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+
+    int longestPalindromeSubseq(string s) {
+        string s2 = s;
+
+        reverse(s2.begin(), s2.end());
+ 
+        return helper(s, s2);
+    }
+};
