ca11e3eaebdd7fcfa489d5b2a57407e9084e8ff5
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-07-21 17:20:14 +0530

    Update 0080-Remove Duplicates from Sorted Array II.cpp

--- solutions/0080-Remove Duplicates from Sorted Array II.cpp
+++ solutions/0080-Remove Duplicates from Sorted Array II.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
 
+        if(n < 3)
+        {
+            return n;
+        }
+        
+        int index = 2;
+        for(int i=2; i<n;i++)
+        {
+            if(nums[i] != nums[index-2])
+            {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+};
