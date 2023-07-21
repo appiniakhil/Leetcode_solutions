fccf00dd867ff5637142baa00824e0e9c26f8533
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-07-21 16:58:04 +0530

    Update 0027-Remove Element.cpp

--- solutions/0027-Remove Element.cpp
+++ solutions/0027-Remove Element.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
 
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+
+        int res = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i] != val)
+            {
+                nums[res] = nums[i];
+                res++;
+            }
+        }
+
+        return res;
+    }
+};
