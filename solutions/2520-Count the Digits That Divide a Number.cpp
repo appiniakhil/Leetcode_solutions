dcbd84a39a7948bef0f6e20180f56ddcd389d375
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-07-23 01:03:30 +0530

    Update 2520-Count the Digits That Divide a Number.cpp

--- solutions/2520-Count the Digits That Divide a Number.cpp
+++ solutions/2520-Count the Digits That Divide a Number.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
 
+    //Time Complexity :O(log n) && Space Complexity : O(1)
+    int countDigits(int num) {
+        int count = 0;
+        int  y = num;
+        while(num > 0)
+        {
+            int x = num%10;
+            if(y%x == 0)
+            {
+                count++;
+            }
+            num = num/10;
+        }
+
+        return count;
+    }
+};
