77a4dda763a19c920b89a62bcb26e14348031a84
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-07-22 12:39:44 +0530

    Update 0209-Minimum Size Subarray Sum.cpp

--- solutions/0209-Minimum Size Subarray Sum.cpp
+++ solutions/0209-Minimum Size Subarray Sum.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
 
+    //Sliding Window Approach
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+        
+        int j = 0;
+        int mini = INT_MAX;
+        for(int i=0;i<n;i++)
+        {
+            target -= nums[i];
+            while(target <= 0)
+            {
+                mini = min(mini, i-j+1);
+                target += nums[j];
+                j++;
+            }
+        }
+
+        if(mini != INT_MAX)
+        {
+            return mini;
+        }
+
+        return 0;
+    }
+};
