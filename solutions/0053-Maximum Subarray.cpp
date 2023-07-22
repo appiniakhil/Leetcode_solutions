1c8d281a9e510122a1f1d02777c6e622345a1981
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-07-23 02:13:30 +0530

    Update 0053-Maximum Subarray.cpp

--- solutions/0053-Maximum Subarray.cpp
+++ solutions/0053-Maximum Subarray.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
 
+    //Kadane's algorithm 
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int maxSubArray(vector<int>& nums) {
+        int globalMaxSum = nums[0], currMaxSum = nums[0];
+
+        for (int index = 1; index < nums.size(); index++) 
+        {
+            currMaxSum = max(currMaxSum + nums[index], nums[index]);
+            globalMaxSum = max(globalMaxSum, currMaxSum);
+        }
+        
+        return globalMaxSum;
+    }
+};
