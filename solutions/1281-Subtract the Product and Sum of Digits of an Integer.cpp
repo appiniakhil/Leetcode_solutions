906403e2755f799ff910a5de98d17b3c19296032
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-07-23 00:29:15 +0530

    Update 1281-Subtract the Product and Sum of Digits of an Integer.cpp

--- solutions/1281-Subtract the Product and Sum of Digits of an Integer.cpp
+++ solutions/1281-Subtract the Product and Sum of Digits of an Integer.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
 
+    //Time Complexity :O(log n) && Space Compleity :O(1)
+    int subtractProductAndSum(int n) {
+        int sum = 0;
+        int prod = 1;
+
+        while(n>0)
+        {
+            int x = n%10;
+            prod *= x;
+            sum += x;
+            n = n/10;
+        }
+
+        return prod-sum;
+    }
+};
