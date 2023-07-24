e29f235757bc7ecaf14cde6b604f2b0b8b005a02
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-07-24 12:17:51 +0530

    Update 0033-Search in Rotated Sorted Array.cpp

--- solutions/0033-Search in Rotated Sorted Array.cpp
+++ solutions/0033-Search in Rotated Sorted Array.cpp
@@ -1,1 +1,61 @@
+class Solution {
+public:
+    
+    //Naive Approach
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    // int search(vector<int>& nums, int target) {
+    //     int n = nums.size();
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         if(nums[i] == target)
+    //         {
+    //             return i;
+    //         }
+    //     }
 
+    //     return -1;
+    // }
+
+    //Binary Search
+    //Time Complexity :O(log n) && Space Complexity :O(1)
+    int search(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+
+        int low = 0;
+        int high = n - 1;
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target)
+            {
+                return mid;
+            }   
+            else if (nums[low] <= nums[mid])
+            {
+                if (nums[low] <= target && target <= nums[mid])
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            else
+            {
+                if (nums[mid] <= target && target <= nums[high])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
