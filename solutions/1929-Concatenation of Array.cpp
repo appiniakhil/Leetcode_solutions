2e7a53200282b63161f0910207afc60ce77905a1
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-07-29 13:30:26 +0530

    Update 1929-Concatenation of Array.cpp

--- solutions/1929-Concatenation of Array.cpp
+++ solutions/1929-Concatenation of Array.cpp
@@ -1,1 +1,14 @@
+class Solution {
+public:
 
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=0;i<n;i++)
+        {
+            nums.push_back(nums[i]);
+        }
+
+        return nums;
+    }
+};
