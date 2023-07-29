cface9774c7e41454b077d520e85eebde3f0f7a1
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-07-29 13:39:54 +0530

    Update 1920-Build Array from Permutation.cpp

--- solutions/1920-Build Array from Permutation.cpp
+++ solutions/1920-Build Array from Permutation.cpp
@@ -1,1 +1,33 @@
+class Solution {
+public:
 
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    // vector<int> buildArray(vector<int>& nums) {
+    //     int n = nums.size();
+    //     vector<int> ans;
+
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         ans.push_back(nums[nums[i]]);
+    //     }
+
+    //     return ans;
+    // }
+
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    vector<int> buildArray(vector<int>& nums) {
+		int n = nums.size();
+
+		for(int i=0;i<n;i++)
+		{
+			nums[i] = nums[i] + n*(nums[nums[i]]%n);
+		}
+
+		for(int i=0;i<n;i++)
+		{
+			nums[i] = nums[i]/n;
+		}
+        
+		return nums;
+	}
+};
