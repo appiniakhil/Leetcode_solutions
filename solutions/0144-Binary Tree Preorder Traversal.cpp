0c128e8c8849bdaa16c79b18bd23784e59212115
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-08 03:02:23 +0530

    Update 0144-Binary Tree Preorder Traversal.cpp

--- solutions/0144-Binary Tree Preorder Traversal.cpp
+++ solutions/0144-Binary Tree Preorder Traversal.cpp
@@ -13,21 +13,54 @@ class Solution {
 public:
     //Using Recursion
     //Time Complexity :O(n) && Space Complexity :O(n)
-    void helper(TreeNode* root,vector<int>& ans)
-    {
-        if(root == NULL)
-        {
-            return ;
-        }
+    // void helper(TreeNode* root,vector<int>& ans)
+    // {
+    //     if(root == NULL)
+    //     {
+    //         return ;
+    //     }
 
-        ans.push_back(root->val);
-        helper(root->left,ans);
-        helper(root->right,ans);
-    }
+    //     ans.push_back(root->val);
+    //     helper(root->left,ans);
+    //     helper(root->right,ans);
+    // }
 
+    // vector<int> preorderTraversal(TreeNode* root) {
+    //     vector<int> ans;
+    //     helper(root, ans);
+
+    //     return ans;
+    // }
+
+
+    //Iterative Solution
+    //Time Complexity :O(n) && Space Complexity :O(n)
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> ans;
-        helper(root, ans);
+
+        if(root == NULL)
+        {
+            return ans;
+        }
+
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty())
+        {
+            root = st.top();
+            st.pop();
+            ans.push_back(root->val);
+
+            if(root->right != NULL)
+            {
+                st.push(root->right);
+            }
+
+            if(root->left != NULL)
+            {
+                st.push(root->left);
+            }
+        }
 
         return ans;
     }
