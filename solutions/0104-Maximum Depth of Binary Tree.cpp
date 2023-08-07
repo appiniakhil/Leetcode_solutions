e9a6767e9d32bbe5bd590b12a7375302b6303dda
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-08 03:45:52 +0530

    Update 0104-Maximum Depth of Binary Tree.cpp

--- solutions/0104-Maximum Depth of Binary Tree.cpp
+++ solutions/0104-Maximum Depth of Binary Tree.cpp
@@ -1,1 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    //Using Recursion
+    //Time Complexity :O(n) && Space Complexity :O(h) where h is the height of the tree
+    int maxDepth(TreeNode* root) {
+        if(root == NULL)
+        {
+            return 0;
+        }
 
+        int l = maxDepth(root->left);
+        int r = maxDepth(root->right);
+
+        return 1 + max(l,r);
+    }
+};
