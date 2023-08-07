8af00a9b7dcbebe4cce579f46c9e95fc8422ee6b
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-08 04:16:33 +0530

    Update 0110-Balanced Binary Tree.cpp

--- solutions/0110-Balanced Binary Tree.cpp
+++ solutions/0110-Balanced Binary Tree.cpp
@@ -1,1 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
 
+    //Using Recursion
+    //Time Complexity : O(n) && Space Complexity :O(n)
+    int helper(TreeNode* root)
+    {
+        if(root == NULL)
+        {
+            return 0;
+        }
+
+        int l = helper(root->left);
+        int r = helper(root->right);
+
+        if(l == -1 || r == -1)
+        {
+            return -1;
+        }
+
+        if(abs(l-r) > 1)
+        {
+            return -1;
+        }
+
+        return 1 + max(l, r);
+    }
+
+    bool isBalanced(TreeNode* root) {
+       return (helper(root) != -1); 
+    }
+};
