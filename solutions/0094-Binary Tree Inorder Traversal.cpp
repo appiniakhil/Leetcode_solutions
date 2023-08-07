062a29d3eb5ea69914795676e8350a11ad3c762e
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-08 03:20:56 +0530

    Update 0094-Binary Tree Inorder Traversal.cpp

--- solutions/0094-Binary Tree Inorder Traversal.cpp
+++ solutions/0094-Binary Tree Inorder Traversal.cpp
@@ -14,21 +14,49 @@ public:
 
     //Using Recursion
     //Time Complexity :O(n) && Space Complexity :O(n)
-    void helper(TreeNode* root, vector<int>& ans)
-    {
-        if(root == NULL)
-        {
-            return ;
-        }
+    // void helper(TreeNode* root, vector<int>& ans)
+    // {
+    //     if(root == NULL)
+    //     {
+    //         return ;
+    //     }
 
-        helper(root->left,ans);
-        ans.push_back(root->val);
-        helper(root->right,ans);
-    }
+    //     helper(root->left,ans);
+    //     ans.push_back(root->val);
+    //     helper(root->right,ans);
+    // }
 
+    // vector<int> inorderTraversal(TreeNode* root) {
+    //     vector<int> ans;
+    //     helper(root, ans);
+
+    //     return ans;
+    // }
+
+    //Iterative Solution
+    //Time Complexity :O(n) && Space Complexity :O(n)
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        helper(root, ans);
+        
+        stack<TreeNode*> st;
+        TreeNode* node = root;
+
+        while(true)
+        {
+            if(node != NULL)
+            {
+                st.push(node);
+                node = node->left;
+            }
+            else
+            {
+                if(st.empty() == true) break;
+                node = st.top();
+                st.pop();
+                ans.push_back(node->val);
+                node = node->right;
+            }
+        }
 
         return ans;
     }
