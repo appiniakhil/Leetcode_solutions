dd5170056064aa070b60fb0c0416ec2d1230feb8
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-08 18:17:24 +0530

    Update 1572-Matrix Diagonal Sum.cpp

--- solutions/1572-Matrix Diagonal Sum.cpp
+++ solutions/1572-Matrix Diagonal Sum.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
 
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int primaryDiagonal = 0;
+        int secondaryDiagonal = 0;
+
+        for(int i=0;i<n;i++)
+        {
+            primaryDiagonal += mat[i][i];
+            secondaryDiagonal += mat[i][n-i-1];
+        }
+
+        if(n%2 == 1)
+        {
+            return primaryDiagonal + secondaryDiagonal - mat[n/2][n/2];
+        }
+
+        return primaryDiagonal + secondaryDiagonal;
+    }
+};
