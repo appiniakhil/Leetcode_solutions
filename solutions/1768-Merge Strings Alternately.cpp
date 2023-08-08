303f4113f17fb75915ae9fceeffe59f5ea85aae7
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-08 17:25:28 +0530

    Update 1768-Merge Strings Alternately.cpp

--- solutions/1768-Merge Strings Alternately.cpp
+++ solutions/1768-Merge Strings Alternately.cpp
@@ -1,1 +1,31 @@
+class Solution {
+public:
+    //Two Pointer Approach
+    //Time Complexity :O(n + m) && Space Complexity :O(n + m)
+    string mergeAlternately(string word1, string word2) {
+        int i =0,j=0;
+        int n = word1.size();
+        int m = word2.size();
 
+        string ans = "";
+        while(i<n && j<m)
+        {
+            ans += word1[i];
+            ans += word2[j];
+            i++;
+            j++;
+        }
+
+        for(;i<n;i++)
+        {
+            ans += word1[i];
+        }
+
+        for(;j<m;j++)
+        {
+            ans += word2[j];
+        }
+
+        return ans;
+    }
+};
