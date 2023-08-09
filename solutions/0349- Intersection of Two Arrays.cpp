44c6d31745e8e02f31664fcca5ec47eda3feed82
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-09 06:34:37 +0530

    Update 0349- Intersection of Two Arrays.cpp

--- solutions/0349- Intersection of Two Arrays.cpp
+++ solutions/0349- Intersection of Two Arrays.cpp
@@ -1,1 +1,42 @@
+class Solution {
+public:
 
+    //Two pointer
+    //Time Complexity :O(nlogn) && Space Complexity :O(n)
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        set<int> s;
+
+        int i = 0,j = 0;
+        int n = nums1.size();
+        int m = nums2.size();
+
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        while(i<n && j<m)
+        {
+            if(nums1[i] > nums2[j])
+            {
+                j++;
+            }
+            else if(nums1[i] < nums2[j])
+            {
+                i++;
+            }
+            else
+            {
+                s.insert(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+
+        vector<int> res;
+        for(auto i: s)
+        {
+            res.push_back(i);
+        }
+
+        return res;
+    }
+};
