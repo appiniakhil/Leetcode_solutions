994accb405152abe66a8d8531a433defbb67b151
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-09 07:57:31 +0530

    Update 0028-Find the Index of the First Occurrence in a String.cpp

--- solutions/0028-Find the Index of the First Occurrence in a String.cpp
+++ solutions/0028-Find the Index of the First Occurrence in a String.cpp
@@ -1,1 +1,37 @@
+class Solution {
+public:
 
+    //Time Complexity :(n * m) && Space Complexity :O(1)
+    int strStr(string haystack, string needle) {
+        int n = haystack.size();
+        int m = needle.size();
+
+        if(n < m)
+        {
+            return -1;
+        }
+
+        int i=0,j=0,k=0;
+        while(k<m && j<n)
+        {
+            if(haystack[j] != needle[k])
+            {
+                j = i+1;
+                i = j;
+                k = 0;
+            }
+            else
+            {
+                k++;
+                j++;
+            }
+        }
+
+        if(k == m)
+        {
+            return i;
+        }
+
+        return -1;
+    }
+};
