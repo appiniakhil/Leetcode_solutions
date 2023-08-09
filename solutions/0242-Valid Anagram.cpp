9fd36a4ca30b36b386a4ffd94f8e7bd749f54d9c
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-10 01:20:14 +0530

    Update 0242-Valid Anagram.cpp

--- solutions/0242-Valid Anagram.cpp
+++ solutions/0242-Valid Anagram.cpp
@@ -1,1 +1,45 @@
+class Solution {
+public:
 
+    //Time Complexity :O(nlogn  + mlogm) && Space Complexity :O(1)
+    // bool isAnagram(string s, string t) {
+    //     sort(s.begin(), s.end());
+    //     sort(t.begin(), t.end());
+
+    //     if(s == t)
+    //     {
+    //         return true;
+    //     }
+
+    //     return false;
+    // }
+
+    //Using Map
+    //Time Complexity :O(n + m) && Space Complexity :O(min(n,m))
+    bool isAnagram(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+
+        if(n != m)
+        {
+            return false;
+        }
+
+        unordered_map<char,int> mp;
+        for(int i=0;i<n;i++)
+        {
+            mp[s[i]]++;
+            mp[t[i]]--;
+        }
+
+        for(auto x: mp)
+        {
+            if(x.second != 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
