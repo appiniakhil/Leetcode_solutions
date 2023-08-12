94642f8ef01e8751aa7a9fa0fcc7e6561f0feb98
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-12 22:43:40 +0530

    Update 0222-Count Complete Tree Nodes.cpp

--- solutions/0222-Count Complete Tree Nodes.cpp
+++ solutions/0222-Count Complete Tree Nodes.cpp
@@ -1,1 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
 
+    //Time Complexity :O((lo(g n) ^ 2) && Space Complexity :O(log n)
+    int heightLeft(TreeNode* node)
+    {
+        int h = 0;
+        while(node)
+        {
+            h++;
+            node = node->left;
+        }
+
+        return h;
+    }
+
+    int heightRight(TreeNode* node)
+    {
+        int h = 0;
+        while(node)
+        {
+            h++;
+            node = node->right;
+        }
+
+        return h;
+    }
+
+    int countNodes(TreeNode* root) {
+        if(root == NULL)
+        {
+            return 0;
+        }    
+
+        int lh = heightLeft(root);
+        int rh = heightRight(root);
+
+        if(lh == rh)
+        {
+            return (1<<lh) - 1;
+        }
+
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
