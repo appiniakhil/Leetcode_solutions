5d9776b9e1375ffbedc9d58a5ad27d36dfed0b25
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-13 01:06:50 +0530

    Update 2119-A Number After a Double Reversal.cpp

--- solutions/2119-A Number After a Double Reversal.cpp
+++ solutions/2119-A Number After a Double Reversal.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
 
+    //Time Complexity :O(1) && Space Complexity :O(1)
+    bool isSameAfterReversals(int num) {
+        if(num == 0)
+        {
+            return true;
+        }
+
+        if(num%10 == 0)
+        {
+            return false;;
+        }
+
+        return true;
+    }
+};
