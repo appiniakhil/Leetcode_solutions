c479039f83d43a97677838c3e282faf870caed51
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-13 15:05:47 +0530

    Update 2169-Count Operations to Obtain Zero.cpp

--- solutions/2169-Count Operations to Obtain Zero.cpp
+++ solutions/2169-Count Operations to Obtain Zero.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    //Time Complexity :O(num1 + num2) && Space Complexity :O(1)
+    int countOperations(int num1, int num2) {
+        int count = 0;
+        while(num1 > 0 && num2 > 0)
+        {
+            if(num1 < num2)
+            {
+                num2 -= num1;
+                count++;
+            }
+            else if(num1 > num2)
+            {
+                num1 -= num2;
+                count++;
+            }
+            else
+            {
+                count++;
+                break;
+            }
+        }
 
+        return count;
+    }
+};
