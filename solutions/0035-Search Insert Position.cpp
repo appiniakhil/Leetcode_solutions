f600c3bc3f6fbc3520a0a93863d79669b91e43cf
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-13 23:33:01 +0530

    Update 0035-Search Insert Position.cpp

--- solutions/0035-Search Insert Position.cpp
+++ solutions/0035-Search Insert Position.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
 
+    //Time Complexity :O(log n) && Space Complecity :O(1)
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+
+        while(low <= high)
+        {
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            else if(nums[mid] > target)
+            {
+                high = mid-1;
+            }
+            else
+            {
+                low = mid+1;
+            }
+        }
+        
+        return low;
+    }
+};
