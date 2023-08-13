631dc4258b83bd4d6d3adffc7e9e941711fbe0d8
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-14 01:26:38 +0530

    Update 1480-Running Sum of 1d Array.cpp

--- solutions/1480-Running Sum of 1d Array.cpp
+++ solutions/1480-Running Sum of 1d Array.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    // vector<int> runningSum(vector<int>& nums) {
+    //     int n = nums.size();
 
+    //     vector<int> temp;
+    //     int sum = 0;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         sum += nums[i];
+    //         temp.push_back(sum);
+    //     }
+
+    //     return temp;
+    // }
+
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    vector<int> runningSum(vector<int>& nums) {
+        int n = nums.size();
+
+        for(int i=1;i<n;i++)
+        {
+            nums[i] += nums[i-1];
+        }
+
+        return nums;
+    }
+};
