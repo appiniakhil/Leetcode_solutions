09851e9710cd6dddf755edbacdddd992d4c056bb
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-14 01:41:25 +0530

    Update 1672-Richest Customer Wealth.cpp

--- solutions/1672-Richest Customer Wealth.cpp
+++ solutions/1672-Richest Customer Wealth.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    //Time Complexity :O(n*m) && Space Complexity :O(1)
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int n = accounts.size();
+        int m = accounts[0].size();
+        int maxi = INT_MIN;
 
+        for(int i=0;i<n;i++)
+        {
+            int sum = 0;
+            for(int j=0;j<m;j++)
+            {
+                sum += accounts[i][j];
+            }
+            maxi = max(maxi, sum);
+        }
+
+        return maxi;
+    }
+};
