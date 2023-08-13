a9a3bfec066952e36e9a09315da430b1a052a7e8
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-13 14:29:36 +0530

    Update 0007-Reverse Integer.cpp

--- solutions/0007-Reverse Integer.cpp
+++ solutions/0007-Reverse Integer.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
 
+    //Time Complexity :O(n) && Space Complexity:O(1)
+    int helper(int x)
+    {
+        long num = 0;
+        while(x != 0)
+        {
+            num = num*10 + x%10;
+            x /= 10;
+        }
+
+        if(num > INT_MAX || num < INT_MIN)
+        {
+            return 0;
+        }
+
+        return (int)num;
+    }
+
+    int reverse(int x) {
+        return helper(x);
+    }
+};
