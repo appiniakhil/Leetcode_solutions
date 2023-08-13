7c1f51f4f12f421603b6c68eaf08f23a9093a1b5
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-14 02:35:02 +0530

    Update 1207-Unique Number of Occurrences.cpp

--- solutions/1207-Unique Number of Occurrences.cpp
+++ solutions/1207-Unique Number of Occurrences.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(k) where k is the number of unique elements
+    bool uniqueOccurrences(vector<int>& arr) {
+        int n = arr.size();
+        unordered_map<int, int> mp;
+        for(int i=0;i<n;i++)
+        {
+            mp[arr[i]]++;
+        }
 
+        unordered_map<int, int> Countmp;
+        for(auto& it : mp)
+        {
+            Countmp[it.second]++;
+        }
+
+        return mp.size() == Countmp.size();
+    }
+};
