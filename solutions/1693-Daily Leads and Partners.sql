d85d170f3022148b0907aecca8f2711607b6eae0
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-16 03:36:02 +0530

    Update 1693-Daily Leads and Partners.sql

--- solutions/1693-Daily Leads and Partners.sql
+++ solutions/1693-Daily Leads and Partners.sql
@@ -1,1 +1,2 @@
-
+# Write your MySQL query statement below
+SELECT date_id,make_name,COUNT(distinct lead_id) AS unique_leads,COUNT(distinct partner_id) AS unique_partners FROM DailySales GROUP BY date_id,make_name;
