e3c0b817342f2038b6547b07aa8df1c237ba8156
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-16 03:54:56 +0530

    Update 1431-Kids With the Greatest Number of Candies.cpp

--- solutions/1431-Kids With the Greatest Number of Candies.cpp
+++ solutions/1431-Kids With the Greatest Number of Candies.cpp
@@ -1,1 +1,54 @@
+class Solution {
+public:
+    //Bit Lengthy
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    // vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+    //     int n = candies.size();
+    //     vector<bool> res;
+    //     int maxi = INT_MIN;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         maxi = max(maxi, candies[i]);
+    //     }
 
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         candies[i] += extraCandies;
+    //     }
+
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         if(maxi > candies[i])
+    //         {
+    //             res.push_back(false);
+    //         }
+    //         else
+    //         {
+    //             res.push_back(true);
+    //         }
+    //     }
+
+    //     return res;
+    // }
+
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int n = candies.size();
+        vector<bool> res;
+        int maxi = *max_element(candies.begin(),candies.end());
+
+        for(int i=0;i<n;i++)
+        {
+            if(maxi > candies[i] + extraCandies)
+            {
+                res.push_back(false);
+            }
+            else
+            {
+                res.push_back(true);
+            }
+        }
+
+        return res;
+    }
+};
