f5d49e6541afe2f56323092e3ff550c89862ff29
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-16 01:25:45 +0530

    Update 2703-Return Length of Arguments Passed.js

--- solutions/2703-Return Length of Arguments Passed.js
+++ solutions/2703-Return Length of Arguments Passed.js
@@ -1,1 +1,12 @@
+/**
+ * @return {number}
+ */
 
+//Time Complexity :O(1) && Space Complexity :O(1)
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
