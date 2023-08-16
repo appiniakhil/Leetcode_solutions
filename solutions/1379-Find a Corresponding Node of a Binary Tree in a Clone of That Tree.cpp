1436e4d4d9dae9500f4596f5633297e5d0873f30
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-17 05:10:53 +0530

    Update 1379-Find a Corresponding Node of a Binary Tree in a Clone of That Tree.cpp

--- solutions/1379-Find a Corresponding Node of a Binary Tree in a Clone of That Tree.cpp
+++ solutions/1379-Find a Corresponding Node of a Binary Tree in a Clone of That Tree.cpp
@@ -1,1 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 
+class Solution {
+public:
+    //Time Complexit :O(n) && Space Complexity :O(n)
+    TreeNode* res;
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+        
+        if(cloned == NULL)
+        {
+            return cloned;
+        }
+
+        if(cloned->val == target->val)
+        {
+            res = cloned;
+        }
+
+        getTargetCopy(original,cloned->left,target);
+        getTargetCopy(original,cloned->right,target);
+
+        return res;
+    }
+};
