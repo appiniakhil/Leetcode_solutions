9d539bcc06a2a1d66cce79ace176e1260b80c891
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-17 00:27:35 +0530

    Update 2114-Maximum Number of Words Found in Sentences.cpp

--- solutions/2114-Maximum Number of Words Found in Sentences.cpp
+++ solutions/2114-Maximum Number of Words Found in Sentences.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    //Time Complexity :O(n * n) && Space Complexity :O(n)
+    int mostWordsFound(vector<string>& sentences) {
+        int n = sentences.size();
+        int res = 0;
 
+        for(int i=0;i<n;i++)
+        {
+            string str = "";
+            str += sentences[i];
+
+            int count = 1;
+            for(auto& it: str)
+            {
+                if(it == ' ')
+                {
+                    count++;
+                }
+            }
+
+            res = max(res, count);
+        }
+
+        return res;
+    }
+};
