12f1026c0be40fe58e7fc2887b874b59fd583cff
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-17 04:26:07 +0530

    Update 2160-Minimum Sum of Four Digit Number After Splitting Digits.cpp

--- solutions/2160-Minimum Sum of Four Digit Number After Splitting Digits.cpp
+++ solutions/2160-Minimum Sum of Four Digit Number After Splitting Digits.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    //Time Complexity :O(nlogn) && Space Complexity:O(n)
+    int minimumSum(int num) {
+        vector<int> ans;
+        for(int i=0;i<4;i++)
+        {
+            int a = num%10;
+            ans.push_back(a);
+            num = num/10;
+        }
 
+        sort(ans.begin(),ans.end());
+        int n1 = ans[0]*10 + ans[3];
+        int n2 = ans[1]*10 + ans[2];
+
+        return n1 + n2;
+
+    }
+};
