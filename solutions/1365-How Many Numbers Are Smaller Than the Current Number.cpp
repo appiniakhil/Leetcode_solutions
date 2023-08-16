02a19198d140098c2b75c9facc17be1ec057a725
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-17 01:36:04 +0530

    Update 1365-How Many Numbers Are Smaller Than the Current Number.cpp

--- solutions/1365-How Many Numbers Are Smaller Than the Current Number.cpp
+++ solutions/1365-How Many Numbers Are Smaller Than the Current Number.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
 
+        for(int i=0;i<n;i++)
+        {
+            int cnt = 0;
+            for(int j=0;j<n;j++)
+            {
+                if(nums[i] > nums[j] && j!=i)
+                {
+                    cnt++;
+                }
+            }
+
+            ans.push_back(cnt);
+        }
+
+        return ans;
+    }
+};
