73d2d37261155ca7fd987ffce215002b0307ffab
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-16 23:17:35 +0530

    Update 1678-Goal Parser Interpretation.cpp

--- solutions/1678-Goal Parser Interpretation.cpp
+++ solutions/1678-Goal Parser Interpretation.cpp
@@ -1,1 +1,30 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    string interpret(string command) {
+        int n = command.size();
 
+        string ans ="";
+        int i = 0;
+        while(i<n)
+        {
+            if(command[i] == 'G')
+            {
+                ans += 'G';
+                i++;
+            }
+            else if(command[i] == '(' && command[i+1] == ')')
+            {
+                ans += 'o';
+                i += 2;
+            }
+            else
+            {
+                ans += "al";
+                i += 4;
+            }
+        }
+
+        return ans;
+    }
+};
