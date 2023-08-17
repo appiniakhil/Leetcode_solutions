3a6e13ac6f49e3ba64434723d4383d80c1306f81
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-17 08:09:56 +0530

    Update 1720-Decode XORed Array.cpp

--- solutions/1720-Decode XORed Array.cpp
+++ solutions/1720-Decode XORed Array.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    vector<int> decode(vector<int>& encoded, int first) {
+        int n = encoded.size();
+        vector<int> ans;
 
+        int xorop = first;
+        ans.push_back(xorop);
+
+        for(int i=0;i<n;i++)
+        {
+            xorop = xorop ^ encoded[i];
+            ans.push_back(xorop);
+        }
+
+        return ans;
+    }
+};
