6b9ea1da3df1e8b47f2277b28341bdafc1bcfb1a
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-17 09:30:39 +0530

    Update 1108-Defanging an IP Address.cpp

--- solutions/1108-Defanging an IP Address.cpp
+++ solutions/1108-Defanging an IP Address.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    string defangIPaddr(string address) {
+        int n = address.size();
+        string ans = "";
+        int i = 0;
 
+        while(i<n)
+        {
+            if(address[i] == '.')
+            {
+                ans += "[.]";
+            }
+            else
+            {
+                ans += address[i];
+            }
+            i++;
+        }
+
+        return ans;
+    }
+};
