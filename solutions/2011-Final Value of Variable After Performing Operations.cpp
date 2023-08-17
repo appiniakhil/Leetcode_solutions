b227d39dbedd37866192e5adb0adf45ee8d2b861
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-17 10:09:52 +0530

    Update 2011-Final Value of Variable After Performing Operations.cpp

--- solutions/2011-Final Value of Variable After Performing Operations.cpp
+++ solutions/2011-Final Value of Variable After Performing Operations.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x = 0;
 
+        for(auto& it : operations)
+        {
+            if(it[1] == '-')
+            {
+                x -= 1;
+            }
+            else
+            {
+                x += 1;
+            }
+        }
+
+        return x;
+    }
+};
