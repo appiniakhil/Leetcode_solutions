bb0f1c07d458fdd5e31921fa585d0b1728204795
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-17 09:55:19 +0530

    Update 1470-Shuffle the Array.cpp

--- solutions/1470-Shuffle the Array.cpp
+++ solutions/1470-Shuffle the Array.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    vector<int> shuffle(vector<int>& nums, int n) {
+        int m = nums.size();
+        vector<int> ans;
 
+        int i = 0;
+        int j = n;
+        while(i < n && j < m)
+        {
+            ans.push_back(nums[i]);
+            ans.push_back(nums[j]);
+            i++;
+            j++;
+        }
+        
+        return ans;
+    }
+};
