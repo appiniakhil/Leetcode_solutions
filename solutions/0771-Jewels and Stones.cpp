2fdf4559f37678c4343357150177df13833b67a1
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-17 10:26:24 +0530

    Update 0771-Jewels and Stones.cpp

--- solutions/0771-Jewels and Stones.cpp
+++ solutions/0771-Jewels and Stones.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    //Time Complexity :O(n * m) && Space Complexity :O(1)
+    int numJewelsInStones(string jewels, string stones) {
+        int cnt = 0;
+        int n = jewels.size();
+        int m = stones.size();
 
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(jewels[i] == stones[j])
+                {
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
