c042284c7001e466f3f2d4853b822fc1f8461361
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-17 09:19:17 +0530

    Update 1689-Partitioning Into Minimum Number Of Deci-Binary Numbers.cpp

--- solutions/1689-Partitioning Into Minimum Number Of Deci-Binary Numbers.cpp
+++ solutions/1689-Partitioning Into Minimum Number Of Deci-Binary Numbers.cpp
@@ -1,1 +1,14 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int minPartitions(string n) {
+        char c = '0';
 
+        for(auto &it: n)
+        {
+            c = max(c,it);
+        }
+
+        return c-'0';
+    }
+};
