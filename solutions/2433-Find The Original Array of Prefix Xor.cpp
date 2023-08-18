248f54cbb72bd9fbaafab593549772f3fe7370c5
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-19 03:20:33 +0530

    Update 2433-Find The Original Array of Prefix Xor.cpp

--- solutions/2433-Find The Original Array of Prefix Xor.cpp
+++ solutions/2433-Find The Original Array of Prefix Xor.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
 
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    vector<int> findArray(vector<int>& pref) {
+        int n = pref.size();
+
+        vector<int> ans;
+        int xorop = pref[0];
+        ans.push_back(xorop);
+
+        for(int i=1;i<n;i++)
+        {
+            xorop = pref[i-1]^pref[i];
+            ans.push_back(xorop);
+        }
+
+        return ans;
+    }
+};
