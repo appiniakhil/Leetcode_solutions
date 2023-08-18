a185bd66da43eba5a7a1a8c0947a54807d8ba084
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-19 03:08:47 +0530

    Update 1221-Split a String in Balanced Strings.cpp

--- solutions/1221-Split a String in Balanced Strings.cpp
+++ solutions/1221-Split a String in Balanced Strings.cpp
@@ -1,1 +1,32 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int balancedStringSplit(string s) {
+        int n = s.size();
+        int ans = 0;
+        int cnt1= 0;
+        int cnt2 = 0;
+
+        for(int i=0;i<n;i++)
+        {
+            if(s[i] == 'R')
+            {
+                cnt2++;
+            }
+            else
+            {
+                cnt1++;
+            }
+
+            if(cnt1 == cnt2)
+            {
+                ans++;
+                cnt1=0;
+                cnt2=-0;
+            }
+        }
+
+        return ans;
+    }
+};
 
