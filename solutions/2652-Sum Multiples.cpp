202a3988707ff5ee585e76cc2f1be8ada9768b53
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-19 03:55:21 +0530

    Update 2652-Sum Multiples.cpp

--- solutions/2652-Sum Multiples.cpp
+++ solutions/2652-Sum Multiples.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int sumOfMultiples(int n) {
 
+        int sum = 0;
+        for(int i=1;i<n+1;i++)
+        {
+            if(i%3 == 0 || i%5 ==0 || i%7 == 0)
+            {
+                sum += i;
+            }
+        }
+        
+        return sum;
+    }
+};
