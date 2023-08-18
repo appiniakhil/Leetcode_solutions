40cd437cb36118e03f2a4f79997c4d0890b9f1df
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-19 03:49:57 +0530

    Update 1486-XOR Operation in an Array.cpp

--- solutions/1486-XOR Operation in an Array.cpp
+++ solutions/1486-XOR Operation in an Array.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int xorOperation(int n, int start) {
+        vector<int> nums;
+        for(int i=0;i<n;i++)
+        {
+            nums.push_back(start + 2 * i);
+        }
 
+        int xorop = nums[0];
+        for(int i=1;i<n;i++)
+        {
+            xorop ^= nums[i];
+        }
+
+        return xorop;
+    }
+};
