98d3dac939fb8926095fa0acc6e40c24df1d9661
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-20 02:13:40 +0530

    Update 1769-Minimum Number of Operations to Move All Balls to Each Box.cpp

--- solutions/1769-Minimum Number of Operations to Move All Balls to Each Box.cpp
+++ solutions/1769-Minimum Number of Operations to Move All Balls to Each Box.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    //Time Complecity :O(n * n) && Space Complecity :O(1)
+    vector<int> minOperations(string boxes) {
+        int n = boxes.size();
 
+        vector<int> ans;
+
+        for(int i=0;i<n;i++)
+        {
+            int cnt  = 0;
+            for(int j=0;j<n;j++)
+            {
+                if(boxes[j] == '1')
+                {
+                    cnt += abs(i-j);
+                }
+            }
+            
+            ans.push_back(cnt);
+        }
+
+        return ans;
+    }
+};
