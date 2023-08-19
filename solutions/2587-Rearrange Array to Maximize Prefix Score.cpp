fcaf27c41b1975d3412146bce22b60552a754254
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-20 02:14:30 +0530

    Update 2587-Rearrange Array to Maximize Prefix Score.cpp

--- solutions/2587-Rearrange Array to Maximize Prefix Score.cpp
+++ solutions/2587-Rearrange Array to Maximize Prefix Score.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    //Time Complexity :O(nlogn) && Space Complexity :O(1)
+    int maxScore(vector<int>& nums) {
+        int n = nums.size();
 
+        sort(nums.begin(),nums.end());
+
+        int maxi = 0;
+        long long sum = 0;
+        for(int i=n-1;i>=0;i--)
+        {
+            sum += nums[i];
+            if(sum > 0)
+            {
+                maxi++;
+            }
+        }
+
+        return maxi;
+    }
+};
