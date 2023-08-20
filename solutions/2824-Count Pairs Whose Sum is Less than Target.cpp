c4541ea2039a64e896aef901037323adc60b57f4
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-21 04:14:36 +0530

    Update 2824-Count Pairs Whose Sum is Less than Target.cpp

--- solutions/2824-Count Pairs Whose Sum is Less than Target.cpp
+++ solutions/2824-Count Pairs Whose Sum is Less than Target.cpp
@@ -1,1 +1,45 @@
+class Solution {
+public:
+    //Time Complexity :O(n * n) && Space Complexity :O(1)
+    // int countPairs(vector<int>& nums, int target) {
+    //     int n = nums.size();
+    //     int cnt = 0;
 
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         for(int j=i+1;j<n;j++)
+    //         {
+    //             if(nums[i] + nums[j] < target)
+    //             {
+    //                 cnt++;
+    //             }
+    //         }
+    //     }
+
+    //     return cnt;
+    // }
+
+    //Time Complexity :O(nlogn) && Space Complexity :O(1)
+    int countPairs(vector<int>& nums, int target) {
+        int n = nums.size();
+        int i=0;
+        int j=n-1;
+        int cnt = 0;
+
+        sort(nums.begin(),nums.end());
+        while(i<j)
+        {
+            if(nums[i] + nums[j] < target)
+            {
+                cnt += (j-i);
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+        }
+
+        return cnt;
+    }
+};
