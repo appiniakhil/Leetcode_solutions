3bc1379e20f0863e4162abafa53183226cbab9a8
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-21 00:04:03 +0530

    Update 2161-Partition Array According to Given Pivot.cpp

--- solutions/2161-Partition Array According to Given Pivot.cpp
+++ solutions/2161-Partition Array According to Given Pivot.cpp
@@ -1,1 +1,35 @@
+class Solution {
+public:
 
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        int n = nums.size();
+
+        vector<int> ans;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i] < pivot)
+            {
+                ans.push_back(nums[i]);
+            }
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i] == pivot)
+            {
+                ans.push_back(nums[i]);
+            }
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i] > pivot)
+            {
+                ans.push_back(nums[i]);
+            }
+        }
+
+        return ans;
+    }
+};
