c17ed724f2e941dba880b29166dc4ef5c959b9af
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-21 19:37:08 +0530

    Update 2535-Difference Between Element Sum and Digit Sum of an Array.cpp

--- solutions/2535-Difference Between Element Sum and Digit Sum of an Array.cpp
+++ solutions/2535-Difference Between Element Sum and Digit Sum of an Array.cpp
@@ -1,1 +1,31 @@
+class Solution {
+public:
+    //Time Complexity :O(n * d) && Space Compexity : O(1) where d is the max no of digits in element of nums[i]
+    int differenceOfSum(vector<int>& nums) {
+        int n = nums.size();
+        int eleSum = 0;
 
+        int digSum = 0;
+        for(int i=0;i<n;i++)
+        {
+            eleSum += nums[i];
+
+            if(nums[i] > 9)
+            {
+                while(nums[i] > 0)
+                {
+                    int d = nums[i]%10;
+                    digSum += d;
+                    nums[i] /= 10;
+                }
+            }
+            else
+            {
+                digSum += nums[i];
+            }
+
+        }
+
+        return abs(eleSum - digSum);
+    }
+};
