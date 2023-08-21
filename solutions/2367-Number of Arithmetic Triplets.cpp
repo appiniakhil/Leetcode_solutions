d53c4f69d7e9ae0395b436e24f41f5816406c44b
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-21 23:28:37 +0530

    Update 2367-Number of Arithmetic Triplets.cpp

--- solutions/2367-Number of Arithmetic Triplets.cpp
+++ solutions/2367-Number of Arithmetic Triplets.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    //Time Complecity :O(n) && Space Complexity :O(n)
+    int arithmeticTriplets(vector<int>& nums, int diff) {
+        int n = nums.size();
+        int cnt = 0;
 
+        unordered_map<int, bool> mp;
+
+        for(int i=0;i<n;i++)
+        {
+            mp[nums[i]] = true;
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            if(mp[nums[i]-diff]== true && mp[nums[i] + diff] == true)
+            {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+};
