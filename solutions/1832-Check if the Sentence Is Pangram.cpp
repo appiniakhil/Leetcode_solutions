61594dccbe743ec7f2909ecb4bf3c1970ec985e0
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-21 23:56:29 +0530

    Update 1832-Check if the Sentence Is Pangram.cpp

--- solutions/1832-Check if the Sentence Is Pangram.cpp
+++ solutions/1832-Check if the Sentence Is Pangram.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
 
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    bool checkIfPangram(string sentence) {
+        int hash[26] = {0};
+
+        int cnt = 0;
+        for(auto it: sentence)
+        {
+            if(hash[it - 'a'] == 0)
+            {
+                cnt++;
+                hash[it-'a']++;
+            }
+        }
+
+        if(cnt == 26)
+        {
+            return true;
+        }
+
+        return false;
+    }
+};
