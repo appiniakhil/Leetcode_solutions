e78a370e6e0c299130120fb2e33fd7055be92484
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-21 21:20:48 +0530

    Update 1816-Truncate Sentence.cpp

--- solutions/1816-Truncate Sentence.cpp
+++ solutions/1816-Truncate Sentence.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    string truncateSentence(string s, int k) {
+        int cnt = 0;
+        string ans = "";
 
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i] == ' ')
+            {
+                cnt++;
+            }
+
+            if(cnt == k)
+            {
+                break;
+            }
+
+            ans += s[i];
+        }
+
+        return ans;
+    }
+};
