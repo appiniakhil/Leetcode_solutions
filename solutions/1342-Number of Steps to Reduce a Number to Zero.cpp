032dae929c587cd86497b01845d287e7b3d2c548
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-21 16:39:50 +0530

    Update 1342-Number of Steps to Reduce a Number to Zero.cpp

--- solutions/1342-Number of Steps to Reduce a Number to Zero.cpp
+++ solutions/1342-Number of Steps to Reduce a Number to Zero.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    //Time Complexity O(n) && Space Complexity: O(1)
+    int numberOfSteps(int num) {
+        int cnt = 0;
 
+        while(num > 0)
+        {
+            if(num % 2 == 0)
+            {
+                num = num/2;
+                cnt++;
+            }
+            else
+            {
+                num = num-1;
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+};
