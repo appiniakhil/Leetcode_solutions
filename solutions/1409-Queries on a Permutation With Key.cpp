e526a15e9f367b789feff5ba0c44fc573c445c98
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-21 20:08:47 +0530

    Update 1409-Queries on a Permutation With Key.cpp

--- solutions/1409-Queries on a Permutation With Key.cpp
+++ solutions/1409-Queries on a Permutation With Key.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
 
+    //Time Complexity :O(n * m) && Space Complexity :O(n + m)
+    vector<int> processQueries(vector<int>& queries, int m) {
+        vector<int> P(m,0);
+        vector<int> res;
+
+        for(int i=0;i<m;i++)
+        {
+            P[i] = i+1;
+        }
+        
+        int pos = 0, val;
+        for(int i=0;i<queries.size();i++)
+        {
+            auto it = find(P.begin(), P.end(), queries[i]);
+            pos = it - P.begin();
+            val = P[pos];
+            P.erase(it);
+            P.insert(P.begin(), val);
+            res.push_back(pos);
+        }
+
+        return res;
+    }
+};
