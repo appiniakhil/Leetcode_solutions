462a2cae6e4c7e8d22b598f6778448356f52d8ae
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-21 16:49:59 +0530

    Update 1528-Shuffle String.cpp

--- solutions/1528-Shuffle String.cpp
+++ solutions/1528-Shuffle String.cpp
@@ -1,1 +1,15 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    string restoreString(string s, vector<int>& indices) {
+        int n = s.size();
+        string ans = s;
 
+        for(int i=0;i<n;i++)
+        {
+            ans[indices[i]] = s[i];
+        }
+
+        return ans;
+    }
+};
