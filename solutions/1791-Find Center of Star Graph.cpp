33608d1d1f8d752f5c7d8151cef5a5c98eff0fb2
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-21 21:32:43 +0530

    Update 1791-Find Center of Star Graph.cpp

--- solutions/1791-Find Center of Star Graph.cpp
+++ solutions/1791-Find Center of Star Graph.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
 
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    int findCenter(vector<vector<int>>& edges) {
+        int n = edges.size();
+        vector<int> hash(n+2, 0);
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<2;j++)
+            {
+                hash[edges[i][j]]++;
+            }
+        }
+
+        for(int i=0;i<(n+2);i++)
+        {
+            if(hash[i] > 1)
+            {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+};
