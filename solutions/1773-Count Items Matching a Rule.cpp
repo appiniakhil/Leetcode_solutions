30c4734378930e00b9a3d60e3a09d151afaead04
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-21 19:15:36 +0530

    Update 1773-Count Items Matching a Rule.cpp

--- solutions/1773-Count Items Matching a Rule.cpp
+++ solutions/1773-Count Items Matching a Rule.cpp
@@ -1,1 +1,36 @@
+class Solution {
+public:
 
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        int n = items.size();
+
+        int cnt = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(ruleKey == "color")
+            {
+                if(items[i][1] == ruleValue)
+                {
+                    cnt++;
+                }
+            }
+            else if(ruleKey == "type")
+            {
+                if(items[i][0] == ruleValue)
+                {
+                    cnt++;
+                }
+            }
+            else
+            {
+                if(items[i][2] == ruleValue)
+                {
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
