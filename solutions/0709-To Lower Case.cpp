cb6e920ff9533ba06c21b5d40fd83aaf11d20b23
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-23 00:21:27 +0530

    Update 0709-To Lower Case.cpp

--- solutions/0709-To Lower Case.cpp
+++ solutions/0709-To Lower Case.cpp
@@ -1,1 +1,13 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    string toLowerCase(string s) {
+        string ans ="";
+        for(int i=0;i<s.length();i++)
+        {
+            ans += tolower(s[i]);
+        }
 
+        return ans;
+    }
+};
