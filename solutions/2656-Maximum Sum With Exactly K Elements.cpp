d3b825f26e5ec04590398ea493eb6f58355ce3de
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-23 01:11:40 +0530

    Update 2656-Maximum Sum With Exactly K Elements.cpp

--- solutions/2656-Maximum Sum With Exactly K Elements.cpp
+++ solutions/2656-Maximum Sum With Exactly K Elements.cpp
@@ -1,1 +1,15 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    int maximizeSum(vector<int>& nums, int k) {
+        int maxi = 0;
+        int n = nums.size();
 
+        for(int i =0;i<n;i++)
+        {
+            maxi = max(maxi, nums[i]);
+        }
+
+        return k*(maxi) + (k * (k - 1))/2;
+    }
+};
