e24bca7656f17bcfd76708332b35c4c17f2bb627
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-22 23:09:42 +0530

    Update 2810-Faulty Keyboard.cpp

--- solutions/2810-Faulty Keyboard.cpp
+++ solutions/2810-Faulty Keyboard.cpp
@@ -1,1 +1,54 @@
+class Solution {
+public:
+    //Time Complexity :O(n * n) && Space Complexity :O(n)
+    // string helper(string s)
+    // {
+    //     int n = s.length();
 
+    //     string ans = "";
+    //     for(int i=n-1;i>=0;i--)
+    //     {
+    //         ans += s[i];
+    //     }
+    //     return ans;
+    // }
+
+    // string finalString(string s) {
+    //     int n = s.length();
+
+    //     string ans = "";
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         if(s[i] == 'i')
+    //         {
+    //             ans = helper(ans);
+    //         }
+    //         else
+    //         {
+    //             ans += s[i];
+    //         }
+    //     }
+
+    //     return ans;
+    // }
+
+    //Using builtin function
+    string finalString(string s) {
+        int n = s.length();
+
+        string ans = "";
+        for(int i=0;i<n;i++)
+        {
+            if(s[i] == 'i')
+            {
+                reverse(ans.begin(), ans.end());
+            }
+            else
+            {
+                ans += s[i];
+            }
+        }
+
+        return ans;
+    }
+};
