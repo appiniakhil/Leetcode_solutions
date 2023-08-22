f573512b74dfa8dcc3865ae9fb95de8269d14c52
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-23 00:55:05 +0530

    Update 0804-Unique Morse Code Words.cpp

--- solutions/0804-Unique Morse Code Words.cpp
+++ solutions/0804-Unique Morse Code Words.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
 
+    //Time Complexity :O(n * n) && Space Complexity :O(n)
+    int uniqueMorseRepresentations(vector<string>& words) {
+      	vector<string> a = {".-","-...","-.-.","-..",".","..-.","--.","....",
+                  "..",".---","-.-",".-..","--","-.","---",".--.",
+                  "--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+            unordered_map<string,int> mp;
+
+            for (int i=0; i< words.size(); i++) 
+            {
+                string s;
+                for (int j=0; j<words[i].size(); j++) 
+                {
+                    s += a[words[i][j] - 'a'];
+                }
+
+                mp[s]=1;
+            }
+
+        return mp.size (); 
+    }
+};
