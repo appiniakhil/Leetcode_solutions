a9580684fe39452c58a72dc3f8d796a8457ce869
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-23 02:07:41 +0530

    Update 1732-Find the Highest Altitude.cpp

--- solutions/1732-Find the Highest Altitude.cpp
+++ solutions/1732-Find the Highest Altitude.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
 
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    int largestAltitude(vector<int>& gain) {
+        int n = gain.size();
+        vector<int> alt;
+        alt.push_back(0);
+
+        int sum = 0;
+        int maxi = INT_MIN;
+        for(int i=0;i<n;i++)
+        {
+            sum += gain[i];
+            alt.push_back(sum);
+        }
+
+        for(int i=0;i<n+1;i++)
+        {
+            maxi = max(maxi,alt[i]);
+        }
+
+        return maxi;
+    }
+};
