6735025293b8da5b7b043743261bd14d3c76aad4
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-22 20:33:56 +0530

    Update 1688-Count of Matches in Tournament.cpp

--- solutions/1688-Count of Matches in Tournament.cpp
+++ solutions/1688-Count of Matches in Tournament.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int numberOfMatches(int n) {
+        int cnt = 0;
 
+        while(n > 0)
+        {
+            if(n == 1)
+            {
+                break;
+            }
+
+            if(n %2 == 0)
+            {
+                cnt += n/2;
+                n = n/2;
+            }
+            else
+            {
+                cnt += (n-1)/2;
+                n = (n-1)/2 + 1;
+            }
+        }
+
+        return cnt;
+    }
+};
