0c895183254ca3e4c2c5bae8e87b7748d956b41d
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-23 19:21:28 +0530

    Update 1534-Count Good Triplets.cpp

--- solutions/1534-Count Good Triplets.cpp
+++ solutions/1534-Count Good Triplets.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
 
+    //Time Complexity :O(n * n * n) && Space Complexity :O(1)
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        int n = arr.size();
+
+        int cnt = 0;
+        for(int i=0;i<n-2;i++)
+        {
+            for(int j=i+1;j<n-1;j++)
+            {
+                for(int k=j+1;k<n;k++)
+                {
+                    if(abs(arr[i] - arr[j]) <= a && abs(arr[j] - arr[k]) <=b && abs(arr[i] - arr[k]) <= c)
+                    {
+                        cnt++;
+                    }
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
