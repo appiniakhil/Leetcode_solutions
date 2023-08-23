3b37832bae42f5ccdc2f0e05d90d88559285ae1b
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-23 19:34:07 +0530

    Update 1913-Maximum Product Difference Between Two Pairs.cpp

--- solutions/1913-Maximum Product Difference Between Two Pairs.cpp
+++ solutions/1913-Maximum Product Difference Between Two Pairs.cpp
@@ -1,1 +1,10 @@
+class Solution {
+public:
+    //Time Complexity :O(nlogn) && Space Complexity :O(1)
+    int maxProductDifference(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
 
+        return nums[n-1] * nums[n-2] - nums[0] * nums[1];
+    }
+};
