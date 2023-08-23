a57d15ca51d945a481157bdafbb70db8eb0f105f
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-23 19:22:38 +0530

    Update 2103-Rings and Rods.cpp

--- solutions/2103-Rings and Rods.cpp
+++ solutions/2103-Rings and Rods.cpp
@@ -1,1 +1,43 @@
+class Solution {
+public:
+    //Time Complexity :O(nlogn) && Space Complexity :O(n)
+    int countPoints(string rings) {
+        map<int, string> mp;
+        int ans = 0;
 
+        for(int i=0;i<rings.size();i++)
+        {
+            mp[rings[i+1] - '0'] += rings[i];
+        }
+
+        for(int i=0;i<10;i++)
+        {
+            int R=0,G=0,B=0;
+
+            string ch = mp[i];
+
+            for(int i=0;i<ch.size();i++)
+            {
+                if(ch[i] == 'R')
+                {
+                    R=1;
+                }
+                if(ch[i] == 'G')
+                {
+                    G=1;
+                }
+                if(ch[i] == 'B')
+                {
+                    B=1;
+                }
+            }
+
+            if(R==1 && G==1 && B==1)
+            {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+};
