fa7cd78e5d8027a1f5a0600e068a14fd95986909
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-23 19:58:06 +0530

    Update 2744-Find Maximum Number of String Pairs.cpp

--- solutions/2744-Find Maximum Number of String Pairs.cpp
+++ solutions/2744-Find Maximum Number of String Pairs.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    //Time Complexity :O(n * n) && Space Complexity :O(1)
+    int maximumNumberOfStringPairs(vector<string>& words) {
+        int cnt = 0;
+        for(int i=0;i<words.size();i++)
+        {
+            for(int j=i+1;j<words.size();j++)
+            {
+                if(words[i][0] == words[j][1] && words[i][1] == words[j][0])
+                {
+                    cnt++;
+                }
+            }
+        }
 
+        return cnt;
+    }
+};
