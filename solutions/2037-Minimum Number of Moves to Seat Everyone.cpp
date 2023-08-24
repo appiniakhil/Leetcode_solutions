0fc2437c8f0c90e1d078e82143afa998a9ddc04d
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-25 01:45:28 +0530

    Update 2037-Minimum Number of Moves to Seat Everyone.cpp

--- solutions/2037-Minimum Number of Moves to Seat Everyone.cpp
+++ solutions/2037-Minimum Number of Moves to Seat Everyone.cpp
@@ -1,1 +1,16 @@
+class Solution {
+public:
+    //Time Complexity :O(nlogn) && Space Complexity :O(1)
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        sort(seats.begin(), seats.end());
+        sort(students.begin(), students.end());
 
+        int mov = 0;
+        for(int i=0;i<seats.size();i++)
+        {
+            mov += abs(students[i] - seats[i]);
+        }
+
+        return mov;
+    }
+};
