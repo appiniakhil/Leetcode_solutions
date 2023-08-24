209fab8f0da81fdb118b5f9722dc11c8ab169542
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-25 02:16:19 +0530

    Update 1323-Maximum 69 Number.cpp

--- solutions/1323-Maximum 69 Number.cpp
+++ solutions/1323-Maximum 69 Number.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
 
+    //Time Complexity :O(n + logn) && Space Complexity :O(logn)
+    int maximum69Number (int num) {
+        string s = to_string(num); //Takes O(logn)
+        int n = s.size();
+        
+        for(int i=0;i<n;i++)
+        {
+            if(s[i] - '0' == 6)
+            {
+                s[i] = '9';
+                
+                return stoi(s);
+            }
+        }
+        
+        return stoi(s);
+    }
+};
