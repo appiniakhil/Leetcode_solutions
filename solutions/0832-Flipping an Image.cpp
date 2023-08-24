da1ef24dfdcac6cb82dcf1f030871a5e3995481b
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-25 01:04:35 +0530

    Update 0832-Flipping an Image.cpp

--- solutions/0832-Flipping an Image.cpp
+++ solutions/0832-Flipping an Image.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    //Time Complexity :O(n * m) && Space Complexity :O(1)
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+        vector<vector<int>> row;
+        int n = image.size();
+        int m = image[0].size();
 
+        for(int i=0;i<n;i++)
+        {
+            reverse(image[i].begin(), image[i].end());
+            for(int j=0;j<m;j++)
+            {
+                if(image[i][j] == 0)
+                {
+                    image[i][j] = 1;
+                }
+                else
+                {
+                    image[i][j] = 0;
+                }
+            }
+        }
+
+        return image;
+    }
+};
