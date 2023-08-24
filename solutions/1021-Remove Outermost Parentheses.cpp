217845153cc846bdde2ef6fb08f8d5ea7aa724ab
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-25 01:29:02 +0530

    Update 1021-Remove Outermost Parentheses.cpp

--- solutions/1021-Remove Outermost Parentheses.cpp
+++ solutions/1021-Remove Outermost Parentheses.cpp
@@ -1,1 +1,33 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    string removeOuterParentheses(string s) {
+        
+        string ans = "";
+        int i=0;
+        int cnt = 0;
+        for(char i: s)
+        {
+            if(i == '(' && cnt == 0)
+            {
+                cnt++;
+            }
+            else if(i == '(' && cnt >= 1)
+            {
+                ans += i;
+                cnt++;
+            }
+            else if(i == ')' && cnt > 1)
+            {
+                ans += i;
+                cnt--;
+            }
+            else if(i == ')' && cnt == 1)
+            {
+                cnt--;
+            }
+        }
 
+        return ans;
+    }
+};
