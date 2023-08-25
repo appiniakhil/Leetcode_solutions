23a8f8ea5107aea9a0fb84a86c63e4ce8b323112
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-26 02:20:11 +0530

    Update 0031-Next Permutation.cpp

--- solutions/0031-Next Permutation.cpp
+++ solutions/0031-Next Permutation.cpp
@@ -1,1 +1,35 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int ind = -1;
+        int mini =INT_MAX;
 
+        for(int i=n-2;i>=0;i--)
+        {
+            if(nums[i] < nums[i+1])
+            {
+                ind = i;
+                break;
+            }
+        }
+
+        if(ind == -1)
+        {
+            reverse(nums.begin(),nums.end());
+            return ;
+        }
+
+        for(int i=n-1;i>ind;i--)
+        {
+            if(nums[i] > nums[ind])
+            {
+                swap(nums[i],nums[ind]);
+                break;
+            }
+        }
+
+        reverse(nums.begin() + ind + 1, nums.end());
+    }
+};
