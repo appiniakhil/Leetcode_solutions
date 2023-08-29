8317f0b1554f88fc283a8b3e419932156cf0da92
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-30 00:57:17 +0530

    Update 0050-Pow(x, n).cpp

--- solutions/0050-Pow(x, n).cpp
+++ solutions/0050-Pow(x, n).cpp
@@ -1,1 +1,35 @@
+class Solution {
+public:
 
+    //Time Complexity :O(log n) && Space Complexity :O(1)
+    double myPow(double x, int n) {
+        double ans = 1.0;
+        long long a = n;
+
+        if(a < 0)
+        {
+            a = -1 * a;
+        }
+
+        while(a)
+        {
+            if(a % 2)
+            {
+                ans *= x;
+                a--;
+            }
+            else
+            {
+                x *= x;
+                a /= 2;
+            }
+        }
+
+        if(n < 0)
+        {
+            ans = (double)(1.0) / (double)(ans);
+        }
+
+        return ans;
+    }
+};
