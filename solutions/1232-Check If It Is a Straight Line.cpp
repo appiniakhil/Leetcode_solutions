321d7eaff88ec951f35761d6c3686566068a0c37
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-08-30 19:41:03 +0530

    Update 1232-Check If It Is a Straight Line.cpp

--- solutions/1232-Check If It Is a Straight Line.cpp
+++ solutions/1232-Check If It Is a Straight Line.cpp
@@ -1,1 +1,18 @@
-1
+class Solution {
+public:
+
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+        int n = coordinates.size();
+
+        for(int i=0;i<n-2;i++)
+        {
+            if((coordinates[i+2][1] - coordinates[i+1][1]) * (coordinates[i+1][0] - coordinates[i][0])  != (coordinates[i+1][1] - coordinates[i][1]) * (coordinates[i+2][0] - coordinates[i+1][0]))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
