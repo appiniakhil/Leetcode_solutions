e683faa7bb7ca2a876c60ba8ae53765d36dbc5ac
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-02 01:43:16 +0530

    Update 0977-Squares of a Sorted Array.cpp

--- solutions/0977-Squares of a Sorted Array.cpp
+++ solutions/0977-Squares of a Sorted Array.cpp
@@ -1,1 +1,33 @@
+class Solution {
+public:
+    //Time Complexitu : O(nlogn) && Space Complexity :O(n)
+    // vector<int> sortedSquares(vector<int>& nums) {
+    //     int n = nums.size();
 
+    //     vector<int> ans;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         ans.push_back(nums[i] * nums[i]);
+    //     }
+
+    //     sort(ans.begin(),ans.end());
+
+    //     return ans;
+    // }
+
+    //Time Complexitu : O(nlogn) && Space Complexity :O(n)
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+
+        
+        for(int i=0;i<n;i++)
+        {
+            int temp = nums[i] * nums[i];
+            nums[i] = temp;
+        }
+
+        sort(nums.begin(),nums.end());
+
+        return nums;
+    }
+};
