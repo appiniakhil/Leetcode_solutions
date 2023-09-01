d26d223e096d9f1098505dc3b21fd2b1909e3c4a
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-01 16:25:04 +0530

    Update 0876- Middle of the Linked List.cpp

--- solutions/0876- Middle of the Linked List.cpp
+++ solutions/0876- Middle of the Linked List.cpp
@@ -1,1 +1,75 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getLength(ListNode* head)
+    {
+        int len = 0;
 
+        while(head != NULL)
+        {
+            head = head->next;
+            len++;
+        }
+
+        return len;
+    }
+
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    // ListNode* middleNode(ListNode* head) {
+    //     int n = getLength(head);
+
+    //     int res = n/2;
+
+    //     ListNode* temp = head;
+    //     int cnt = 0;
+
+    //     while(cnt < res)
+    //     {
+    //         temp = temp->next;
+    //         cnt++;
+    //     }
+
+    //     return temp;
+    // }
+
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    ListNode* middleNode(ListNode* head) {
+
+        //if len = 0 or 1
+        if(head == NULL || head->next == NULL)
+        {
+            return head;
+        }
+
+        //if len = 2;
+        if(head->next->next == NULL)
+        {
+            return head->next;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while(fast != NULL)
+        {
+            fast = fast->next;
+            if(fast != NULL)
+            {
+                fast = fast->next;
+            }
+
+            slow = slow->next;
+        }
+
+        return slow;
+    }
+};
