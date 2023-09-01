c704df0bc01db4c5ee4102110f8d41152ec52759
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-02 03:20:15 +0530

    Update 0237-Delete Node in a Linked List.cpp

--- solutions/0237-Delete Node in a Linked List.cpp
+++ solutions/0237-Delete Node in a Linked List.cpp
@@ -1,1 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
 
+    //Time Complexity :O(1) && Space Complexity :O(1)
+    void deleteNode(ListNode* node) {
+
+        node->val = node->next->val; 
+        node->next = node->next->next;
+    }
+};
