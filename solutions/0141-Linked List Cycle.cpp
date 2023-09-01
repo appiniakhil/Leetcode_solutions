c090d7daca6818803105580b83ef89f185fc61bb
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-01 23:10:30 +0530

    Update 0141-Linked List Cycle.cpp

--- solutions/0141-Linked List Cycle.cpp
+++ solutions/0141-Linked List Cycle.cpp
@@ -1,1 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
 
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    bool hasCycle(ListNode *head) {
+        if(head == NULL)
+        {
+            return false;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
