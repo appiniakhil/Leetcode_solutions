66a83b84a21949587072ee140266e2fc5232a5aa
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-02 01:32:11 +0530

    Update 0338-Counting Bits.cpp

--- solutions/0338-Counting Bits.cpp
+++ solutions/0338-Counting Bits.cpp
@@ -1,1 +1,15 @@
+class Solution {
+public:
 
+    //Time Complexity: O(n) && Space Complexity :O(n)
+    vector<int> countBits(int n) {
+        vector<int> ans(n + 1, 0);
+
+        for (int i = 1; i <= n; ++i) 
+        {
+            ans[i] = ans[i >> 1] + (i & 1);
+        }
+
+        return ans;
+    }
+};
