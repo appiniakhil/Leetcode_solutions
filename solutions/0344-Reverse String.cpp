ae983191257c71e6b1f55fd17e3b64241db7b15f
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-03 19:35:58 +0530

    Update 0344-Reverse String.cpp

--- solutions/0344-Reverse String.cpp
+++ solutions/0344-Reverse String.cpp
@@ -1,1 +1,31 @@
+class Solution {
+public:
+    //Builtin function
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    // void reverseString(vector<char>& s) {
+    //     reverse(s.begin(),s.end());
+    // }
 
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    // void reverseString(vector<char>& s) {
+    //     int n = s.size();
+        
+    //     for(int i=0;i<n/2;i++)
+    //     {
+    //         swap(s[i],s[n-i-1]);
+    //     }
+    // }
+
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        vector<char> ans;
+
+        for(int i=0;i<n;i++)
+        {
+            ans.push_back(s[n-i-1]);
+        }
+
+        s = ans;
+    }
+};
