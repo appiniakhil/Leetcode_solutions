29b8d43a72484c16517fce9c25a87e3e8a6b586e
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-04 10:31:18 +0530

    Update 0258-Add Digits.cpp

--- solutions/0258-Add Digits.cpp
+++ solutions/0258-Add Digits.cpp
@@ -1,1 +1,32 @@
+class Solution {
+public:
+    //Time Complexity :O(d) && Space Complexity :O(d) where d is the no of digits in num
+    int helper(int num)
+    {
+        int sum = 0;
+        while(num > 0)
+        {
+            int dig = num%10;
+            sum += dig;
+            num /= 10;
+        }
 
+        return sum;
+    }
+
+    int addDigits(int num) {
+        if(num <=9)
+        {
+            return num;
+        }
+
+        int sum = helper(num);
+        while(sum>9)
+        {
+            int s = helper(sum);
+            sum = s;
+        }
+
+        return sum;
+    }
+};
