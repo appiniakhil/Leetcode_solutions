a13ecb073c0dd1f166621d666a794310e8d396a7
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-06 03:07:37 +0530

    Update 0009-Palindrome Number.cpp

--- solutions/0009-Palindrome Number.cpp
+++ solutions/0009-Palindrome Number.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    //Time Complexity :O(logn) && Space Complexity :O(1)
+    int reverse(int x)
+    {
+        long rev = 0;
+        while(x > 0)
+        {
+            int dig = x%10;
+            rev = rev*10 + dig;
+            x = x/10;
+        }
 
+        return (int)rev;
+    }
+
+    bool isPalindrome(int x) {
+        if(x<0)
+        {
+            return false;
+        }
+        
+        return (reverse(x) == x);
+    }
+};
