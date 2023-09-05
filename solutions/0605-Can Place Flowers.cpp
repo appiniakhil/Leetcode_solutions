ec7ce888f40a723ad759242c99367b8f40cb49aa
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-05 18:57:45 +0530

    Update 0605-Can Place Flowers.cpp

--- solutions/0605-Can Place Flowers.cpp
+++ solutions/0605-Can Place Flowers.cpp
@@ -1,1 +1,33 @@
+class Solution {
+public:
 
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int s = flowerbed.size();
+        int cnt = 0;
+        int i = 0;
+
+        while(i< s)
+        {
+            if(flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && flowerbed[i] == 0 && (i == s-1 || flowerbed[i+1] == 0))
+            {
+                flowerbed[i] = 1;
+                cnt++;
+            }
+
+            if(cnt >= n)
+            {
+                return true;
+            }
+
+            i++;
+        }
+
+        if(cnt >= n)
+        {
+            return true;
+        }
+
+        return false;
+    }
+};
