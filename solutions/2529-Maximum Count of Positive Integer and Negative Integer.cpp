3e78b9ad22fc4c2df9ebb9f83cba4432a1ef346e
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-08 02:00:45 +0530

    Update 2529-Maximum Count of Positive Integer and Negative Integer.cpp

--- solutions/2529-Maximum Count of Positive Integer and Negative Integer.cpp
+++ solutions/2529-Maximum Count of Positive Integer and Negative Integer.cpp
@@ -1,1 +1,80 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    // int maximumCount(vector<int>& nums) {
+    //     int n = nums.size();
 
+    //     int pos = 0;
+    //     int neg = 0;
+
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         if(nums[i] > 0)
+    //         {
+    //             pos++;
+    //         }
+    //         else if(nums[i] < 0)
+    //         {
+    //             neg++;
+    //         }
+    //     }
+
+    //     return max(pos, neg);
+    // }
+
+    //Time Complexity :O(2logn) && Space Complexity :O(1)
+    int binaryneg(vector<int>& nums)
+    {
+        int n = nums.size();
+        int l = 0, h = n-1;
+
+        int neg = 0;
+        while(l <= h)
+        {
+            int mid = l + (h-l)/2;
+            if(nums[mid] < 0)
+            {
+                neg = mid+1;
+                l = mid+1;
+            }
+            else
+            {
+                h = mid-1;
+            }
+        }
+
+        return neg;
+    }
+
+    int binarypos(vector<int>& nums)
+    {
+        int n = nums.size();
+        int l = 0, h = n-1;
+
+        int pos = 0;
+        while(l <= h)
+        {
+            int mid = l + (h-l)/2;
+            if(nums[mid] > 0)
+            {
+                pos = n - mid;
+                h = mid-1;
+            }
+            else
+            {
+                l = mid+1;
+            }
+        }
+
+        return pos;
+    }
+
+    int maximumCount(vector<int>& nums) {
+        int n = nums.size();
+
+        int neg = binaryneg(nums);
+        int pos = binarypos(nums);
+
+        return max(pos, neg);
+    }
+};
