194e40c4ee5b7e88269d2db2a2d329810d2df3df
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-08 03:04:40 +0530

    Update 0014-Longest Common Prefix.cpp

--- solutions/0014-Longest Common Prefix.cpp
+++ solutions/0014-Longest Common Prefix.cpp
@@ -1,1 +1,31 @@
+class Solution {
+public:
 
+    //Time Complexity :O(m * n) && Space Complecity :O(1) where n is the number of strings and m is the length of the longest string in the array
+    string longestCommonPrefix(vector<string>& strs) {
+
+        if (strs.empty()) 
+        {
+            return ""; 
+        }
+
+        string prefix = strs[0];
+
+        for (int i = 1; i < strs.size(); i++) 
+        {
+            int j = 0;
+            while (j < prefix.length() && j < strs[i].length() && prefix[j] == strs[i][j]) 
+            {
+                j++;
+            }
+
+            prefix = prefix.substr(0, j);
+
+            if (prefix.empty()) {
+                return "";
+            }
+        }
+
+        return prefix;
+    }
+};
