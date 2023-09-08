00a6ad51d8b0710b26f6b3a9db0d5a3ad7780634
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-09 01:03:43 +0530

    Update 0153-Find Minimum in Rotated Sorted Array.cpp

--- solutions/0153-Find Minimum in Rotated Sorted Array.cpp
+++ solutions/0153-Find Minimum in Rotated Sorted Array.cpp
@@ -1,1 +1,45 @@
+class Solution {
+public:
 
+    //Time Complexity :O(n) && Space Complexity :O(1) 
+    // int findMin(vector<int>& nums) {
+    //     int n = nums.size();
+
+    //     int mini = INT_MAX;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         mini = min(nums[i], mini);
+    //     }
+
+    //     return mini;
+    // }
+
+    //Time Complexity :O(logn) && Space Complexity :O(1)
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+
+        int low = 0, high = n-1;
+
+        int ans = INT_MAX;
+
+        while(low <= high)
+        {
+            int mid = low + (high - low)/2;
+
+            //Left Sorted
+            if(nums[low] <= nums[mid])
+            {
+                ans = min(ans, nums[low]);
+                low = mid+1;
+            }
+            //Right Sorted
+            else
+            {
+                high = mid-1;
+                ans = min(ans, nums[mid]);
+            }
+        }
+
+        return ans;
+    }
+};
