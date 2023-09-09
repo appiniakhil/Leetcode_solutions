2ede15b96b54ea71c77d09bdbef7548e887bf65f
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-09 18:03:20 +0530

    Update 0058-Length of Last Word.cpp

--- solutions/0058-Length of Last Word.cpp
+++ solutions/0058-Length of Last Word.cpp
@@ -1,1 +1,31 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    int lengthOfLastWord(string s) {
+        int n = s.size();
 
+        string ans ="";
+        int i = 0;
+
+        for(int j=0;j<n;j++)
+        {
+            if(s[n-j-1] != ' ')
+            {
+                break;
+            }
+            i++;
+        }
+        
+        while(i < n)
+        {
+            if(s[n-i-1] == ' ')
+            {
+                break;
+            }
+            ans += s[n-i-1]; 
+            i++;
+        }
+
+        return ans.size();
+    }
+};
