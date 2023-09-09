8f5ae00f85cb6b3b6c04c4ad562cb9918b03ddde
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-09 20:27:41 +0530

    Update 0054-Spiral Matrix.cpp

--- solutions/0054-Spiral Matrix.cpp
+++ solutions/0054-Spiral Matrix.cpp
@@ -1,1 +1,52 @@
+class Solution {
+public:
 
+    //Time Complexity :O(m*n) && Space Complexity :O(1)
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<int> ans;
+        if(matrix.empty() || matrix[0].empty())
+        {
+            return ans;
+        }
+
+        int top = 0,bottom = m-1,left = 0,right = n-1;
+        while(top <= bottom && left <= right)
+        {
+            //for top row elements in our example 1,2,3
+            for(int i=left;i<=right;i++) 
+            {
+                ans.push_back(matrix[top][i]);
+            }
+            top++;
+
+            //from top row last element to last row last element ,in our example 6,9
+            for(int i=top;i<=bottom;i++) 
+            {
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+
+            if (top <= bottom && left <= right) 
+            {
+                // Traverse from right to left , in our example 9-7 && 4-5
+                for (int i=right; i >= left; --i) 
+                {
+                    ans.push_back(matrix[bottom][i]);
+                }
+                bottom--;
+
+                // Traverse from bottom to top, in our example 7-4
+                for (int i=bottom; i >= top; --i) 
+                {
+                    ans.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        return ans;
+    }
+};
