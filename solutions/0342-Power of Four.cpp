4c056ce40c7e1b17d8492ddeb9962e68d8f657d5
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-11 00:11:05 +0530

    Update 0342-Power of Four.cpp

--- solutions/0342-Power of Four.cpp
+++ solutions/0342-Power of Four.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
+    //Time Complexity :O(logn base to 4) && Space Complexity :O(1)
+    bool isPowerOfFour(int n) {
+        if(n <= 0) 
+        {
+            return false; 
+        }
 
+        while (n % 4 == 0) 
+        {
+            n /= 4;
+        }
+        
+        return n == 1;
+    }
+};
