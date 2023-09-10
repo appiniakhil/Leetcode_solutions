9bbf97e26e44c02fb714478ac91911c7283428d3
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-11 01:45:39 +0530

    Update 0387-First Unique Character in a String.cpp

--- solutions/0387-First Unique Character in a String.cpp
+++ solutions/0387-First Unique Character in a String.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int firstUniqChar(string s) {
+        unordered_map<char, int> freq;
 
+        for(char c : s) 
+        {
+            freq[c]++;
+        }
+
+        for(int i=0; i < s.length(); i++) 
+        {
+            if (freq[s[i]] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
