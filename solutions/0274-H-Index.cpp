3ad92cb6e9ffe6de18f428f769eeb877aa65add5
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-10 23:14:32 +0530

    Update 0274-H-Index.cpp

--- solutions/0274-H-Index.cpp
+++ solutions/0274-H-Index.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        //Sorts in descending order
+        sort(citations.rbegin(), citations.rend());
 
+        // reverse(citations.begin(), citations.end());
+        int ans = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(citations[i] >= i+1)
+            {
+                ans = i+1;
+            }
+            else
+            {
+                break;
+            }
+        }
+        
+        return ans;
+    }
+    +
+};
