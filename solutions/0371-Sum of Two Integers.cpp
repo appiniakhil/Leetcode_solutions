55edc7985d55b0b9d9f846fa0a62cb63dfa3388a
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-11 01:21:22 +0530

    Update 0371-Sum of Two Integers.cpp

--- solutions/0371-Sum of Two Integers.cpp
+++ solutions/0371-Sum of Two Integers.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    //Time Complexity: O(1) && Space Complexity :O(1)
+    int getSum(int a, int b) {
+        while (b != 0) {
+            // Calculate the carry
+            int carry = a & b;
+            
+            // Sum of bits without considering carry
+            a = a ^ b;
+            
+            // Carry is shifted by one so that adding it to a gives the correct sum
+            b = (unsigned int)(carry) << 1;
+        }
+        
+        return a;  
+    }
+};
