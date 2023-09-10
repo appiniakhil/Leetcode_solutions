f0e52bb7580d85efd658046d706e5e9034884ff0
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-11 00:04:09 +0530

    Update 0326-Power of Three.cpp

--- solutions/0326-Power of Three.cpp
+++ solutions/0326-Power of Three.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
+    //Time Complexity :O(logn to the base 3) && Space Complexity :O(1)
+    bool isPowerOfThree(int n) {
+        if(n <= 0) 
+        {
+            return false; 
+        }
 
+        while (n % 3 == 0) 
+        {
+            n /= 3;
+        }
+        
+        return n == 1;
+    }
+};
