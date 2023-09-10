b2fbc5ba15db0b8956ebea23b1a36ac1666ed99e
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-11 00:29:57 +0530

    Update 0367-Valid Perfect Square.cpp

--- solutions/0367-Valid Perfect Square.cpp
+++ solutions/0367-Valid Perfect Square.cpp
@@ -1,1 +1,32 @@
+class Solution {
+public:
+    //Time Complexity :O(logn) && Space Complexity :O(1)
+    bool isPerfectSquare(int num) {
+        if(num < 2)
+        {
+            return true;
+        }
 
+        long low = 2,high = num/2;
+        while(low <= high)
+        {
+            long mid = low + (high - low)/2;
+            long square = mid * mid;
+
+            if(num == square)
+            {
+                return true;
+            }
+            else if(square < num)
+            {
+                low = mid+1;
+            }
+            else
+            {
+                high = mid-1;
+            }
+        }
+
+        return false;
+    }
+};
