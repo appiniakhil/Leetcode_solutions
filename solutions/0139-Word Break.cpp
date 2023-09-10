9a4f16978b68cbf5e60f7200262a7cbe53dd93c7
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-10 21:26:10 +0530

    Update 0139-Word Break.cpp

--- solutions/0139-Word Break.cpp
+++ solutions/0139-Word Break.cpp
@@ -1,1 +1,31 @@
+class Solution {
+public:
+    //Time Complexity :O(n*n) && Space Complexity :O(n + w), where 'n' is the length of the input string and 'w' is the number of unique words in wordDict
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.length();
 
+        unordered_set<string> dict;
+
+        for(string word : wordDict)
+        {
+            dict.insert(word);
+        }
+
+        vector<bool> dp(n+1, false);
+        dp[0] = true;
+
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if (dp[j] && dict.find(s.substr(j, i - j)) != dict.end()) 
+                {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+};
