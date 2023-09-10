c1a63c1506b1280990a64eac8697c8dd074c726e
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-10 23:27:43 +0530

    Update 0275-H-Index II.cpp

--- solutions/0275-H-Index II.cpp
+++ solutions/0275-H-Index II.cpp
@@ -1,1 +1,46 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    // int hIndex(vector<int>& citations) {
+    //     int n = citations.size();
 
+    //     reverse(citations.begin(), citations.end());
+
+    //     int ans = 0;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         if(citations[i] >= i+1)
+    //         {
+    //             ans = i+1;
+    //         }
+    //         else
+    //         {
+    //             break;
+    //         }
+    //     }
+
+    //     return ans;
+    // }
+
+    //Time Complexity :O(logn) && Space Complexity :O(1)
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+
+        int low = 0, high = n-1;
+
+        while(low <= high)
+        {
+            int mid = low + (high - low)/2;
+            if(citations[mid] >= n - mid)
+            {
+                high = mid-1;
+            }
+            else
+            {
+                low = mid+1;
+            }
+        }
+
+        return n - low;
+    }
+};
