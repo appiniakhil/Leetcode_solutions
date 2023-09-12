5eb9332d4c32017d89f56648f219d4068c14a649
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-13 00:51:32 +0530

    Update 0069-Sqrt(x).cpp

--- solutions/0069-Sqrt(x).cpp
+++ solutions/0069-Sqrt(x).cpp
@@ -1,1 +1,50 @@
+class Solution {
+public:
+    //Time Complexity : O(n) && Space Complexity :O(1)
+    // int mySqrt(int x) {
+    //     if(x == 1)
+    //     {
+    //         return 1;
+    //     }
 
+    //     int ans = 0;
+
+    //     for(long i=0;i<x;i++)
+    //     {
+    //         if(i * i <= x)
+    //         {
+    //             ans = i;
+    //         }
+    //         else
+    //         {
+    //             break;
+    //         }
+    //     }
+
+    //     return ans;
+    // }
+
+    //Time Complexity : O(logn) && Space Complexity :O(1)
+    int mySqrt(int x) {
+        int low = 0,high=x;
+        
+        while(low <= high)
+        {
+            long mid = low + (high-low)/2;
+            if(mid * mid == x)
+            {
+                return mid;
+            }
+            else if(mid * mid > x)
+            {
+                high = mid-1;
+            }
+            else
+            {
+                low = mid+1;
+            }
+        }
+        
+        return high;
+    }
+};
