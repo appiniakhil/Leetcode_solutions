042fc455a3f16553b0dc596704f0184b6a2e09e7
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-13 02:49:26 +0530

    Update 0875-Koko Eating Bananas.cpp

--- solutions/0875-Koko Eating Bananas.cpp
+++ solutions/0875-Koko Eating Bananas.cpp
@@ -1,1 +1,36 @@
+class Solution {
+public:
+    //Time Complexity :O(logn) && Space Complexity :O(1)
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int maxiEle = INT_MIN;
+        int n = piles.size();
 
+        for(int i=0;i<n;i++)
+        {
+            maxiEle = max(maxiEle, piles[i]);
+        }
+
+        int low = 1, high = maxiEle;
+        while(low <= high)
+        {
+            int mid = low + (high-low)/2;
+            long long total_hours = 0;
+
+            for (int pile : piles) 
+            {
+                total_hours += (pile + mid - 1) / mid;  
+            }
+
+            if(total_hours <= h)
+            {
+                high = mid-1;
+            }
+            else
+            {
+                low = mid+1;
+            }
+        }
+
+        return low;
+    }
+};
