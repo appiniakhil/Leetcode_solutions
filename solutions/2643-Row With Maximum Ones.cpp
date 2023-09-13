05c5156e1f3982730a5cd3d14e9f0cb817cd14d4
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-14 03:16:33 +0530

    Update 2643-Row With Maximum Ones.cpp

--- solutions/2643-Row With Maximum Ones.cpp
+++ solutions/2643-Row With Maximum Ones.cpp
@@ -1,1 +1,33 @@
+class Solution {
+public:
 
+    //Time Complexxity :O(n * m) && Space Complexity :O(1)
+    vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+
+        int max_cnt = -1,ind=-1;
+        int n = mat.size();
+        int m = mat[0].size();
+
+        for(int i=0;i<n;i++)
+        {
+            int cnt_row = 0;
+            for(int j=0;j<m;j++)
+            {
+                cnt_row += mat[i][j];
+            }
+
+            if(cnt_row > max_cnt)
+            {
+                max_cnt = cnt_row;
+                ind = i;
+            }
+        }
+
+        vector<int> ans;
+
+        ans.push_back(ind);
+        ans.push_back(max_cnt);
+
+        return ans;
+    }
+};
