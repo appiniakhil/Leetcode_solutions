2c53899a21b2876a384a4874f55d37b41aa00c62
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-13 20:24:24 +0530

    Update 1037-Valid Boomerang.cpp

--- solutions/1037-Valid Boomerang.cpp
+++ solutions/1037-Valid Boomerang.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //Time Complexity :O(1) && Space Complexity :O(1)
     bool isBoomerang(vector<vector<int>>& points) {
 
         if((points[2][1] - points[1][1]) * (points[1][0] - points[0][0]) == (points[1][1] - points[0][1]) * (points[2][0] - points[1][0]))
