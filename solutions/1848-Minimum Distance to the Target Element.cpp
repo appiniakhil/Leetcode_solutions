f54fa0a7ba02528a7cb31fcfd60b9f0779b3460e
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-14 02:27:05 +0530

    Update 1848-Minimum Distance to the Target Element.cpp

--- solutions/1848-Minimum Distance to the Target Element.cpp
+++ solutions/1848-Minimum Distance to the Target Element.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        int n = nums.size();
 
+        int mini = INT_MAX;
+
+        for(int i=0;i<n;i++)
+        {
+          if(nums[i] == target)
+          {
+            mini = min(mini, abs(i - start));
+          }
+        }
+
+        return mini;
+    }
+};
