3c3f14190df3b98a38d983e465fb6df9f3b7ccf4
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-14 22:14:41 +0530

    Update 0728-Self Dividing Numbers.cpp

--- solutions/0728-Self Dividing Numbers.cpp
+++ solutions/0728-Self Dividing Numbers.cpp
@@ -1,1 +1,38 @@
+class Solution {
+public:
+    //Time Complexity :O(num * log(right)) && Space Complexity :O(1)
+    bool helper(int num)
+    {
+        int temp = num;
+        while(temp > 0)
+        {
+            int dig = temp%10;
+            if(dig == 0)
+            {
+                return false;
+            }
 
+            if(num % dig != 0)
+            {
+                return false;
+            }
+            temp = temp/10;
+        }
+
+        return true;
+    }
+
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> ans;
+
+        for(int i=left;i<=right;i++)
+        {
+            if(helper(i))
+            {
+                ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+};
