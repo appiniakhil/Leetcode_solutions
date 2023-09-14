0fa49708b194ed63624c10053b3be7fba20eb580
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-15 00:39:22 +0530

    Update 1835-Find XOR Sum of All Pairs Bitwise AND.cpp

--- solutions/1835-Find XOR Sum of All Pairs Bitwise AND.cpp
+++ solutions/1835-Find XOR Sum of All Pairs Bitwise AND.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    //Time Complexity :O(n + m) && Space Complexity :O(1)
+    int getXORSum(vector<int>& arr1, vector<int>& arr2) {
+        int n = arr1.size(), m = arr2.size();
+        
+        int xorop1 = 0, xorop2= 0;
 
+        for(auto i : arr1)
+        {
+            xorop1 ^= i;
+        }
+
+        for(auto i : arr2)
+        {
+            xorop2 ^= i;
+        }
+
+        return xorop1 & xorop2;
+    }
+};
