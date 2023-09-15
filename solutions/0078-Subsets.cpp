8b1e1e215037e28d02699bb2d444aa3c8f22a11a
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-15 22:17:37 +0530

    Update 0078-Subsets.cpp

--- solutions/0078-Subsets.cpp
+++ solutions/0078-Subsets.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    //Time Complexity :O(2 ^ n) && Space Complexity :O(2 ^ n + n)
+    void helper(vector<int>& nums, int ind, vector<int>& curr,vector<vector<int>>& res)
+    {
+        int n = nums.size();
 
+        res.push_back(curr);
+
+        for(int i=ind;i<n;i++)
+        {
+            curr.push_back(nums[i]);
+            helper(nums,i+1,curr,res);
+            curr.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> curr;
+        vector<vector<int>> ans;
+
+        helper(nums,0,curr,ans);
+
+        return ans;
+    }
+};
