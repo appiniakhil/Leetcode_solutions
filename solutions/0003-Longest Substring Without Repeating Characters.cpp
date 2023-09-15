7ce3ef7e67c1bed2bc9cfa2e9637e68d5bd8274e
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-15 19:36:38 +0530

    Update 0003-Longest Substring Without Repeating Characters.cpp

--- solutions/0003-Longest Substring Without Repeating Characters.cpp
+++ solutions/0003-Longest Substring Without Repeating Characters.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        vector<int> mp(256, -1);
 
+        int left = 0,right = 0;
+        int ans = 0;
+
+        while(right < n)
+        {
+            if(mp[s[right]] != -1)
+            {
+                left = max(mp[s[right]] + 1, left);
+            }
+
+            mp[s[right]] = right;
+
+            ans = max(ans, right-left+1);
+            right++;
+        }
+
+        return ans;
+    }
+};
