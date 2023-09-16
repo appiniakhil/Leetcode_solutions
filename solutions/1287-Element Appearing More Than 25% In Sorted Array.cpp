b5903b9c897c9e6624445eaa7d06e94ae1ea71c6
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-16 21:47:31 +0530

    Update 1287-Element Appearing More Than 25% In Sorted Array.cpp

--- solutions/1287-Element Appearing More Than 25% In Sorted Array.cpp
+++ solutions/1287-Element Appearing More Than 25% In Sorted Array.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
 
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int findSpecialInteger(vector<int>& arr) {
+        int n = arr.size();
+        int ans = n/4;
+        int cnt = 1;
+
+        for(int i = 1;i<arr.size();i++)
+        {
+            if(arr[i] == arr[i-1])
+            {
+                cnt++;
+                if(cnt > ans)
+                {
+                    return arr[i];
+                }
+            }
+            else
+            {
+                cnt = 1;
+            }
+        }
+
+        return 1;
+    }
+};
