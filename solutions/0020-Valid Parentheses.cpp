0abadd7ce3f83ad8bb6f5918aaf56807943fb657
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-18 01:42:39 +0530

    Update 0020-Valid Parentheses.cpp

--- solutions/0020-Valid Parentheses.cpp
+++ solutions/0020-Valid Parentheses.cpp
@@ -1,1 +1,32 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    bool isValid(string s) {
+        stack<char> st;
 
+        for(char c : s)
+        {
+            if(c == '(' || c == '{' || c == '[')
+            {
+                st.push(c);
+            }
+            else
+            {
+                if(st.empty())
+                {
+                    return false;
+                }
+
+                char openBrack = st.top();
+                st.pop();
+
+                if ((c == ')' && openBrack != '(') || (c == ']' && openBrack != '[') || (c == '}' && openBrack != '{')) 
+                {
+                    return false; 
+                }
+            }
+        }
+
+        return st.empty();
+    }
+};
