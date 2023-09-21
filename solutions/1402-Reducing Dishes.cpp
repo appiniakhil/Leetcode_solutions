73666534f797b6c4856aab11d0aa2f80ee335364
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-22 01:07:34 +0530

    Update 1402-Reducing Dishes.cpp

--- solutions/1402-Reducing Dishes.cpp
+++ solutions/1402-Reducing Dishes.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+    //Time Complexity :O(nlogn) && Space Complexity :O(1)
+    int maxSatisfaction(vector<int>& satisfaction) {
+        int n = satisfaction.size();
 
+        sort(satisfaction.begin(),satisfaction.end());
+
+        int maxiTime = 0, currTime= 0 , totSat = 0;
+
+        for(int i=n-1;i>=0;i--)
+        {
+            totSat += satisfaction[i];
+            currTime += totSat;
+
+            if(currTime > maxiTime)
+            {
+                maxiTime = currTime;
+            }
+        }
+
+        return maxiTime;
+    }
+};
