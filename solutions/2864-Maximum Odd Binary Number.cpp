07815b256c862d78212767fedce04df91f313c0e
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-24 20:14:24 +0530

    Update 2864-Maximum Odd Binary Number.cpp

--- solutions/2864-Maximum Odd Binary Number.cpp
+++ solutions/2864-Maximum Odd Binary Number.cpp
@@ -1,1 +1,51 @@
-
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    string maximumOddBinaryNumber(string s) {
+        string ans ="";
+        
+        int n = s.size();
+        
+        int cntfor1 = 0;
+        int cntfor0 = 0;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(s[i] == '1')
+            {
+                cntfor1++;
+            }
+        }
+        
+        cntfor0 = n - cntfor1;
+        
+        if(cntfor1 == 1)
+        {
+            while(cntfor0 > 0)
+            {
+                ans += '0';
+                cntfor0--;
+            }
+            
+            ans += '1';
+            
+            return ans;
+        }
+        
+        while(cntfor1>1)
+        {
+            ans += '1';
+            cntfor1--;
+        }
+        
+        while(cntfor0 > 0)
+        {
+            ans += '0';
+            cntfor0--;
+        }
+        
+        ans += '1';
+        
+        return ans;
+    }
+};
