e77eb009b493b7af47bad146ba320f6c3f134b06
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-25 23:30:45 +0530

    Update 0389-Find the Difference.cpp

--- solutions/0389-Find the Difference.cpp
+++ solutions/0389-Find the Difference.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    char findTheDifference(string s, string t) {
+        char ans = 0; 
+        int i = 0;
+        int n = s.size();
+        int m = t.size();
 
+        while(i < n)
+        {
+            ans ^= s[i]; 
+            ans ^= t[i]; 
+            i++;
+        }
+
+        ans ^= t[i];
+
+        return ans;
+
+    }
+};
