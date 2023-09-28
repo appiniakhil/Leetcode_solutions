36b6f24014918c5c94fdb6ca1f9ebc306417b5dd
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-28 20:21:27 +0530

    Update 0278-First Bad Version.cpp

--- solutions/0278-First Bad Version.cpp
+++ solutions/0278-First Bad Version.cpp
@@ -1,1 +1,27 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
 
+class Solution {
+public:
+    //Time Complexity :O(logn) && Space Complexity :O(1)
+    int firstBadVersion(int n) {
+        int l = 0, h = n;
+        int ans = 0;
+
+        while(l <= h)
+        {
+            int mid = l + (h-l)/2;
+            if(isBadVersion(mid))
+            {
+                ans = mid;
+                h = mid-1;
+            }
+            else
+            {
+                l = mid+1;
+            }
+        }
+
+        return ans;
+    }
+};
