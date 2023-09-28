8a8ecf8839b317ec6f8db5f6390fdcf1f77d9859
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-29 01:22:33 +0530

    Update 1337-The K Weakest Rows in a Matrix.cpp

--- solutions/1337-The K Weakest Rows in a Matrix.cpp
+++ solutions/1337-The K Weakest Rows in a Matrix.cpp
@@ -1,1 +1,32 @@
+class Solution {
+public:
+    //Time Complexity :O(n * m + nlogn) && Space Complexity :O(n + k)
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<pair<int,int>> weRow;
 
+        for(int i=0;i<n;i++)
+        {
+            int cnt = 0;
+            for(int j=0;j<m;j++)
+            {
+                if(mat[i][j] == 1)
+                {
+                    cnt++;
+                }
+            }
+            weRow.push_back({cnt, i});
+        }
+
+        sort(weRow.begin(), weRow.end());
+        vector<int> res;
+
+        for(int i=0;i<k;i++)
+        {
+            res.push_back(weRow[i].second);
+        }
+
+        return res;
+    }
+};
