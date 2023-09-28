47943c7e29f5c47f228a2a8549401990abb80cb1
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-28 23:15:54 +0530

    Update 0744-Find Smallest Letter Greater Than Target.cpp

--- solutions/0744-Find Smallest Letter Greater Than Target.cpp
+++ solutions/0744-Find Smallest Letter Greater Than Target.cpp
@@ -1,1 +1,30 @@
+class Solution {
+public:
+    //Time Complexity :O(logn) && Space Complexity :O(1)
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int n = letters.size();
 
+        int l = 0, h = n-1;
+
+        while(l <= h)
+        {
+            int mid = l + (h-l)/2;
+
+            if(letters[mid]  > target)
+            {
+                h = mid-1;
+            }
+            else
+            {
+                l = mid+1;
+            }
+        }
+
+        if(l < n)
+        {
+            return letters[l];
+        }
+        
+        return letters[0];
+    }
+};
