5e389ac5c6c8c6dc0bcc0046b78c398af0e2b6be
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-30 03:25:14 +0530

    Update 0896-Monotonic Array.cpp

--- solutions/0896-Monotonic Array.cpp
+++ solutions/0896-Monotonic Array.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    bool isMonotonic(vector<int>& nums) {
+        int n = nums.size();
+        bool inc = true;
+        bool decr = true;
 
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i] < nums[i-1])
+            {
+                inc = false;
+            }
+
+            if(nums[i] > nums[i-1])
+            {
+                decr = false;
+            }
+        }
+
+        return inc || decr;
+    }
+};
