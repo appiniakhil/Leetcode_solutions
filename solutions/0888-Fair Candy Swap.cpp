a7ccf9b8d71caa181c781735d26753caa9bf6577
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-29 07:04:20 +0530

    Update 0888-Fair Candy Swap.cpp

--- solutions/0888-Fair Candy Swap.cpp
+++ solutions/0888-Fair Candy Swap.cpp
@@ -1,1 +1,37 @@
+class Solution {
+public:
+    vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
+        int sumA = 0;
+        int sumB = 0;
+
+        unordered_set<int> s;
+
+        for(int i: aliceSizes)
+        {
+            sumA += i;
+        }
+
+        for(int j: bobSizes)
+        {
+            sumB += j;
+            s.insert(j);
+        }
+
+        int target = (sumA + sumB) / 2;
+
+        for(int i : aliceSizes) 
+        {
+            int candyGive = i;
+            int candyRecieve = target - (sumA - candyGive);
+
+            if(s.count(candyRecieve) > 0)
+            {
+                return {candyGive, candyRecieve};
+            }
+        }
+
+        return {};
+    }
+};
+
 
