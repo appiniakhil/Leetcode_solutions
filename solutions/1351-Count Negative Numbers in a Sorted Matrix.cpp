25bb2c03f562e6b4653b469c6110ec74ba73ddfa
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-09-30 02:42:21 +0530

    Update 1351-Count Negative Numbers in a Sorted Matrix.cpp

--- solutions/1351-Count Negative Numbers in a Sorted Matrix.cpp
+++ solutions/1351-Count Negative Numbers in a Sorted Matrix.cpp
@@ -1,1 +1,54 @@
+class Solution {
+public:
+    //Time Complexity :O(n * m) && Space Complexity :O(1)
+    // int countNegatives(vector<vector<int>>& grid) {
+    //     int n = grid.size();
+    //     int m = grid[0].size();
 
+    //     int cnt = 0;
+
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         for(int j=0;j<m;j++)
+    //         {
+    //             if(grid[i][j] < 0)
+    //             {
+    //                 cnt++;
+    //             }
+    //         }
+    //     }
+
+    //     return cnt;
+    // }
+
+    //Time Complexity :O(mlogn) && Space Complexity :O(1)
+    int countNegatives(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        int cnt = 0;
+
+        for(int i=0;i<n;i++)
+        {
+            int l = 0, h = m-1;
+
+            while(l <= h)
+            {
+                int mid = l + (h-l)/2;
+
+                if(grid[i][mid] < 0)
+                {
+                    h = mid-1;
+                }
+                else
+                {
+                    l = mid+1;
+                }
+            }
+
+            cnt += (m - l);
+        }
+
+        return cnt;
+    }
+};
