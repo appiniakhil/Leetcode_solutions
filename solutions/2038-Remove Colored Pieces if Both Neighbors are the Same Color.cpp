8fd6167ba8118604b2a0dabd99da63ee6714f851
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-10-03 01:15:29 +0530

    Update 2038-Remove Colored Pieces if Both Neighbors are the Same Color.cpp

--- solutions/2038-Remove Colored Pieces if Both Neighbors are the Same Color.cpp
+++ solutions/2038-Remove Colored Pieces if Both Neighbors are the Same Color.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    bool winnerOfGame(string colors) {
+        int n = colors.size();
+        int cntA = 0;
+        int cntB = 0;
 
+        for(int i=0;i<n-2;i++) 
+        {
+            if(colors[i] == 'A' && colors[i + 1] == 'A' && colors[i + 2] == 'A') 
+            {
+                cntA++;
+            }
+            else if(colors[i] == 'B' && colors[i + 1] == 'B' && colors[i + 2] == 'B') 
+            {
+                cntB++;
+            }
+        }
+
+        return cntA > cntB;
+    }
+};
