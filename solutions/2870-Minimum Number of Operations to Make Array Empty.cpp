b5479915a39a6a2931face792b879194ce0fc5cd
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-10-04 01:46:31 +0530

    Update 2870-Minimum Number of Operations to Make Array Empty.cpp

--- solutions/2870-Minimum Number of Operations to Make Array Empty.cpp
+++ solutions/2870-Minimum Number of Operations to Make Array Empty.cpp
@@ -1,1 +1,35 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int> mp;
 
+        for(auto& i : nums)
+        {
+            mp[i]++;
+        }
+
+        int cnt = 0;
+
+        for(auto& i : mp)
+        {
+            int freq = i.second;
+            if(freq == 1)
+            {
+                return -1;
+            }
+            else if(freq %3 == 0)
+            {
+                cnt += freq/3;
+            }
+            else
+            {
+                cnt += freq/3;
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+};
