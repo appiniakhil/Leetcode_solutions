60d1e6bc48c9ef93718268370bd29eb244c55d03
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-10-03 19:18:22 +0530

    Update 2869-Minimum Operations to Collect Elements.cpp

--- solutions/2869-Minimum Operations to Collect Elements.cpp
+++ solutions/2869-Minimum Operations to Collect Elements.cpp
@@ -1,1 +1,23 @@
-2
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    int minOperations(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_set<int> s;
+
+        for(int i=n-1;i>=0;i--)
+        {
+            if(nums[i] <= k)
+            {
+                s.insert(nums[i]);
+            }
+
+            if(s.size() == k)
+            {
+                return n-i;
+            }
+        }
+
+        return n;
+    }
+};
