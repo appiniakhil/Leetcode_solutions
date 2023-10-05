c002ed4ecc083a3afac41cb9ed5e0bbc76dcafb1
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-10-05 05:45:21 +0530

    Update 0880-Decoded String at Index.cpp

--- solutions/0880-Decoded String at Index.cpp
+++ solutions/0880-Decoded String at Index.cpp
@@ -1,1 +1,40 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    string decodeAtIndex(string s, int k) {
+        long size = 0;
+        int i = 0;
 
+        while(size < k)
+        {
+            if(isdigit(s[i]))
+            {
+                size *= (s[i] - '0');
+            }
+            else
+            {
+                size++;
+            }
+            i++;
+        }
+
+        while(i--)
+        {
+            if(isdigit(s[i]))
+            {
+                size /= (s[i] - '0');
+                k %= size;
+            }
+            else if(k == 0 || k == size)
+            {
+                return string(1, s[i]);
+            }
+            else
+            {
+                size--;
+            }
+        }
+
+        return "";
+    }
+};
