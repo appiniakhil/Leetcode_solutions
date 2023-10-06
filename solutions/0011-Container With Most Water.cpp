e7e900016cf4de06eede9ceab88b73c69480ce14
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-10-06 21:00:34 +0530

    Update 0011-Container With Most Water.cpp

--- solutions/0011-Container With Most Water.cpp
+++ solutions/0011-Container With Most Water.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int l = 0, r = n-1;
+        int maxi = 0;
 
+        while(l <= r)
+        {
+            int h = min(height[l], height[r]);
+            int w = r-l;
+            int area = h * w;
+            maxi = max(maxi, area);
+
+            if(height[l] < height[r])
+            {
+                l++;
+            }
+            else
+            {
+                r--;
+            }
+        }
+
+        return maxi;
+    }
+};
