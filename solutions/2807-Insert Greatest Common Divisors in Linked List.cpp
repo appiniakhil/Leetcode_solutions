6dd57427966b121c908d87ae32a947f7391c1e31
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-10-09 00:45:16 +0530

    Update 2807-Insert Greatest Common Divisors in Linked List.cpp

--- solutions/2807-Insert Greatest Common Divisors in Linked List.cpp
+++ solutions/2807-Insert Greatest Common Divisors in Linked List.cpp
@@ -1,1 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    int gcd(int a, int b)
+    {
+        if(a == 0)
+        {
+            return b;
+        }
 
+        return gcd(b%a, a);
+    }
+
+    ListNode* insertGreatestCommonDivisors(ListNode* head) {
+        ListNode* curr = head;
+
+        while(curr != NULL && curr->next != NULL)
+        {
+            int gcdval = gcd(curr->val, curr->next->val);
+            ListNode*  node = new ListNode(gcdval);
+
+            node->next = curr->next;
+            curr->next = node;
+
+            curr = curr->next->next;
+        }
+
+        return head;
+    }
+};
