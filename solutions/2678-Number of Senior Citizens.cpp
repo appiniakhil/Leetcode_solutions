bd550e4aea6d32c73fd68a126ea0174cdd405ce4
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-10-10 23:00:58 +0530

    Update 2678-Number of Senior Citizens.cpp

--- solutions/2678-Number of Senior Citizens.cpp
+++ solutions/2678-Number of Senior Citizens.cpp
@@ -1,1 +1,36 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int stoI(string age)
+    {
+        int num = 0;
+        for(char c : age)
+        {
+            if(c >= '0' && c <= '9')
+            {
+                num = num*10 + (c - '0');
+            }
+        }
 
+        return num;
+    }
+
+    int countSeniors(vector<string>& details) {
+        int n = details.size();
+
+        string age = "";
+        int cnt = 0;
+        for(int i=0;i<n;i++)
+        {
+            age = details[i].substr(11, 2);
+            int num = stoI(age);
+            if(num > 60)
+            {
+                cnt++;
+            }
+            age = "";
+        }
+
+        return cnt;
+    }
+};
