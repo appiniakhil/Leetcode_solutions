9225b2a7dce955ec1df427391c59376bae464f84
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-10-10 08:07:38 +0530

    Update 2009-Minimum Number of Operations to Make Array Continuous.cpp

--- solutions/2009-Minimum Number of Operations to Make Array Continuous.cpp
+++ solutions/2009-Minimum Number of Operations to Make Array Continuous.cpp
@@ -1,1 +1,36 @@
+class Solution {
+public:
+    //Time Complexity :O(nlogm) && Space Complexity :O(1)
+    int minOperations(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
 
+        int left = 0;
+        int ans = 1;
+        int n = nums.size();
+        int cnt = 1;
+        for(int i=1;i<nums.size();i++) 
+        {
+            if((nums[i]-nums[left]) <= (n-1) && nums[i]-nums[i-1] != 0) 
+            {
+                cnt++;
+            }
+
+            int r = left;
+
+            while(left <= i && nums[i]-nums[left] > (n-1)) 
+            {
+                if(left != r && nums[left] != nums[left-1])
+                {
+                    cnt--;
+                }
+
+                left++;
+            }
+
+            ans = max(ans, cnt);
+        }
+
+        return n-ans;
+    }
+
+};
