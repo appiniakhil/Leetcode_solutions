0ffb701ad1df802c8be7dea6296c60706c3a0d5f
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-10-10 10:28:54 +0530

    Update 2679-Sum in a Matrix.cpp

--- solutions/2679-Sum in a Matrix.cpp
+++ solutions/2679-Sum in a Matrix.cpp
@@ -1,1 +1,51 @@
+class Solution {
+public:
+    //Time Complexity :O(n * m * logm) && Space Complexity :O(1)
+    // int matrixSum(vector<vector<int>>& nums) {
+    //     int n = nums.size();
+    //     int m = nums[0].size();
 
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         sort(nums[i].begin(), nums[i].end());
+    //     }
+
+    //     int score = 0;
+    //     for(int i=0;i<m;i++)
+    //     {
+    //         int maxi = 0;
+    //         for(int j=0;j<n;j++)
+    //         {
+    //             maxi = max(maxi, nums[j][i]);
+    //         }
+    //         score += maxi;
+    //     }
+
+    //     return score;
+    // }
+
+    //Time Complexity :O(n * m * logm) && Space Complexity :O(1)
+    int matrixSum(vector<vector<int>>& nums) {
+        int n = nums.size();
+        int m = nums[0].size();
+
+        vector<int> columnMax(m, INT_MIN);
+
+        for(int i = 0;i<n;i++) 
+        {
+            sort(nums[i].begin(), nums[i].end());
+            for (int j=0;j<m;j++) 
+            {
+                columnMax[j] = max(columnMax[j], nums[i][j]);
+            }
+        }
+
+        int score = 0;
+        for (int j=0;j<m;j++) 
+        {
+            score += columnMax[j];
+        }
+
+        return score;
+    }
+};
