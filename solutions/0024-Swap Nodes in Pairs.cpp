d33952c01c24b93d1f507727b2988d1158a49a2a
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-10-11 22:17:16 +0530

    Update 0024-Swap Nodes in Pairs.cpp

--- solutions/0024-Swap Nodes in Pairs.cpp
+++ solutions/0024-Swap Nodes in Pairs.cpp
@@ -1,1 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* temp = new ListNode(0);
+        temp->next = head;
 
+        ListNode* prev = temp;
+        ListNode* curr = head;
+
+        while(curr != NULL && curr->next != NULL)
+        {
+            ListNode* nextNode = curr->next;
+            prev->next = nextNode;
+            curr->next = nextNode->next;
+            nextNode->next = curr;
+            prev = curr;
+            curr = curr->next;
+        }
+
+        ListNode* newhead = temp->next;
+        delete temp;
+        
+        return newhead;
+    }
+};
