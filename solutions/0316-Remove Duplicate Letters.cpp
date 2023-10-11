9c523f97d77684fbf3b90cc0ae6bb35af6eb4ba0
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-10-12 04:04:49 +0530

    Update 0316-Remove Duplicate Letters.cpp

--- solutions/0316-Remove Duplicate Letters.cpp
+++ solutions/0316-Remove Duplicate Letters.cpp
@@ -1,1 +1,40 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    string removeDuplicateLetters(string s) {
+        int n = s.size();
+
+        string ans = "";
+
+        vector<int> cnt(26, 0);
+
+        for(char c : s) 
+        {
+            cnt[c - 'a']++;
+        }
+
+        vector<bool> st(26, false);
+
+        for(char c : s) 
+        {
+            cnt[c - 'a']--;
+
+            if(st[c - 'a']) 
+            {
+                continue;
+            }
+
+            while(!ans.empty() && c < ans.back() && cnt[ans.back() - 'a'] > 0) 
+            {
+                st[ans.back() - 'a'] = false;
+                ans.pop_back();
+            }
+
+            ans.push_back(c);
+            st[c - 'a'] = true;
+        }
+
+        return ans;
+    }
+};
 
