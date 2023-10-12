11a03c8840244ec7ae1392f90062de4bfc06fbf8
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-10-12 07:16:47 +0530

    Update 1658-Minimum Operations to Reduce X to Zero.cpp

--- solutions/1658-Minimum Operations to Reduce X to Zero.cpp
+++ solutions/1658-Minimum Operations to Reduce X to Zero.cpp
@@ -1,1 +1,46 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int minOperations(vector<int>& nums, int x) {
+        int n = nums.size();
+        
+        int totsum = 0;
+        for(int i=0;i<n;i++)
+        {
+            totsum += nums[i];
+        }
+        
+        int target = totsum - x;
 
+        if(target == 0)
+        {
+            return n;
+        }
+        
+        int j = 0;
+        int sum = 0;
+        int mini = INT_MAX;
+        for(int i=0;i<n;i++)
+        {
+            sum += nums[i];
+            
+            while(sum > target && j <= i)
+            {
+                sum -= nums[j];
+                j++;
+            }
+
+            if(sum == target)
+            {
+                mini = min(mini, (n-1-i) + j);
+            }
+        }
+
+        if(mini != INT_MAX)
+        {
+            return mini;
+        }
+
+        return -1;
+    }
+};
