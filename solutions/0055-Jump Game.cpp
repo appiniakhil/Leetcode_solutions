0c1ea91ff0f7d48a170b3d3b226181e62b11eefb
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-10-18 00:45:29 +0530

    Update 0055-Jump Game.cpp

--- solutions/0055-Jump Game.cpp
+++ solutions/0055-Jump Game.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
 
+        int maxi = 0;
+
+        for(int i=0;i<n;i++)
+        {
+            if(i > maxi)
+            {
+                return false;
+            }
+            maxi = max(maxi, i + nums[i]);
+        }
+
+        return true;
+    }
+};
