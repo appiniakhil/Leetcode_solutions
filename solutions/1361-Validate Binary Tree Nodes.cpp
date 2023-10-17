bf7045a3fbc15ba86f6923150e9c97ded8103ecb
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-10-18 00:16:17 +0530

    Update 1361-Validate Binary Tree Nodes.cpp

--- solutions/1361-Validate Binary Tree Nodes.cpp
+++ solutions/1361-Validate Binary Tree Nodes.cpp
@@ -1,1 +1,68 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    bool validateBinaryTreeNodes(int n, std::vector<int>& leftChild, std::vector<int>& rightChild) {
+        vector<int> inDeg(n, 0);
 
+        for(int i=0;i<n;i++) 
+        {
+            if(leftChild[i] != -1) 
+            {
+                inDeg[leftChild[i]]++;
+            }
+
+            if(rightChild[i] != -1) 
+            {
+                inDeg[rightChild[i]]++;
+            }
+        }
+
+        int zeroInDegCnt = 0;
+        int zeroInDegNode = -1;
+
+        for(int i = 0;i<n;i++) 
+        {
+            if(inDeg[i] == 0) 
+            {
+                zeroInDegCnt++;
+                zeroInDegNode = i;
+            } 
+            else if(inDeg[i] > 1) 
+            {
+                return false;
+            }
+        }
+
+        if(zeroInDegCnt != 1) 
+        {
+            return false;
+        }
+
+        vector<bool> vis(n, false);
+
+        dfs(zeroInDegNode, leftChild, rightChild, vis);
+        for(int i = 0;i<n;i++) 
+        {
+            if(!vis[i]) 
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void dfs(int node, vector<int>& leftChild, vector<int>& rightChild, vector<bool>& vis) 
+    {
+        if(node == -1 || vis[node]) 
+        {
+            return;
+        }
+
+        vis[node] = true;
+
+        dfs(leftChild[node], leftChild, rightChild, vis);
+        dfs(rightChild[node], leftChild, rightChild, vis);
+    }
+
+};
