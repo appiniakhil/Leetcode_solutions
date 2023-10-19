242c502de391fab1861a7d4101f6c79ba6334c6c
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-10-19 11:04:24 +0530

    Update 0844-Backspace String Compare.cpp

--- solutions/0844-Backspace String Compare.cpp
+++ solutions/0844-Backspace String Compare.cpp
@@ -1,1 +1,36 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    string finalS(string s)
+    {
+        stack<char> st;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i] != '#')
+            {
+                st.push(s[i]);
+            }
+            else if(!st.empty())
+            {
+                st.pop();
+            }
+        }
 
+        string ans = "";
+        while(!st.empty())
+        {
+            ans += st.top();
+            st.pop();
+        }
+
+        return ans;
+    }
+
+    bool backspaceCompare(string s, string t) {
+        string s1 = finalS(s);
+        string s2 = finalS(t);
+
+        return (s1 == s2);
+        
+    }
+};
