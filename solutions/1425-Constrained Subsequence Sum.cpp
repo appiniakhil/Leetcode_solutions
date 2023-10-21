cc92ce4445bad5bdbf9d586120b5a5ce1a7692ba
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-10-21 11:07:46 +0530

    Update 1425-Constrained Subsequence Sum.cpp

--- solutions/1425-Constrained Subsequence Sum.cpp
+++ solutions/1425-Constrained Subsequence Sum.cpp
@@ -1,1 +1,30 @@
+class Solution {
+public:
+    //Time Complexity :O(nlogn) && Space Complexity :O(n)
+    int constrainedSubsetSum(vector<int>& nums, int k) {
+        int n = nums.size();
 
+        vector<int> dp(n);
+        priority_queue<pair<int, int>> pq;
+
+        int ans = INT_MIN;
+        for(int i=0;i<n;i++)
+        {
+            while(!pq.empty() && pq.top().second < i - k)
+            {
+                pq.pop();
+            }
+
+            dp[i] = nums[i];
+            if(!pq.empty())
+            {
+                dp[i] = max(dp[i], pq.top().first + nums[i]);
+            }
+
+            ans = max(ans, dp[i]);
+            pq.push({dp[i], i});
+        }
+
+        return ans;
+    }
+};
