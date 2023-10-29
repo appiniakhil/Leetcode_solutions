cc7689e2ced91a86f8823b1382fdeecaa1c02344
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-10-29 11:01:36 +0530

    Update 0458-Poor Pigs.cpp

--- solutions/0458-Poor Pigs.cpp
+++ solutions/0458-Poor Pigs.cpp
@@ -1,1 +1,9 @@
-
+class Solution {
+public:
+    //Time Complexity :O(1) && Space Complexity :O(1)
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int rounds = minutesToTest / minutesToDie;
+        
+        return ceil(log2(buckets) / log2(rounds + 1));
+    }
+};
