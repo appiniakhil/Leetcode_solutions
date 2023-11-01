2a8d65b10b07ea837b7ba3ebef10d062b7878fc0
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-01 15:41:38 +0530

    Update 0501-Find Mode in Binary Search Tree.cpp

--- solutions/0501-Find Mode in Binary Search Tree.cpp
+++ solutions/0501-Find Mode in Binary Search Tree.cpp
@@ -1,1 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    //Time Complexity: O(n) && Space Complexity:O(n)
+    void helper(TreeNode* root, vector<int>& arr)
+    {
+        if(root == NULL)
+        {
+            return ;
+        }
 
+        arr.push_back(root->val);
+        helper(root->left, arr);
+        helper(root->right, arr);
+
+    }
+
+    vector<int> findMode(TreeNode* root) {
+        vector<int> arr;
+        helper(root, arr);
+
+        unordered_map<int, int> freq;
+        int maxi = 0;
+
+        for(int val : arr) 
+        {
+            freq[val]++;
+            maxi = max(maxi, freq[val]);
+        }
+        
+        vector<int> res;
+        for(const auto& pair : freq) 
+        {
+            if(pair.second == maxi) 
+            {
+                res.push_back(pair.first);
+            }
+        }
+        
+        return res;
+    }
+};
