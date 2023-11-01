54912b0c6eea252432151b28a77ec7986842f618
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-01 20:00:02 +0530

    Update 0032-Longest Valid Parentheses.cpp

--- solutions/0032-Longest Valid Parentheses.cpp
+++ solutions/0032-Longest Valid Parentheses.cpp
@@ -1,1 +1,32 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    int longestValidParentheses(string s) {
+        int n = s.length();
 
+        stack<int> st;
+        st.push(-1);
+
+        int ans = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='(')
+            {
+                st.push(i);
+            } 
+            else
+            {
+                st.pop();
+                if(st.empty())
+                {
+                    st.push(i);
+                }
+                else
+                {
+                    ans = max(ans,i-st.top());
+                }
+            }
+        }
+        return ans;
+    }
+};
