c0d54baaa4d80ec52aa2d1b6644d911b857f309d
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-03 22:40:27 +0530

    Update 0202-Happy Number.cpp

--- solutions/0202-Happy Number.cpp
+++ solutions/0202-Happy Number.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
+    //Time Complexity :O(logn) && Space Complexity :O(logn)
+    int helper(int n)
+    {
+        int sum = 0;
 
+        while(n > 0)
+        {
+            int dig = n % 10;
+            sum += dig * dig;
+            n = n/10;
+        }
+
+        return sum;
+    }
+
+    bool isHappy(int n) {
+        set<int> seen;
+
+        while(n != 1 && seen.find(n) == seen.end())
+        {
+            seen.insert(n);
+            n = helper(n);
+        }
+
+        return n == 1;
+    }
+};
