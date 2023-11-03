af4a88b7e4b31cf4539ba9a1aa99a138ad6d928e
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-03 19:03:39 +0530

    Update 1441-Build an Array With Stack Operations.cpp

--- solutions/1441-Build an Array With Stack Operations.cpp
+++ solutions/1441-Build an Array With Stack Operations.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    vector<string> buildArray(vector<int>& target, int n) {
+        vector<string> ans;
+        
+        int num = 1;
+        for(int i=0;i<target.size();i++)
+        {
+            while(num < target[i])
+            {
+                ans.push_back("Push");
+                ans.push_back("Pop");
+                num++;
+            }
+            ans.push_back("Push");
+            num++;
+        }
+        
+        return ans;
+    }
+};
