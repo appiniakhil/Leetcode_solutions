1687e06c987c79071f12d139b8448c8ab3abd4a9
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-05 00:24:29 +0530

    Update 0151-Reverse Words in a String.cpp

--- solutions/0151-Reverse Words in a String.cpp
+++ solutions/0151-Reverse Words in a String.cpp
@@ -1,1 +1,40 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    string reverseWords(string s) {
+        int n = s.length();
+        string ans = "";
 
+        for(int i = n - 1; i >= 0; i--) 
+        {
+            if(s[i] == ' ')
+            {
+                continue;
+            }
+
+            string temp = "";
+
+            while(i >= 0 && s[i] != ' ') 
+            {
+                temp += s[i];
+                i--;
+            }
+
+            reverse(temp.begin(), temp.end());
+            ans += temp;
+
+            if(i >= 0) 
+            {
+                ans += ' ';
+            }
+        }
+
+        int si = ans.size();
+        if(ans[si-1] == ' ')
+        {
+            return ans.substr(0, si-1);
+        }
+        
+        return ans;
+    }
+};
