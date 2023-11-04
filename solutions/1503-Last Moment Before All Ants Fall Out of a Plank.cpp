eaee15cab23e8f2df237031f8c40b7380c23621d
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-04 23:47:20 +0530

    Update 1503-Last Moment Before All Ants Fall Out of a Plank.cpp

--- solutions/1503-Last Moment Before All Ants Fall Out of a Plank.cpp
+++ solutions/1503-Last Moment Before All Ants Fall Out of a Plank.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        int time = 0;
 
+        for(int& l : left)
+        {
+            time = max(time, l);
+        }
+
+        for(int& r : right)
+        {
+            time = max(time,n - r);
+        }
+
+        return time;
+    }
+};
