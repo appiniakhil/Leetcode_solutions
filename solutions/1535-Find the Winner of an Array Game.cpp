88b4d4d69867a93b13824e2e9669869ec1d885e2
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-06 00:25:40 +0530

    Update 1535-Find the Winner of an Array Game.cpp

--- solutions/1535-Find the Winner of an Array Game.cpp
+++ solutions/1535-Find the Winner of an Array Game.cpp
@@ -1,1 +1,46 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int getWinner(vector<int>& arr, int k) {
+        int n = arr.size();
 
+        if(k == 1)
+        {
+            return max(arr[0],arr[1]);
+        }
+
+        if(k >= n)
+        {
+            int maxi = 0;
+            for(int i=0;i<n;i++)
+            {
+                maxi = max(maxi, arr[i]);
+            }
+
+            return maxi;
+        }
+
+        int currW = arr[0];
+        int cons = 0;
+
+        for(int i=1;i<n;i++)
+        {
+            if(currW > arr[i])
+            {
+                cons++;
+            }
+            else
+            {
+                currW = arr[i];
+                cons = 1;
+            }
+
+            if(cons == k)
+            {
+                return currW;
+            }
+        }
+
+        return currW;
+    }
+};
