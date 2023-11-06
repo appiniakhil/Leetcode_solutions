c414aeb7e8140bcc653364a24f6e91f7412e6822
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-06 23:49:38 +0530

    Update 2390-Removing Stars From a String.cpp

--- solutions/2390-Removing Stars From a String.cpp
+++ solutions/2390-Removing Stars From a String.cpp
@@ -1,1 +1,36 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    string removeStars(string s) {
+        int n = s.length();
 
+        stack<char> st;
+
+        for(int i=0;i<n;i++)
+        {
+            if(s[i] != '*')
+            {
+                st.push(s[i]);
+            }
+            else
+            {
+                if(!st.empty())
+                {
+                    st.pop();
+                }
+            }
+        }
+
+        string ans ="";
+
+        while(!st.empty())
+        {
+            ans += st.top();
+            st.pop();
+        }
+
+        reverse(ans.begin(),ans.end());
+
+        return ans;
+    }
+};
