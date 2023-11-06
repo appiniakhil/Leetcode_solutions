a9f2342999b902a8df8b478b1cc52d5e0870ae59
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-06 23:34:42 +0530

    Update 0724-Find Pivot Index.cpp

--- solutions/0724-Find Pivot Index.cpp
+++ solutions/0724-Find Pivot Index.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size(), sum = 0;
 
+        for(int i=0;i<n;i++)
+        {
+            sum += nums[i];
+        }
+
+        int lSum = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(lSum == sum - lSum - nums[i])
+            {
+                return i;
+            }
+            lSum += nums[i];
+        }
+
+        return -1;
+    }
+};
