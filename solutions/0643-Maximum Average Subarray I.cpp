2d6a27645699750f1668a6bfe7025facb3e7c5ba
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-06 23:19:41 +0530

    Update 0643-Maximum Average Subarray I.cpp

--- solutions/0643-Maximum Average Subarray I.cpp
+++ solutions/0643-Maximum Average Subarray I.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n = nums.size();
+        int l=0;
+        double ans=0,curr=0,maxi=INT_MIN;
 
+        for(int r=0;r<n;r++)
+        {
+            curr = nums[r];
+
+            ans += nums[r];
+            if(r-l+1>k)
+            {
+                ans -= nums[l];
+                l++;
+            }
+
+            if(r-l+1 == k)
+            {
+                maxi = max(maxi, ans/k);
+            }
+        }
+
+        return maxi;
+    }
+};
