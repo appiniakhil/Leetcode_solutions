495b981203944d67e7fa3f4f9512e8f6cd040dc6
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-08 01:06:31 +0530

    Update 1921-Eliminate Maximum Number of Monsters.cpp

--- solutions/1921-Eliminate Maximum Number of Monsters.cpp
+++ solutions/1921-Eliminate Maximum Number of Monsters.cpp
@@ -1,1 +1,41 @@
+class Solution {
+public:
+    //Time Complexity :O(nlogn) && Space Complexity :O(n)
+    int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
+        int n = dist.size();
 
+        vector<double> time(n);
+        for(int i=0;i<n;i++)
+        {
+            time[i] = (double)dist[i] / speed[i];
+        }
+
+        priority_queue<double, vector<double>, greater<double>> pq;
+
+        for(double t : time)
+        {
+            pq.push(t);
+        }
+
+        int elemMon = 0;
+        double currT = 0.0;
+
+        while(!pq.empty())
+        {
+            double nxtMonArr = pq.top();
+            pq.pop();
+
+            if(nxtMonArr > currT)
+            {
+                elemMon++;
+                currT += 1.0;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return elemMon;
+    }
+};
