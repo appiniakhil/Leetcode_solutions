cd64d4ec65f2714d8a43886ee05936e20ed808ab
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-09 02:24:39 +0530

    Update 2849-Determine if a Cell Is Reachable at a Given Time.cpp

--- solutions/2849-Determine if a Cell Is Reachable at a Given Time.cpp
+++ solutions/2849-Determine if a Cell Is Reachable at a Given Time.cpp
@@ -1,1 +1,12 @@
+class Solution {
+public:
+    //Time Complexity :O(1) && Space Complexity :O(1)
+    bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+        if(t==1 && sx == fx && sy == fy)
+        {
+            return false;
+        }
 
+        return t >= max(abs(sx-fx), abs(sy-fy));
+    }
+};
