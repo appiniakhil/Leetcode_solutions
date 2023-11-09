78f059ece7a5f6e28a75338d3d7c1e0cb07a2d74
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-09 18:48:25 +0530

    Update 0459-Repeated Substring Pattern.cpp

--- solutions/0459-Repeated Substring Pattern.cpp
+++ solutions/0459-Repeated Substring Pattern.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    //Time Complexity :O(n * n) && Space Complexity :O(n)
+    bool repeatedSubstringPattern(string s) {
+        int n=s.size();
 
+        for(int i=n/2;i>0;i--)
+        {
+            int rep = n/i;
+
+            string substring = s.substr(0, i);
+            string ans="";
+            for(int i=0;i<rep;i++)
+            {
+                ans += substring;
+            }
+
+            if(ans == s)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
