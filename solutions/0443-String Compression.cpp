bf79d83f7af00cd04068df12def45ea288da768d
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-10 02:03:31 +0530

    Update 0443-String Compression.cpp

--- solutions/0443-String Compression.cpp
+++ solutions/0443-String Compression.cpp
@@ -1,1 +1,32 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int compress(vector<char>& chars) {
+        int i=0;
+        int j=0;
 
+        while(i < chars.size())
+        {
+            chars[j] = chars[i];
+            int cnt = 0;
+            while(i < chars.size() && chars[i] == chars[j])
+            {
+                i++;
+                cnt++;
+            }
+
+            if(cnt > 1)
+            {
+                string strcnt = to_string(cnt);
+                for(char c : strcnt)
+                {
+                    chars[++j] = c;
+                }
+            }
+
+            j++;
+        }
+    
+        return j;
+    }
+};
