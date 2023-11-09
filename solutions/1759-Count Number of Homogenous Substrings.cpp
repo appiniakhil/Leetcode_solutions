8ee8c4dccc9a8cfb26d5b96e12f31fe70b0a7b51
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-10 00:14:58 +0530

    Update 1759-Count Number of Homogenous Substrings.cpp

--- solutions/1759-Count Number of Homogenous Substrings.cpp
+++ solutions/1759-Count Number of Homogenous Substrings.cpp
@@ -1,1 +1,28 @@
+class Solution {
+    //Time Complexity :O(n) && Space Complexity :O(1)
+private:
+    const int MOD = 1e9+7;
+public:
+    int countHomogenous(string s) {
+        long long res = 0, cnt = 0;
 
+        char prev = ' ';
+        for(char c : s)
+        {
+            if(c == prev)
+            {
+                cnt += 1;
+            }
+            else
+            {
+                cnt = 1;
+            }
+
+            res = (res + cnt) % MOD;
+
+            prev = c;
+        }
+
+        return res;
+    }
+};
