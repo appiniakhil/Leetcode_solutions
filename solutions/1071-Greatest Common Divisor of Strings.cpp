59aa0275326d36bfc305d51f51eb4f596cfd5906
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-10 00:09:11 +0530

    Update 1071-Greatest Common Divisor of Strings.cpp

--- solutions/1071-Greatest Common Divisor of Strings.cpp
+++ solutions/1071-Greatest Common Divisor of Strings.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+    //Time Complexity :O(max(n,m)) && Space Complexity :O(n + m)
+    string gcdOfStrings(string str1, string str2) {
+        if(str1 + str2 != str2 + str1)
+        {
+            return "";
+        }
 
+        while(str1 != str2)
+        {
+            if(str1.size() > str2.size())
+            {
+                str1 = str1.substr(str2.size());
+            }
+            else
+            {
+                str2 = str2.substr(str1.size());
+            }
+        
+        }
+        return str1;
+    }
+};
