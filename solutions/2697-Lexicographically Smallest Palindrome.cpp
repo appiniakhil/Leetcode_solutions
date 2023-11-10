f64f6fe3ffbcce202208bbf93902784f92307ba4
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-11 00:09:53 +0530

    Update 2697-Lexicographically Smallest Palindrome.cpp

--- solutions/2697-Lexicographically Smallest Palindrome.cpp
+++ solutions/2697-Lexicographically Smallest Palindrome.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    //Time Complexityu :O(n) && Space Complexity :O(1)
+    string makeSmallestPalindrome(string s) {
+        int n = s.length();
 
+        for(int i=0;i<n/2;i++) 
+        {
+            int j=n-i-1;
+
+            if(s[i] != s[j]) 
+            {
+                s[i] = s[j] = min(s[i], s[j]);
+            }
+        }
+
+        return s;
+    }
+};
