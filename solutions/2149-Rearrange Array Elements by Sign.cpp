d967aed2afd0506db5a5ade6fa1306d802294906
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-10 23:51:14 +0530

    Update 2149-Rearrange Array Elements by Sign.cpp

--- solutions/2149-Rearrange Array Elements by Sign.cpp
+++ solutions/2149-Rearrange Array Elements by Sign.cpp
@@ -1,1 +1,33 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
 
+        vector<int> pos,neg;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i] > 0)
+            {
+                pos.push_back(nums[i]);
+            }
+            else
+            {
+                neg.push_back(nums[i]);
+            }
+        }
+
+        int i=0,j=0;
+        vector<int> ans;
+        
+        while(i < n/2 && j < n/2)
+        {
+            ans.push_back(pos[i]);
+            ans.push_back(neg[j]);
+            i++;
+            j++;
+        }
+
+        return ans;
+    }
+};
