aa9c4a61c969e7f86653f4bd75e3cfa34265b1a8
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-11 02:52:34 +0530

    Update 0992-Subarrays with K Different Integers.cpp

--- solutions/0992-Subarrays with K Different Integers.cpp
+++ solutions/0992-Subarrays with K Different Integers.cpp
@@ -1,1 +1,35 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int helper(vector<int>& nums, int k) 
+    {
+        int i = 0, res = 0;
+        unordered_map<int, int> cnt;
+
+        for(int j=0;j<nums.size();j++) 
+        {
+            if(!cnt[nums[j]]++)
+            {
+                k--;
+            } 
+
+            while(k < 0) 
+            {
+                if (!--cnt[nums[i]]) 
+                {
+                    k++;
+                }
+                i++;
+            }
+
+            res += j - i + 1;
+        }
+
+        return res;
+    }
+
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return helper(nums, k) - helper(nums, k - 1);
+    }
+};
 
