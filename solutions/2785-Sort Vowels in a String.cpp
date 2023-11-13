db8d61dcdee288c88a4bd63bcd964e74328ab59f
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-13 23:21:30 +0530

    Update 2785-Sort Vowels in a String.cpp

--- solutions/2785-Sort Vowels in a String.cpp
+++ solutions/2785-Sort Vowels in a String.cpp
@@ -1,1 +1,44 @@
+class Solution {
+public:
+    //Time Complexity :O(nlogn) && Space Complexity :O(n)
+    bool isVowel(char s)
+    {
+        if(s == 'a' || s == 'e' || s == 'i' || s == 'o' || s == 'u' || s == 'A' || s == 'E' || s == 'I' || s == 'O' || s == 'U')
+        {
+            return true;
+        }
 
+        return false;
+    }
+    
+    string sortVowels(string s) {
+        int n = s.length();
+
+        string vow = "";
+
+        for(int i=0;i<n;i++)
+        {
+            if(isVowel(s[i]))
+            {
+                vow += s[i];
+            }
+        }
+
+        sort(vow.begin(), vow.end());
+        int j = 0;
+
+        for(int i=0;i<n;i++)
+        {
+            if(isVowel(s[i]))
+            {
+                if(j < n)
+                {
+                    s[i] = vow[j];
+                    j++;
+                }
+            }
+        }
+
+        return s;
+    }
+};
