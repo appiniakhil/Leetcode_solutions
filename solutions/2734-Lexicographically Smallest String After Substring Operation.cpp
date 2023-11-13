3ff410fee1df19950b51664cbb87ab0c96d45777
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-14 04:26:18 +0530

    Update 2734-Lexicographically Smallest String After Substring Operation.cpp

--- solutions/2734-Lexicographically Smallest String After Substring Operation.cpp
+++ solutions/2734-Lexicographically Smallest String After Substring Operation.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    string smallestString(string s) {
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i] != 'a')
+            {
+                while(i < s.size() && s[i] != 'a')
+                {
+                    s[i] = s[i] - 1;
+                    i++;
+                }
 
+                break;
+            }
+
+            if(i == s.size() - 1 && s[i] == 'a') 
+            {
+                s[i] = 'z';
+            }
+        }
+
+        return s;
+    }
+};
