cb3a80a46f194338238ab2492705d9c5dc008ee0
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-14 22:56:15 +0530

    Update 1930-Unique Length-3 Palindromic Subsequences.cpp

--- solutions/1930-Unique Length-3 Palindromic Subsequences.cpp
+++ solutions/1930-Unique Length-3 Palindromic Subsequences.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int countPalindromicSubsequence(string s) {
+        int n = s.size();
 
+        vector<int> first(26, INT_MAX), last(26, INT_MIN);
+        
+        for(int i=0;i<n;i++)
+        {
+            first[s[i] - 'a'] = min(first[s[i] -'a'], i);
+
+            last[s[i] - 'a'] = max(last[s[i] - 'a'], i);
+        }
+
+        int cnt = 0;
+        for(int i=0;i<26;i++)
+        {
+            if(first[i] < last[i])
+            {
+                set<char> uniq(s.begin() + first[i] + 1, s.begin() + last[i]);
+                
+                cnt += uniq.size();
+            }
+        }
+
+        return cnt;
+    }
+};
