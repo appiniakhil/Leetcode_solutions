e20f0127b4da932fb115bfb4c452cddb8a88b785
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-16 00:12:37 +0530

    Update 1846-Maximum Element After Decreasing and Rearranging.cpp

--- solutions/1846-Maximum Element After Decreasing and Rearranging.cpp
+++ solutions/1846-Maximum Element After Decreasing and Rearranging.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    //Time Complexity :O(nlogn) && Space Complexity :O(1)
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        int n = arr.size();
 
+        sort(arr.begin(),arr.end());
+        arr[0] = 1;
+
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i] > arr[i-1])
+            {
+                arr[i] = arr[i-1] + 1;
+            }
+        }
+
+        return arr.back();
+    }
+};
