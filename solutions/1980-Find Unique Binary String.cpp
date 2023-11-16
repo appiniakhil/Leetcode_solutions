8eb5b128688c745872aff2a1b33f9e815e00e7e2
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-16 19:33:04 +0530

    Update 1980-Find Unique Binary String.cpp

--- solutions/1980-Find Unique Binary String.cpp
+++ solutions/1980-Find Unique Binary String.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    string findDifferentBinaryString(vector<string>& nums) {
+        int n = nums.size();
 
+        string ans = "";
+
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i][i] == '0')
+            {
+                ans += '1';
+            }
+            else
+            {
+                ans += '0';
+            }
+        }
+
+        return ans;
+    }
+};
