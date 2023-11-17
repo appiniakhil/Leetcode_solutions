237a63b0c9b5943b9bb6df8e00a10b19801513c6
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-17 23:05:06 +0530

    Update 1877-Minimize Maximum Pair Sum in Array.cpp

--- solutions/1877-Minimize Maximum Pair Sum in Array.cpp
+++ solutions/1877-Minimize Maximum Pair Sum in Array.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    //Time Complexity :O(nlogn) && Space Complexity :O(1)
+    int minPairSum(vector<int>& nums) {
+        int n = nums.size();
 
+        sort(nums.begin(), nums.end());
+
+        int maxi = INT_MIN;
+        int i=0,j=n-1;
+
+        while(i < j)
+        {
+            maxi = max(maxi, nums[i] + nums[j]);
+            i++;
+            j--;
+        }
+
+        return maxi;
+    }
+};
