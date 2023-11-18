92c50be65b9cbe7b7beba5ab2fd76d3d487e6519
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-18 17:41:56 +0530

    Update 1838-Frequency of the Most Frequent Element.cpp

--- solutions/1838-Frequency of the Most Frequent Element.cpp
+++ solutions/1838-Frequency of the Most Frequent Element.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    //Time Complexity :O(nlogn) && Space Complexity :O(1)
+    int maxFrequency(vector<int>& nums, int k) {
+        int n = nums.size();
 
+        sort(nums.begin(), nums.end());
+
+        long sum = 0;
+        int res = 1, j=0;
+
+        for(int i=0;i<n;i++)
+        {
+            sum += nums[i];
+
+            while(sum + k < (long)nums[i] * (i-j+1))
+            {
+                sum -= nums[j];
+                j++;
+            }
+
+            res = max(res, i-j+1);
+        }
+
+        return res;
+    }
+};
