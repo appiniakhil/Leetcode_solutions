7e24b468b06386461dbb470c12ec221ba79adc62
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-19 21:57:27 +0530

    Update 1887-Reduction Operations to Make the Array Elements Equal.cpp

--- solutions/1887-Reduction Operations to Make the Array Elements Equal.cpp
+++ solutions/1887-Reduction Operations to Make the Array Elements Equal.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    //Time Complexity :O(nlogn) && Space Complexity :O(1)
+    int reductionOperations(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
 
+        int ans = 0;
+        for(int i=n-1;i>0;i--)
+        {
+            if(nums[i-1] != nums[i])
+            {
+                ans += n-i;
+            }
+        }
+
+        return ans;
+    }
+};
