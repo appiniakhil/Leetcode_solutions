99597f0f2e1a818a05c6fa34525d6ef574bc9297
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-20 01:00:49 +0530

    Update 0137-Single Number II.cpp

--- solutions/0137-Single Number II.cpp
+++ solutions/0137-Single Number II.cpp
@@ -1,1 +1,40 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    // int singleNumber(vector<int>& nums) {
+    //     int n = nums.size();
 
+    //     unordered_map<int, int> mp;
+
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         mp[nums[i]]++;
+    //     }
+
+    //     for(auto& i : mp)
+    //     {
+    //         if(i.second == 1)
+    //         {
+    //             return i.first;
+    //         }
+    //     }
+
+    //     return -1;
+    // }
+
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+
+        int ones = 0, twos = 0;
+
+        for(int i=0;i<n;i++)
+        {
+            ones = (ones ^ nums[i]) & ~twos;
+
+            twos = (twos ^ nums[i]) & ~ones;
+        }
+        
+        return ones;
+    }
+};
