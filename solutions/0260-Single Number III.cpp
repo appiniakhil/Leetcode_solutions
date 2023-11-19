c33d2cca0161978bcac802c6e204ebdfaae61ebc
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-20 00:52:10 +0530

    Update 0260-Single Number III.cpp

--- solutions/0260-Single Number III.cpp
+++ solutions/0260-Single Number III.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    vector<int> singleNumber(vector<int>& nums) {
+        int n = nums.size();
 
+        vector<int> ans;
+        unordered_map<int, int> mp;
+
+        for(int i=0;i<n;i++)
+        {
+            mp[nums[i]]++;
+        }
+
+        for(auto& i : mp)
+        {
+            if(i.second == 1)
+            {
+                ans.push_back(i.first);
+            }
+        }
+
+        return ans;
+    }
+};
