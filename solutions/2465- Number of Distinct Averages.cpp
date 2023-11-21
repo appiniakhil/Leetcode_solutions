5b07d6fb871d81735621e52faaab3947047cbb84
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-22 00:33:34 +0530

    Update 2465- Number of Distinct Averages.cpp

--- solutions/2465- Number of Distinct Averages.cpp
+++ solutions/2465- Number of Distinct Averages.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    //Time Complexity :O(nlogn) && Space Complxity :O(n)
+    int distinctAverages(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
 
+        unordered_map<long double, long> mp;
+        int  i = 0, j = n-1;
+
+        while(i < j)
+        {
+            long double sum = (long double)(nums[i] + nums[j]) / 2.0; 
+            mp[sum]++;
+            i++;
+            j--;
+        }
+
+        return mp.size();
+    }
+};
