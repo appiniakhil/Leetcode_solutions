4bf68c118fc0d9ad447a87ca7f9f3182d495380e
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-22 01:00:26 +0530

    Update 0594-Longest Harmonious Subsequence.cpp

--- solutions/0594-Longest Harmonious Subsequence.cpp
+++ solutions/0594-Longest Harmonious Subsequence.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    int findLHS(vector<int>& nums) {
+        map<int, int> mp;
 
+        for(int i : nums)
+        {
+            mp[i]++;
+        }
+
+        int cnt = 0;
+
+        for(auto it : mp)
+        {
+            if(mp.count(it. first + 1))
+            {
+                cnt = max(cnt, it.second + mp[it.first + 1]);
+            }
+        }
+
+        return cnt;
+    }
+};
