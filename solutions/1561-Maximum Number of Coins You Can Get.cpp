0cbd79c8ef58e7a3d87befeac56e573be5a6b234
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-24 23:19:45 +0530

    Update 1561-Maximum Number of Coins You Can Get.cpp

--- solutions/1561-Maximum Number of Coins You Can Get.cpp
+++ solutions/1561-Maximum Number of Coins You Can Get.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
+    //Time Complexity :O(nlogn) && Space Complexity :O(1)
+    int maxCoins(vector<int>& piles) {
+        sort(piles.begin(), piles.end(), greater<int>());
 
+        int ans = 0;
+        int n = piles.size();
+
+        for(int i=1;i<n*2/3;i+=2)
+        {
+            ans += piles[i];
+        }
+
+        return ans;
+    }
+};
