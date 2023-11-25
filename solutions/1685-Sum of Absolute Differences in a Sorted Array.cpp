1503f8079d7358c6c9a741002f00b2fe6e099fa5
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-25 10:23:59 +0530

    Update 1685-Sum of Absolute Differences in a Sorted Array.cpp

--- solutions/1685-Sum of Absolute Differences in a Sorted Array.cpp
+++ solutions/1685-Sum of Absolute Differences in a Sorted Array.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
+        int n = nums.size();
 
+        vector<int> prefixSum(n+1);
+
+        for(int i=0;i<n;i++)
+        {
+            prefixSum[i+1] = prefixSum[i] + nums[i];
+        }
+
+        vector<int> res(n);
+
+        for(int i=0;i<n;i++)
+        {
+            res[i] = nums[i]*i - prefixSum[i] + prefixSum[n] - prefixSum[i] - nums[i]*(n-i);
+        }
+
+        return res;
+    }
+};
