643b925fdee40c29f55fe1f1f5c4dac1ad7b991a
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-29 02:03:32 +0530

    Update 2147-Number of Ways to Divide a Long Corridor.cpp

--- solutions/2147-Number of Ways to Divide a Long Corridor.cpp
+++ solutions/2147-Number of Ways to Divide a Long Corridor.cpp
@@ -1,1 +1,36 @@
+class Solution {   
+public:
+    int numberOfWays(string corridor) {
+        int seat=0,plant=0;
+        long long res=1;
 
+        for(char i : corridor) 
+        {
+            if(i == 'S')
+            {
+                seat += 1;
+            }
+            else
+            {
+                if(seat == 2)
+                {
+                    plant += 1;
+                }
+            }
+
+            if(seat == 3) 
+            {
+                res = (res * (plant + 1)) % (1000000007);
+                seat = 1;
+                plant = 0;
+            }
+        }
+
+        if(seat != 2)
+        {
+            return 0;
+        }
+
+        return (int)res;     
+    }
+};
