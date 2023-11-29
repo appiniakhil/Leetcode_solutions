dd11aab92319415f05bab391051bb5ad41b725db
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-29 08:29:50 +0530

    Update 0191-Number of 1 Bits.cpp

--- solutions/0191-Number of 1 Bits.cpp
+++ solutions/0191-Number of 1 Bits.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int hammingWeight(uint32_t n) {
+        string num = bitset<32>(n).to_string();
 
+        int cnt = 0; 
+        for(int i=0;i<num.length();i++)
+        {
+            if(num[i] == '1')
+            {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+};
