a5aceed7825dbf442398cf0df68e49ed514eea84
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-11-30 14:19:25 +0530

    Update 1611-Minimum One Bit Operations to Make Integers Zero.cpp

--- solutions/1611-Minimum One Bit Operations to Make Integers Zero.cpp
+++ solutions/1611-Minimum One Bit Operations to Make Integers Zero.cpp
@@ -1,1 +1,13 @@
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        int res = 0;
+        while(n)
+        {
+            res ^= n;
+            n >>= 1;
+        }
 
+        return res;
+    }
+};
