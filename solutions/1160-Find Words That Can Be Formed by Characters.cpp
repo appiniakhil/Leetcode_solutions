1095efb0733fce321174fc094509daeaed90dbcc
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-12-02 23:19:40 +0530

    Update 1160-Find Words That Can Be Formed by Characters.cpp

--- solutions/1160-Find Words That Can Be Formed by Characters.cpp
+++ solutions/1160-Find Words That Can Be Formed by Characters.cpp
@@ -1,1 +1,37 @@
+class Solution {
+public:
+    int countCharacters(vector<string>& words, string chars) {
+        unordered_map<char, int> chars_count;
+        for(char c : chars)
+        {
+            ++chars_count[c];
+        }
 
+        int res = 0;
+        for(string& word : words)
+        {
+            unordered_map<char, int> word_count;
+            bool is_good = true;
+            for(char c : word)
+            {
+                ++word_count[c];
+            }
+
+            for(char c : word) 
+            {
+                if(chars_count[c] < word_count[c]) 
+                {
+                    is_good = false;
+                    break;
+                }
+            }
+
+            if(is_good)
+            {
+                res += word.size();
+            }
+        }
+        
+        return res;
+    }
+};
