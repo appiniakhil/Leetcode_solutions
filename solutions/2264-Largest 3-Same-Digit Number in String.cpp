8cfbfd1b2a3b5fac04e6800e4991de19d17926f3
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-12-05 00:24:37 +0530

    Update 2264-Largest 3-Same-Digit Number in String.cpp

--- solutions/2264-Largest 3-Same-Digit Number in String.cpp
+++ solutions/2264-Largest 3-Same-Digit Number in String.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    string largestGoodInteger(string num) {
+        int n = num.length();
 
+        string maxi = "";
+
+        for(int i=0;i<=n - 3;i++) 
+        {
+            if(num[i] == num[i + 1] && num[i + 1] == num[i + 2]) 
+            {
+                if(maxi.empty() || num.substr(i, 3) > maxi) 
+                {
+                    maxi = num.substr(i, 3);
+                }
+            }
+        }
+
+        return maxi;
+    }
+};
