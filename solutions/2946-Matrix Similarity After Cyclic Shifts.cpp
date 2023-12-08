678eff88874d6fd8a99acdba37706cbad14077f0
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-12-09 04:17:52 +0530

    Update 2946-Matrix Similarity After Cyclic Shifts.cpp

--- solutions/2946-Matrix Similarity After Cyclic Shifts.cpp
+++ solutions/2946-Matrix Similarity After Cyclic Shifts.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+    //Time Complexity :O(m*n) && Space Complxity :O(m*n)
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+        int n = mat.size();
+        int m = mat[0].size();
+        
+        vector<vector<int>> dummy = mat;
+        for(int i=0;i<n;i++)
+        {
+            if(i%2 == 0)
+            {
+                rotate(dummy[i].begin(), dummy[i].begin() + k%m, dummy[i].end());
+            }
+            else
+            {
+                rotate(dummy[i].rbegin(), dummy[i].rbegin() + k%m, dummy[i].rend());
+            }
+        }
+        
+        return mat == dummy;
+    }
+};
