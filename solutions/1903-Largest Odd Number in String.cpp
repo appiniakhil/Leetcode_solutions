76f5106c87eaf9cbfd761f93eaf610405e5a7b2d
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-12-08 22:05:50 +0530

    Update 1903-Largest Odd Number in String.cpp

--- solutions/1903-Largest Odd Number in String.cpp
+++ solutions/1903-Largest Odd Number in String.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    string largestOddNumber(string num) {
+        int n = num.size();
 
+        for(int i=n-1;i>=0;i--)
+        {
+            if((num[i] - '0') % 2 == 1)
+            {
+                return num.substr(0, i+1);
+            }
+        }
+
+        return "";
+    }
+};
