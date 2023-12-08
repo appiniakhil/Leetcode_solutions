a066dcb2bca97d14702f524f7aae23f5b630a1d6
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-12-08 22:29:14 +0530

    Update 2951-Find the Peaks.cpp

--- solutions/2951-Find the Peaks.cpp
+++ solutions/2951-Find the Peaks.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    vector<int> findPeaks(vector<int>& mountain) {
+        int n = mountain.size();
+        
+        vector<int> ans;
+        for(int i=1;i<n-1;i++)
+        {
+            if(mountain[i-1] < mountain[i] && mountain[i] > mountain[i+1] )
+            {
+                if(i != 0 && i != n-1)
+                {
+                    ans.push_back(i);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
