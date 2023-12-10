7cc85e4405176325388cdaa5f1fdc2592677e97f
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-12-10 16:52:45 +0530

    Update 1647-Minimum Deletions to Make Character Frequencies Unique.cpp

--- solutions/1647-Minimum Deletions to Make Character Frequencies Unique.cpp
+++ solutions/1647-Minimum Deletions to Make Character Frequencies Unique.cpp
@@ -1,1 +1,33 @@
+class Solution {
+public:
+    //Time Complexity :O(nlogn) && Space Complexity: O(n)
+    int minDeletions(string s) {
+        unordered_map<char, int> mp;
 
+        for (char c : s) {
+            mp[c]++;
+        }
+
+        vector<int> freq;
+
+        for(auto& [_, f] : mp) 
+        {
+            freq.push_back(f);
+        }
+
+        sort(freq.begin(), freq.end(), greater<int>());
+
+        int res = 0;
+
+        for(int i=1;i<freq.size();++i) 
+        {
+            while(freq[i]>0 && freq[i]>=freq[i - 1]) 
+            {
+                freq[i]--;
+                res++;
+            }
+        }
+
+        return res;
+    }
+};
