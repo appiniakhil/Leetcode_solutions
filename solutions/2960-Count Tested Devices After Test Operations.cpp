ee79db9a8c6bc42ad8e48831dbca8ab36d507bb3
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-12-11 18:53:03 +0530

    Update 2960-Count Tested Devices After Test Operations.cpp

--- solutions/2960-Count Tested Devices After Test Operations.cpp
+++ solutions/2960-Count Tested Devices After Test Operations.cpp
@@ -1,1 +1,25 @@
-
+class Solution {
+public:
+    //Time Complexity :O(n * n) && Space Complexity :O(1)
+    int countTestedDevices(vector<int>& batteryPercentages) {
+        int n = batteryPercentages.size();
+        
+        int cnt = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(batteryPercentages[i] > 0)
+            {
+                cnt++;
+                for(int j=i+1;j<n;j++)
+                {
+                    if(batteryPercentages[j] >= 0)
+                    {
+                        batteryPercentages[j] = max(0, batteryPercentages[j] - 1);
+                    }
+                }
+            }
+        }
+        
+        return cnt;
+    }
+};
