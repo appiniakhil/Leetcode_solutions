8cfbd80ddda4939d63fb68c732c02e439c802135
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-12-12 17:12:42 +0530

    Update 1464-Maximum Product of Two Elements in an Array.cpp

--- solutions/1464-Maximum Product of Two Elements in an Array.cpp
+++ solutions/1464-Maximum Product of Two Elements in an Array.cpp
@@ -1,1 +1,36 @@
+class Solution {
+public:
+    //Time Complexity :O(nlogn) && Space Complexity :O(1)
+    // int maxProduct(vector<int>& nums) {
+    //     int n = nums.size();
+    //     sort(nums.begin(), nums.end());
 
+    //     return (nums[n-1]-1)*(nums[n-2]-1);
+    // }
+
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int maxi1 = INT_MIN, maxi2 = INT_MIN, ind = 0;
+
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i] > maxi1)
+            {
+                maxi1 = nums[i];
+                ind = i;
+            }
+        }
+
+        nums[ind] = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i] > maxi2)
+            {
+                maxi2 = nums[i];
+            }
+        }
+
+        return (maxi1-1)*(maxi2-1);
+    }
+};
