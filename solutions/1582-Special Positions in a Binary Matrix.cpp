d9f128c3d61ba092e1805980b61270fbf88dae46
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-12-13 19:51:36 +0530

    Update 1582-Special Positions in a Binary Matrix.cpp

--- solutions/1582-Special Positions in a Binary Matrix.cpp
+++ solutions/1582-Special Positions in a Binary Matrix.cpp
@@ -1,1 +1,37 @@
+class Solution {
+public:
+    //Time Complexity :O(m * n) && Space Complexity :O(m + n)
+    int numSpecial(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
 
+        vector<int> row(m, 0), col(n, 0);
+
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(mat[i][j] == 1)
+                {
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+
+        int cnt=0;
+
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(mat[i][j] == 1 && row[i] == 1 && col[j] == 1)
+                {
+                    cnt++;
+                }
+            }
+        }
+        
+        return cnt;
+    }
+};
