4fb55de418a84fa3bda5a63375f8d8ed0fbc68e4
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-12-13 23:28:32 +0530

    Update 2929-Distribute Candies Among Children II.cpp

--- solutions/2929-Distribute Candies Among Children II.cpp
+++ solutions/2929-Distribute Candies Among Children II.cpp
@@ -1,1 +1,15 @@
+class Solution {
+public:
+    //Time Complexity :O(1) && Space Complexity :O(1)
+    long long distributeCandies(int n, int limit) {
+        long long cnt = 0;
 
+        for(int i = max(0, n-2*limit);i<=min(limit, n);i++) 
+        {
+            int j = min(limit, n-i)-max(0,n-i-limit) + 1;
+            cnt += max(0, j);
+        }
+
+        return cnt;
+    }
+};
