bdd318e4d87fac257be91c2a3c471bbc4082356f
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-12-15 04:09:06 +0530

    Update 1629-Slowest Key.cpp

--- solutions/1629-Slowest Key.cpp
+++ solutions/1629-Slowest Key.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    char slowestKey(vector<int>& releaseTimes, string keysPressed) {
+        int n = releaseTimes.size();
+        unordered_map<char, int> mp;
 
+        for(int i=0;i<n;i++) 
+        {
+            int time = (i == 0) ? releaseTimes[i] : releaseTimes[i] - releaseTimes[i - 1];
+            mp[keysPressed[i]] = max(mp[keysPressed[i]], time);
+        }
+
+        char ans = keysPressed[0];
+        for(char c : keysPressed) 
+        {
+            if(mp[c] > mp[ans] || (mp[c] == mp[ans] && c > ans)) 
+            {
+                ans = c;
+            }
+        }
+
+        return ans;
+    }
+};
