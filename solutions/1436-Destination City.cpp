c3a8d6361888780ad4b9de4fcb6f0971e4568ddf
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-12-16 01:07:00 +0530

    Update 1436-Destination City.cpp

--- solutions/1436-Destination City.cpp
+++ solutions/1436-Destination City.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    string destCity(vector<vector<string>>& paths) {
+        unordered_set<string> cities;
 
+        for(auto& path : paths)
+        {
+            cities.insert(path[0]);
+        }
+
+        for(auto& path : paths)
+        {
+            if(!cities.count(path[1]))
+            {
+                return path[1];
+            }
+        }
+
+        return "";
+    }
+};
