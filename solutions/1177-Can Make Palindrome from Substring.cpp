30c5ddd26c01f08b3fc25eb5112c9f470a179648
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-12-17 00:22:01 +0530

    Update 1177-Can Make Palindrome from Substring.cpp

--- solutions/1177-Can Make Palindrome from Substring.cpp
+++ solutions/1177-Can Make Palindrome from Substring.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    //Time Complexity :O(n*q) && Space Complexity :O(n)
+    vector<bool> canMakePaliQueries(string s, vector<vector<int>>& queries) {
+        vector<vector<int>> count(s.size() + 1, vector<int>(26, 0));
 
+        for(int i=0;i<s.size();i++) 
+        {
+            count[i + 1] = count[i];
+            count[i + 1][s[i] - 'a']++;
+        }
+
+        vector<bool> res;
+
+        for(auto& q : queries) 
+        {
+            int sum = 0;
+            for(int i=0;i<26;i++) 
+            {
+                sum += (count[q[1] + 1][i] - count[q[0]][i]) % 2;
+            }
+
+            res.push_back(sum / 2 <= q[2]);
+        }
+
+        return res;
+    }
+};
