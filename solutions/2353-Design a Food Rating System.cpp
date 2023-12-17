e6287920fd11a350e3be2f871bfb42529a64e0a3
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-12-18 02:49:15 +0530

    Update 2353-Design a Food Rating System.cpp

--- solutions/2353-Design a Food Rating System.cpp
+++ solutions/2353-Design a Food Rating System.cpp
@@ -1,1 +1,28 @@
+class FoodRatings {
+private:
+    unordered_map<string, pair<string, int>> foodToCuisineAndRating;
+    map<string, map<int, set<string>, greater<int>>> cuisineToFood;
 
+public:
+    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
+        int n = foods.size();
+        for (int i = 0; i < n; ++i) {
+            foodToCuisineAndRating[foods[i]] = {cuisines[i], ratings[i]};
+            cuisineToFood[cuisines[i]][ratings[i]].insert(foods[i]);
+        }
+    }
+
+    void changeRating(string food, int newRating) {
+        auto& [cuisine, rating] = foodToCuisineAndRating[food];
+        cuisineToFood[cuisine][rating].erase(food);
+        if (cuisineToFood[cuisine][rating].empty()) {
+            cuisineToFood[cuisine].erase(rating);
+        }
+        rating = newRating;
+        cuisineToFood[cuisine][newRating].insert(food);
+    }
+
+    string highestRated(string cuisine) {
+        return *cuisineToFood[cuisine].begin()->second.begin();
+    }
+};
