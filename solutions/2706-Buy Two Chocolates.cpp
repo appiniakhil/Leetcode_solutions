5f8569abbca59f8fe0019980a2652752ed3c3bcc
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-12-20 22:36:05 +0530

    Update 2706-Buy Two Chocolates.cpp

--- solutions/2706-Buy Two Chocolates.cpp
+++ solutions/2706-Buy Two Chocolates.cpp
@@ -1,1 +1,16 @@
+class Solution {
+public:
+    //Time Complexity :O(nlogn) && Space Complexity :O(1)
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(), prices.end());
+        
+        int diff = money - prices[0] - prices[1];
 
+        if(diff >= 0)
+        {
+            return diff;
+        }
+
+        return money;
+    }
+};
