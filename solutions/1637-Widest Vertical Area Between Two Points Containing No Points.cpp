45af5aedcc18c80c1c2547cbdc46ab132b89b3fb
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-12-21 15:27:29 +0530

    Update 1637-Widest Vertical Area Between Two Points Containing No Points.cpp

--- solutions/1637-Widest Vertical Area Between Two Points Containing No Points.cpp
+++ solutions/1637-Widest Vertical Area Between Two Points Containing No Points.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    //Time Complexity :O(nlogn) && Space Complexity :O(n)
+    int maxWidthOfVerticalArea(vector<vector<int>>& points) {
+        vector<int> xcoord;
 
+        for(auto& i : points)
+        {
+            xcoord.push_back(i[0]);
+        }
+
+        sort(xcoord.begin(), xcoord.end());
+
+        int ans = 0;
+
+        for(int i=1;i<xcoord.size();i++)
+        {
+            ans = max(ans, xcoord[i] - xcoord[i-1]);
+        }
+
+        return ans;
+    }
+};
