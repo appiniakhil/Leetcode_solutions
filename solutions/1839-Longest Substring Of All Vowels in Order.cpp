1e1526614054b68cde089d57dbf49b6eada5adb8
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-12-23 15:13:24 +0530

    Update 1839-Longest Substring Of All Vowels in Order.cpp

--- solutions/1839-Longest Substring Of All Vowels in Order.cpp
+++ solutions/1839-Longest Substring Of All Vowels in Order.cpp
@@ -1,1 +1,33 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int longestBeautifulSubstring(string word) {
+        int n = word.size();
 
+        int res=0, cnt=1, vow=1;
+
+        for(int i=1;i<n;i++)
+        {
+            if(word[i] >= word[i-1])
+            {
+                if(word[i] > word[i-1])
+                {
+                    vow++;
+                }
+                cnt++;
+
+                if(vow == 5)
+                {
+                    res = max(res, cnt);
+                }
+            }
+            else
+            {
+                cnt = 1;
+                vow = 1;
+            }
+        }
+
+        return res;
+    }
+};
