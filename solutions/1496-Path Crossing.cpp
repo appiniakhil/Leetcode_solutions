17557399112a153a19512a1e6d0041b914a9be71
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-12-23 15:01:16 +0530

    Update 1496-Path Crossing.cpp

--- solutions/1496-Path Crossing.cpp
+++ solutions/1496-Path Crossing.cpp
@@ -1,1 +1,37 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    bool isPathCrossing(string path) {
+        set<pair<int, int>> vis;
 
+        int x=0, y=0;
+        vis.insert({x, y});
+
+        for(char i : path)
+        {
+            if(i == 'N')
+            {
+                ++y;
+            }
+            else if(i == 'S')
+            {
+                --y;
+            }
+            else if(i == 'E')
+            {
+                ++x;
+            }
+            else if(i == 'W')
+            {
+                --x;
+            }
+
+            if(!vis.insert({x, y}).second)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
