fbfd39176100bd7d2647055c7d605e850b1e6f5d
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-12-24 19:35:01 +0530

    Update 1758-Minimum Changes To Make Alternating Binary String.cpp

--- solutions/1758-Minimum Changes To Make Alternating Binary String.cpp
+++ solutions/1758-Minimum Changes To Make Alternating Binary String.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    int minOperations(string s) {
+        int n = s.size();
 
+        int cnt=0, cnt1=0;
+
+        for(int i=0;i<n;i++)
+        {
+            if(s[i] != '0' + i%2)
+            {
+                cnt++;
+            }
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            if(s[i] != '0' + (i+1)%2)
+            {
+                cnt1++;
+            }
+        }
+
+        return min(cnt, cnt1);
+    }
+};
