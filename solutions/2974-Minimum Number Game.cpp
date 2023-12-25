2839f813891802a7314f8f6b5037c70fa46d7220
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-12-25 18:32:54 +0530

    Update 2974-Minimum Number Game.cpp

--- solutions/2974-Minimum Number Game.cpp
+++ solutions/2974-Minimum Number Game.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    vector<int> numberGame(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        
+        vector<int> ans;
+        
+        int i=0,j=1;
+        while(i <n || j< n)
+        {
+            ans.push_back(nums[j]);
+            ans.push_back(nums[i]);
+            i+=2;
+            j+=2;
+        }
+            
+        return ans;
+    }
+};
