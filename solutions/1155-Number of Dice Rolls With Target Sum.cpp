e896ae97531e6b9c071f476f456624c7fda4ae34
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-12-26 23:07:05 +0530

    Update 1155-Number of Dice Rolls With Target Sum.cpp

--- solutions/1155-Number of Dice Rolls With Target Sum.cpp
+++ solutions/1155-Number of Dice Rolls With Target Sum.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+    //Time Complexity :O(n*k*target) && Space Complexity :O(n*target)
+    int numRollsToTarget(int n, int k, int target) {
+        const int MOD = 1e9+7;
 
+        vector<vector<int>> dp(n+1, vector<int>(target+1));
+
+        dp[0][0] = 1;
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=target;j++)
+            {
+                for(int x=1;x<=k && x<=j;x++)
+                {
+                    dp[i][j] = (dp[i][j] + dp[i-1][j-x]) % MOD;
+                }
+            }
+        }
+
+        return dp[n][target];
+    }
+};
