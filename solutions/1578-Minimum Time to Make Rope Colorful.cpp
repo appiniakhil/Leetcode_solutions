79c39753890bd1e8e7de9076c99f6beb1bb564f2
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-12-27 17:30:23 +0530

    Update 1578-Minimum Time to Make Rope Colorful.cpp

--- solutions/1578-Minimum Time to Make Rope Colorful.cpp
+++ solutions/1578-Minimum Time to Make Rope Colorful.cpp
@@ -1,1 +1,33 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    int helper(int i, string& colorSeq, vector<int>& time, vector<int>& v, char prevColor, int prevTime)
+    {
+        if(i<0)
+        {
+            return 0;
+        }
 
+        if(v[i] != -1)
+        {
+            return v[i];
+        }
+
+        if(colorSeq[i] == prevColor)
+        {
+            return v[i] = helper(i-1,colorSeq,time,v,colorSeq[i],max(time[i], prevTime)) + min(time[i], prevTime);
+        }
+        else
+        {
+            return v[i] = helper(i-1,colorSeq,time,v,colorSeq[i],time[i]);
+        }
+    }
+
+    int minCost(string colors, vector<int>& neededTime) {
+        int n = colors.size();
+
+        vector<int> dp(n, -1);
+
+        return helper(n-1, colors, neededTime,dp,'A',neededTime[n-1]);
+    }
+};
