4c793d9018eed7ea19c3b701713157f7c6529b6f
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-12-30 23:31:38 +0530

    Update 1897-Redistribute Characters to Make All Strings Equal.cpp

--- solutions/1897-Redistribute Characters to Make All Strings Equal.cpp
+++ solutions/1897-Redistribute Characters to Make All Strings Equal.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    //Time complexity: O(N*M) && Space complexity:O(M) where N is the number of words and M is the average length of the words
 
+    bool makeEqual(vector<string>& words) {
+        int n = words.size();
+
+        unordered_map<char, int> mp;
+
+        for(int i=0;i<n;i++)
+        {
+            for(auto j : words[i])
+            {
+                mp[j]++;
+            }
+        }
+
+        for(auto i : mp)
+        {
+            if(i.second % n != 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
