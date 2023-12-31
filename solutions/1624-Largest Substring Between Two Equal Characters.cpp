fa960b58e598e8ec0b21b1ed240da8b9867b024f
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2024-01-01 00:28:23 +0530

    Update 1624-Largest Substring Between Two Equal Characters.cpp

--- solutions/1624-Largest Substring Between Two Equal Characters.cpp
+++ solutions/1624-Largest Substring Between Two Equal Characters.cpp
@@ -1,20 +1,42 @@
 class Solution {
 public:
+    //Time Complexity :O(n*n) && Space Complexity :O(n)
+    // int maxLengthBetweenEqualCharacters(string s) {
+    //     int n=s.size();
+
+    //     int maxi = -1;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         for(int j=i+1;j<n;j++)
+    //         {
+    //             if(s[i] == s[j])
+    //             {
+    //                 maxi = max(maxi, j-i-1);
+    //             }
+    //         }
+    //     }
+
+    //     return maxi;
+    // }
+    
+    //Time Complexity :O(n) && Space Complexity :O(n)
     int maxLengthBetweenEqualCharacters(string s) {
         int n=s.size();
 
+        unordered_map<char, int> mp;
         int maxi = -1;
         for(int i=0;i<n;i++)
         {
-            for(int j=i+1;j<n;j++)
+            if(mp.find(s[i]) != mp.end())
             {
-                if(s[i] == s[j])
-                {
-                    maxi = max(maxi, j-i-1);
-                }
+                maxi = max(maxi, i-mp[s[i]]-1);
+            }
+            else
+            {
+                mp[s[i]] = i;
             }
         }
 
         return maxi;
     }
-};v
+};
