ca728ef2cbfa8b5cfd53206a86a20ef91c6b2ede
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2024-01-02 23:19:14 +0530

    Update 2610-Convert an Array Into a 2D Array With Conditions.cpp

--- solutions/2610-Convert an Array Into a 2D Array With Conditions.cpp
+++ solutions/2610-Convert an Array Into a 2D Array With Conditions.cpp
@@ -1,1 +1,37 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        map<int, int> mp;
 
+        for(int i : nums)
+        {
+            mp[i]++;
+        }
+
+        vector<vector<int>> ans;
+
+        while(!mp.empty())
+        {
+            vector<int> row;
+
+            for(auto i = mp.begin();i != mp.end();)
+            {
+                row.push_back(i->first);
+
+                if(--i->second == 0)
+                {
+                    i = mp.erase(i);
+                }
+                else
+                {
+                    ++i;
+                }
+            }
+
+            ans.push_back(row);
+        }
+
+        return ans;
+    }
+};
