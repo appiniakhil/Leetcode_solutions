38fea4eac376f1b01aa2e10f21382b29b928b9a6
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2024-01-04 23:15:37 +0530

    Update 2980-Check if Bitwise OR Has Trailing Zeros.cpp

--- solutions/2980-Check if Bitwise OR Has Trailing Zeros.cpp
+++ solutions/2980-Check if Bitwise OR Has Trailing Zeros.cpp
@@ -1,1 +1,38 @@
+class Solution {
+public:
+    //Time Complexity :O(n * n) && Space Complexity :O(1)
+    // bool hasTrailingZeros(vector<int>& nums) {
+    //     int n = nums.size();
+        
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         for(int j=i+1;j<n;j++)
+    //         {
+    //             int orOp = nums[i]  | nums[j];
+                
+    //             if(orOp % 2 == 0)
+    //             {
+    //                 return true;
+    //             }
+    //         }
+    //     }
+        
+    //     return false;
+    // }
 
+    //Time Complexity :O(n) && Space Complexity :O(1)
+    bool hasTrailingZeros(vector<int>& nums) {
+        int n = nums.size();
+        
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i] % 2 == 0)
+            {
+                cnt++;
+            }
+        }
+        
+        return (cnt < 2) ? false: true; 
+    }
+};
