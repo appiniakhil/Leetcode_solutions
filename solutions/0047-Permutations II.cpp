2c7e98a597bcc0812c3fb5993ddf138a4b11f820
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2024-01-04 22:57:10 +0530

    Update 0047-Permutations II.cpp

--- solutions/0047-Permutations II.cpp
+++ solutions/0047-Permutations II.cpp
@@ -1,1 +1,41 @@
+class Solution {
+public:
+    //Time Complexity :o(n!) && Space Complexity :O(n)
+    void helper(vector<int>& nums, vector<bool>& vis, vector<int>& currPerm, vector<vector<int>>& res)
+    {
+        if(currPerm.size() == nums.size())
+        {
+            res.push_back(currPerm);
+            return ;
+        }
 
+        for(int i=0;i<nums.size();i++)
+        {
+            if(vis[i] || (i>0 && nums[i] == nums[i-1] && !vis[i-1]))
+            {
+                continue;
+            }
+
+            vis[i] = true;
+
+            currPerm.push_back(nums[i]);
+
+            helper(nums, vis, currPerm, res);
+
+            vis[i] = false;
+            currPerm.pop_back();
+        }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> ans;
+        vector<int> currPerm;
+        vector<bool> vis(nums.size(), false);
+
+        helper(nums, vis, currPerm ,ans);
+
+        return ans;
+    }
+};
