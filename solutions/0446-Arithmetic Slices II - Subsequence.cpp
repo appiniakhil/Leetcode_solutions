7f08eebaab0ee66e9e58768f9278187e1fadab28
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2024-01-08 00:16:54 +0530

    Update 0446-Arithmetic Slices II - Subsequence.cpp

--- solutions/0446-Arithmetic Slices II - Subsequence.cpp
+++ solutions/0446-Arithmetic Slices II - Subsequence.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    //Time Complexity :O(n * n) && Space Complexity :O(n * n)
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int n = nums.size();
 
+        vector<unordered_map<long long, int>> dp(n);
+
+        int cnt = 0;
+        for(int i=0;i<n;i++) 
+        {
+            for(int j=0;j<i;j++) 
+            {
+                long long diff = (long long)nums[i] - nums[j];
+                int seqCnt = dp[j].count(diff) ? dp[j][diff] : 0;
+
+                cnt += seqCnt;
+
+                dp[i][diff] += seqCnt + 1;
+            }
+        }
+
+        return cnt;
+    }
+};
