2bf35005fce8d111f70d6a5e483b9db3d79a0340
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2024-01-12 07:46:46 +0530

    Update 1704-Determine if String Halves Are Alike.cpp

--- solutions/1704-Determine if String Halves Are Alike.cpp
+++ solutions/1704-Determine if String Halves Are Alike.cpp
@@ -1,1 +1,42 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    int vow(string s)
+    {
+        int n = s.size();
+        int cnt = 0;
 
+        for(int i=0;i<n;i++)
+        {
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+            {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+
+    bool halvesAreAlike(string s) {
+        int n = s.size();
+        string s1 = "";
+        string s2 = "";
+
+        for(int i=0;i<n/2;i++)
+        {
+            s1 += s[i];
+        }
+
+        for(int i=n/2;i<n;i++)
+        {
+            s2 += s[i];
+        }
+
+        if(vow(s1) == vow(s2))
+        {
+            return true;
+        }
+
+        return false;
+    }
+};
