d229b9f3cef4c9df4c8c52e6ec7f33053a9ed556
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2024-01-22 22:38:24 +0530

    Update 0645-Set Mismatch.cpp

--- solutions/0645-Set Mismatch.cpp
+++ solutions/0645-Set Mismatch.cpp
@@ -1,1 +1,33 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int> mp;
+        vector<int> ans;
 
+        for(auto& i : nums)
+        {
+            mp[i]++;
+        }
+
+        for(int i=1;i<=n;i++)
+        {
+            if(mp[i] == 2)
+            {
+                ans.push_back(i);
+            }
+        }
+
+        for(int i=1;i<=n;i++)
+        {
+            if(mp[i] == 0)
+            {
+                ans.push_back(i);
+                break;
+            }
+        }
+
+        return ans;
+    }
+};
