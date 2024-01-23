c2757a94c904ba93cdf803261c58e60f4ad91754
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2024-01-23 17:34:48 +0530

    Update 1239-Maximum Length of a Concatenated String with Unique Characters.cpp

--- solutions/1239-Maximum Length of a Concatenated String with Unique Characters.cpp
+++ solutions/1239-Maximum Length of a Concatenated String with Unique Characters.cpp
@@ -1,1 +1,50 @@
+class Solution {
+public:
+    //Time complexity: O(2 ^ n) && Space Complexity :O(2 ^ n)
+    bool isValid(string& curr, string& newS)
+    {
+        unordered_set<char> s;
 
+        for(char c : newS)
+        {
+            if(s.count(c) > 0)
+            {
+                return false;
+            }
+
+            s.insert(c);
+
+            if(curr.find(c) != string::npos)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void backtrack(vector<string>& arr, string curr, int start, int& len)
+    {
+        if(len < curr.length())
+        {
+            len = curr.length();
+        }
+
+        for(int i=start;i<arr.size();i++)
+        {
+            if(!isValid(curr, arr[i]))
+            {
+                continue;
+            }
+
+            backtrack(arr, curr + arr[i], i+1, len);
+        }
+    }
+
+    int maxLength(vector<string>& arr) {
+        int len = 0;
+        backtrack(arr, "", 0, len);
+
+        return len;
+    }
+};
