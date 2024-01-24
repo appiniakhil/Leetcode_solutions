6c45d5d8872496af77da823df921615f17678965
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2024-01-25 00:13:54 +0530

    Update 1457-Pseudo-Palindromic Paths in a Binary Tree.cpp

--- solutions/1457-Pseudo-Palindromic Paths in a Binary Tree.cpp
+++ solutions/1457-Pseudo-Palindromic Paths in a Binary Tree.cpp
@@ -1,1 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(h)
+    int dfs(TreeNode* root, int cnt)
+    {
+        if(!root)
+        {
+            return 0;
+        }
 
+        cnt ^= 1 << (root->val-1);
+
+        int res = dfs(root->left, cnt) + dfs(root->right, cnt);
+
+        if(!root->left && !root->right && ((cnt) & (cnt-1)) == 0)
+        {
+            res++;
+        }
+
+        return res;
+    }
+
+    int pseudoPalindromicPaths (TreeNode* root) {
+        return dfs(root, 0);    
+    }
+};
