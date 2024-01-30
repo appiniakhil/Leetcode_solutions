4ae421e44a8cc7c8ace85be6e3df885df15d8101
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2024-01-31 00:32:49 +0530

    Update 0150-Evaluate Reverse Polish Notation.cpp

--- solutions/0150-Evaluate Reverse Polish Notation.cpp
+++ solutions/0150-Evaluate Reverse Polish Notation.cpp
@@ -1,1 +1,46 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    bool isOperator(string &op)
+    {
+        return op.size() == 1 && string("+-*/").find(op) != string::npos;
+    }
 
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+
+        for(string& token : tokens)
+        {
+            if(isOperator(token))
+            {
+                int op2 = st.top();
+                st.pop();
+
+                int op1 = st.top();
+                st.pop();
+
+                switch(token[0])
+                {
+                    case '+':
+                        st.push(op1 + op2);
+                        break;
+                    case '-':
+                        st.push(op1 - op2);
+                        break;
+                    case '*':
+                        st.push(op1 * op2);
+                        break;
+                    case '/':
+                        st.push(op1 / op2);
+                        break;
+                }
+            }
+            else
+            {
+                st.push(stoi(token));
+            }
+        }
+
+        return st.top();
+    }
+};
