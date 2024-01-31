489649703d19a21f8da71a15e6574c12cb56b658
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2024-02-01 00:56:04 +0530

    Update 0739-Daily Temperatures.cpp

--- solutions/0739-Daily Temperatures.cpp
+++ solutions/0739-Daily Temperatures.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    //Time Complexity :O(n) && Space Complexity :O(n)
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
 
+        stack<int> s;
+        vector<int> ans(n);
+
+        for(int i=n-1;i>=0;i--)
+        {
+            while(!s.empty() && temperatures[i] >= temperatures[s.top()])
+            {
+                s.pop();
+            }
+
+            ans[i] = s.empty() ? 0 : s.top() - i;
+            s.push(i);
+        }
+
+        return ans;
+    }
+};
