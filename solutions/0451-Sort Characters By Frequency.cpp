3b5a8436e222d1131c6a2456ea3b76ecf78f621d
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2024-02-08 00:39:37 +0530

    Update 0451-Sort Characters By Frequency.cpp

--- solutions/0451-Sort Characters By Frequency.cpp
+++ solutions/0451-Sort Characters By Frequency.cpp
@@ -1,1 +1,33 @@
+class Solution {
+public:
+    //Time Complexity :O(nlogn) && Space Complexity :O(n)
+    string frequencySort(string s) {
+        int n = s.size();
+        vector<pair<int, char>> v;
+        unordered_map<char, int> mp;
 
+        for(int i=0;i<n;i++)
+        {
+            mp[s[i]]++;
+        }
+
+        for(auto i : mp)
+        {
+            v.push_back({i.second, i.first});
+        }
+        
+        sort(v.rbegin(), v.rend());
+        
+        string ans = "";
+        
+        for(auto i: v)
+        {
+            for(int j=0;j<i.first;j++)
+            {
+                ans += i.second;
+            }
+        }
+        
+        return ans;
+    }
+};
