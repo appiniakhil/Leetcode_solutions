4dd1f65edc089406d710603474d5865e05d18570
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2024-02-13 19:12:31 +0530

    Update 2108-Find First Palindromic String in the Array.cpp

--- solutions/2108-Find First Palindromic String in the Array.cpp
+++ solutions/2108-Find First Palindromic String in the Array.cpp
@@ -1,1 +1,35 @@
+class Solution {
+public:
+    //Time Complexity :O(n * n) && Space Complexity :O(n)
+    bool isPalindrome(string word)
+    {
+        int n = word.size();
+        int i=0,j=n-1;
 
+        while(i < j)
+        {
+            if(word[i] != word[j])
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+    string firstPalindrome(vector<string>& words) {
+        int n = words.size();
+
+        for(int i=0;i<n;i++)
+        {
+            if(isPalindrome(words[i]))
+            {
+                return words[i];
+            }
+        }
+
+        return "";
+    }
+};
