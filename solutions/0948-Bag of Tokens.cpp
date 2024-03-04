ab9c885e70b147520d44edf0bc2fae6e2759a61d
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2024-03-04 09:52:50 +0530

    Update 0948-Bag of Tokens.cpp

--- solutions/0948-Bag of Tokens.cpp
+++ solutions/0948-Bag of Tokens.cpp
@@ -1,1 +1,36 @@
+class Solution {
+public:
+    //Time Complexity :O(nlogn) && Space Complexity :O(1)
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        int score = 0, maxscore = 0;
+        int n = tokens.size();
 
+        sort(tokens.begin(), tokens.end());
+
+        int left = 0, right = n-1;
+
+        while(left <= right)
+        {
+            if(power >= tokens[left])
+            {
+                power -= tokens[left];
+                score++;
+                left++;
+
+                maxscore = max(maxscore, score);
+            }
+            else if(score > 0)
+            {
+                power += tokens[right];
+                score--;
+                right--;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return maxscore;
+    }
+};
